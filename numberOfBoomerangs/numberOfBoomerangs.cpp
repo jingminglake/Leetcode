8f0a3b7bc640181a8011576888b2dd1cecc6a3ad
jingminglake <yaxionh@121.184.21.198.tigernet.wifi.dyn.clemson.edu>
2017-06-06 18:20:13 -0400

    leetcode 447

--- numberOfBoomerangs/numberOfBoomerangs.cpp
+++ numberOfBoomerangs/numberOfBoomerangs.cpp
@@ -8,13 +8,18 @@ class Solution{
 public:
   int numberOfBoomerangs(vector<pair<int, int> >& points) {
     int ans = 0;
-    for (auto point : points) {
-      unordered_map<double, int> map;
-      for (auto point2 : points)
-	map[hypot(point.first - point2.first, point.second - point2.second)]++;
+    int size = points.size();
+    unordered_map<double, int> map;
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+	if (i == j)
+	  continue;
+	map[hypot(points[i].first - points[j].first, points[i].second - points[j].second)]++;
+      }
       for (auto val : map)
 	if (val.second > 1)
 	  ans += val.second * (val.second - 1);
+      map.clear();
     }
     return ans;
   }
