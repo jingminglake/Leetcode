c0c6cb14619c7f542e9f9e86b96aec13a91f3c42
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2017-06-17 12:01:41 -0400

    leetcode 111

---
+++ minimumDepthOfBinaryTree/minimumDepthOfBinaryTree.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution{
+public:
+  int minDepth(TreeNode *root) {
+    if (!root)
+      return 0;
+    if (root->left && !root->right)
+      return minDepth(root->left) + 1;
+    else if (!root->left && root->right)
+      return minDepth(root->right) + 1;
+    else if (root->left && root->right)
+      return min(minDepth(root->left), minDepth(root->right)) + 1;
+    else
+      return 1;
+  }
+};
+
+int main()
+{
+  Solution s;
+  string str = "123#4";
+  int size = str.length();
+  vector<TreeNode *> vec;
+  for (int i = 0; i < size; i++) {
+    if (str[i] != '#') {
+      vec.push_back(new TreeNode(str[i] - '0'));
+    } else {
+      vec.push_back(NULL);
+    }
+  }
+  for (int i = 0; i < size/2; i++) {
+    if (i*2 + 1 < size)
+      vec[i]->left = vec[i*2 + 1];
+    if (i*2 + 2 < size)
+      vec[i]->right = vec[i*2 + 2];
+  }
+  cout << s.minDepth(vec[0]) << endl;
+  for (TreeNode *t : vec) {
+    delete t;
+  }
+  vec.clear();
+  return 0;
+}
