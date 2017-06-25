2647e7ffb61e5c2b91ed3df4f7fe9d53c06ff560
jingminglake <yaxionh@98.232.21.198.tigernet.wifi.dyn.clemson.edu>
2017-06-25 17:48:48 -0400

    leetcode 153

--- findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp
+++ findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp
@@ -8,13 +8,11 @@ public:
   int findMin(vector<int>& nums) {
     int left = 0, right = nums.size()-1;
     while (left < right) {
-      if (nums[left] < nums[right])
-	return nums[left];
       int mid = left + (right-left)/2;
-      if (nums[mid] >= nums[left]) {
-	left = mid + 1;
-      } else {
+      if (nums[mid] <= nums[right]) {
 	right = mid;
+      } else {
+	left = mid + 1;
       }
     }
     return nums[left];
@@ -24,8 +22,8 @@ public:
 int main()
 {
   Solution s;
-  int a[7] = {4,5,6,7,0,1,2};
-  vector<int> vec(a, a+7);
+  int a[7] = {3,1,2,7,0,1,2};
+  vector<int> vec(a, a+3);
   cout << s.findMin(vec);
   cout << endl;
   return 0;
