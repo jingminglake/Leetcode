e0e00d157128499c38918219122d64867479ae28
jingminglake <yaxionh@yaxionhdeair.attlocal.net>
2017-06-26 23:12:26 -0400

    leetcode 142

---
+++ linkedListCycleII/linkedListCycleII.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode* detectCycle(ListNode* head) {
+    if(!head || !head->next)
+      return NULL;
+    bool hasCycle = false;
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast->next && fast->next->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+	hasCycle = true;
+	break;
+      }
+    }
+    if (!hasCycle)
+      return NULL;
+    fast = head;
+    while (slow != fast) {
+      slow = slow->next;
+      fast = fast->next;
+    }
+    return slow;
+  }
+};
+
+int main()
+{
+  int a[5] = {4, 3, 2, 3, 4};
+  ListNode *l = new ListNode(a[0]);
+  ListNode *tail = l;
+  ListNode *t = NULL;
+  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+    ListNode *temp = new ListNode(a[i]);
+    tail->next = temp;
+    tail = temp;
+    if (i == 2)
+      t = temp;
+  }
+  tail->next = t;
+  Solution s;
+  cout << s.detectCycle(l)->val << endl;
+  ListNode *h = l;
+  tail->next = NULL;
+  while (h) {
+    ListNode *p = h;
+    h = h->next;
+    cout << "delete " << p->val << " ";
+    delete p;
+  }
+  cout << endl;
+  return 0;
+}
