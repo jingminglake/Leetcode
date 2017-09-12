1546f7bb6bd9ea6f54f2d0fa9adda503d74c27ee
jingminglake <yaxionh@134.238.21.198.tigernet.wifi.dyn.clemson.edu>
2017-09-12 12:14:56 -0400

    leetcode 62

---
+++ uniquePaths/uniquePaths.cpp
@@ -0,0 +1,29 @@
+// clang++ uniquePaths.cpp -std=c++11
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+  int uniquePaths(int m, int n) {
+    if (m < 0 || n < 0)
+      return 0;
+    if (m > n)
+      return uniquePaths(n, m);
+    vector<int> cur(m, 1);
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+	cur[i] += cur[i - 1];
+      }
+    }//for
+    return cur[m - 1];
+  }
+};
+
+int main()
+{
+  Solution s;
+  cout << s.uniquePaths(2,1) << endl;;
+  return 0;
+}
