858514a06ffdea384341423c800250395c51032b
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-01-05 11:53:15 -0500

    leetcode 646

--- maximumLengthOfPairChain/maximumLengthOfPairChain.cpp
+++ maximumLengthOfPairChain/maximumLengthOfPairChain.cpp
@@ -15,17 +15,18 @@ public:
 class Solution {
 public:
   int findLongestChain(vector<vector<int> >& pairs) {
-    int size = pairs.size();
-    if (size == 0)
-      return 0;
+    int dp[pairs.size()];
+    fill_n(dp, pairs.size(), 1);
+    int res = 1;
     sort(pairs.begin(), pairs.end(), Compare());
-    vector<int> dp(size, 1);
-    for (int i = 0; i < size; i++) {
+    for (int i = 1; i < pairs.size(); i++) {
       for (int j = 0; j < i; j++) {
-	dp[i] = max(dp[i], pairs[j][1] < pairs[i][0] ? dp[j] + 1 : dp[j]);
+	if (pairs[j][1] < pairs[i][0])
+	  dp[i] = max(dp[i], dp[j] + 1);
       }
+      res = max(res, dp[i]);
     }
-    return dp[size - 1];
+    return res;
   }
 };
 
