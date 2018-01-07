30d0b47d26deff37816fd08083146be762588250
jingminglake <yaxionh@213.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-01-07 18:24:15 -0500

    leetcode 74_2

--- searchA2DMatrix/searchA2DMatrix.cpp
+++ searchA2DMatrix/searchA2DMatrix.cpp
@@ -8,36 +8,36 @@ using namespace std;
 class Solution{
 public:
   bool searchMatrix(vector<vector<int> >& matrix, int target) {
-    int m = matrix.size();
-    if (m == 0)
+    if (matrix.size() == 0)
       return false;
-    int n = matrix[0].size();
-    if (n == 0)
-      return false;
-    int low = 0, high = m - 1;
-    while (low <= high) {
-      int mid = low + (high - low)/2;
-      if (matrix[mid][0] < target)
-	low = mid + 1;
-      else if (matrix[mid][0] > target)
-	high = mid - 1;
-      else
-	return true;
+    vector<int> col0;
+    for (int i = 0; i < matrix.size(); i++) {
+      if (matrix[i].size())
+	col0.push_back(matrix[i][0]);
     }
-    if (high < 0) {
-      return false;
-    }
-    int left = 0, right = n - 1;
-    while (left <= right) {
-      int mid = left + (right - left)/2;
-      if (matrix[high][mid] < target)
-	left = mid + 1;
-      else if (matrix[high][mid] > target)
-	right = mid - 1;
-      else
-	return true;
-    }
+    pair<int, bool> p = binarySearch(col0, target);
+    if (p.second)
+      return true;
+    if (p.first >= 0)
+      return binarySearch(matrix[p.first], target).second;
     return false;
+  }
+  pair<int, bool> binarySearch(vector<int>& vec, int target) {
+    pair<int, bool> res;
+    int left = 0, right = vec.size() - 1;
+    while (left <= right) {
+      int mid = left  + (right - left) / 2;
+      if (vec[mid] == target) {
+	res = make_pair(mid, true);
+	return res;
+      } else if (vec[mid] < target) {
+	left = mid + 1;
+      } else if (vec[mid] > target) {
+	right = mid - 1;
+      }
+    }
+    res = make_pair(right, false);
+    return res;
   }
 };
 
