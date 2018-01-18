e74e040c438546d33db238e3787613336d1d5633
jingminglake <yaxionh@9.234.21.198.tigernet.wifi.dyn.clemson.edu>
2018-01-18 18:35:56 -0500

    leetcode 145

---
+++ binaryTreePostorderTraversal/binaryTreePostorderTraversal2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution{
+public:
+  vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> res;
+    if (!root) 
+      return res;
+    TreeNode *cur = root;
+    stack<TreeNode*> s;
+    TreeNode *lastVisited = nullptr;
+    while (cur || !s.empty()) {
+      while (cur) {
+	s.push(cur);
+	cur = cur->left;
+      }
+      cur = s.top();
+      if (!cur->right || cur->right == lastVisited) {
+	s.pop();
+	res.push_back(cur->val);
+	lastVisited = cur;
+	cur = nullptr;
+      } else {
+	cur = cur->right;
+      }
+    } 
+    return res;
+  }
+};
+
+int main()
+{
+  Solution s;
+  int tree[12] = {1,2,3,4,9999,5,6,9999,9999,9999,9999,7};
+  int size = sizeof(tree)/sizeof(tree[0]);
+  vector<TreeNode *> vec;
+  for (int i = 0; i < size; i++) {
+    if (tree[i] != 9999) {
+      vec.push_back(new TreeNode(tree[i]));
+    } else {
+      vec.push_back(NULL);
+    }
+  }
+  for (int i = 0; i < size/2; i++) {
+    if(!vec[i])
+      continue;
+    if (i*2 + 1 < size)
+      vec[i]->left = vec[i*2 + 1];
+    if (i*2 + 2 < size)
+      vec[i]->right = vec[i*2 + 2];
+  }
+  vector<int> res = s.postorderTraversal(vec[0]);
+  for (int i : res)
+    cout << i << " ";
+  cout << endl;
+  for (TreeNode *t : vec) {
+    //cout << t->val << " ";
+    delete t;
+  }
+  vec.clear();
+  return 0;
+}
