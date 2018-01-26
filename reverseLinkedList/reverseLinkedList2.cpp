5158e90b1eb1f5d3f31e2151e73905c96528b3ab
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-01-26 09:59:26 -0500

    leetcode 206

---
+++ reverseLinkedList/reverseLinkedList2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode* reverseList(ListNode* head) {
+    if (!head || !head->next) {
+      return head;
+    }
+    ListNode *newHead = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
+  }
+};
+
+int main()
+{
+  int a[5] = {0, 1, 2, 3, 4};
+  ListNode *l = new ListNode(a[0]);
+  ListNode *tail = l;
+  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+    ListNode *temp = new ListNode(a[i]);
+    tail->next = temp;
+    tail = temp;
+  }
+  Solution s;
+  ListNode * h = s.reverseList(l);
+  while (h) {
+    ListNode *p = h;
+    h = h->next;
+    cout << "delete " << p->val << " ";
+    delete p;
+  }
+  cout << endl;
+
+  return 0;
+}
