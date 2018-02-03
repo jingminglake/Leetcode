7c8987a686f2fcab78538272c285fdc2ec4408b7
jingminglake <yaxionh@191.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-02 19:52:09 -0500

    leetcode 67

--- addBinary/addBinary.cpp
+++ addBinary/addBinary.cpp
@@ -4,38 +4,25 @@ using namespace std;
 class Solution{
 public:
   string addBinary(string a, string b) {
-    if (a.length() == 0)
-      return b;
-    if (b.length() == 0)
-      return a;
-    string ans;
+    string res;
     int carry = 0;
-    int sum = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
-    while (i >= 0 && j >= 0) {
-      sum = a[i] - '0' + b[j] - '0' + carry;
-      i--;
-      j--;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    int i = 0, j = 0;
+    while (i < a.length() || j < b.length() || carry != 0) {
+      int sum = 0;
+      if (i < a.length())
+	sum += a[i] - '0';
+      if (j < b.length())
+	sum += b[j] - '0';
+      sum += carry;
+      res += sum % 2 + '0';
       carry = sum / 2;
-      ans += sum % 2 + '0';
+      i++;
+      j++;
     }
-    while (i >= 0) {
-      sum = a[i] - '0' + carry;
-      i--;
-      carry = sum / 2;
-      ans += sum % 2 + '0';
-    }
-    while (j >= 0) {
-      sum = b[j] - '0' + carry;
-      j--;
-      carry = sum / 2;
-      ans += sum % 2 + '0';
-    }
-    if (carry == 1)
-      ans += '1';
-    reverse(ans.begin(), ans.end());
-    return ans;
+    reverse(res.begin(), res.end());
+    return res;
   }
 };
 
