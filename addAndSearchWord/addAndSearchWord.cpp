444d2c2661fe712417012b24acf73dea0de6ebf4
jingminglake <yaxionh@191.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-05 00:36:39 -0500

    leetcode 211

--- addAndSearchWord/addAndSearchWord.cpp
+++ addAndSearchWord/addAndSearchWord.cpp
@@ -9,59 +9,60 @@ using namespace std;
 
 class TrieNode {
 public:
-  TrieNode *next[26];
-  bool isWord;
-  TrieNode() : isWord(false){
-    memset(next, 0, sizeof(TrieNode *) * 26);
+  TrieNode() : isWord(false) {
+    memset(next, 0, sizeof(next));
   }
+  ~TrieNode() {
+    for (TrieNode *t : next) {
+      if (t)
+	delete t;
+    }
+  }
+  TrieNode* next[26];
+  bool isWord;
 };
 
 class WordDictionary {
 public:
   /** Initialize your data structure here. */
   WordDictionary() {
-    root = new TrieNode();
+    root = new TrieNode(); 
   }
-    
-  /** Inserts a word into the data structure. */
-  void addWord(string word) {
-    TrieNode *p = root;
-    for (int i = 0; i < word.size(); i++) {
-      if (p->next[word[i]- 'a'] == NULL)
-	p->next[word[i] - 'a'] = new TrieNode();
-      p = p->next[word[i] - 'a'];
-    }
-    p->isWord = true;
-  }
-    
-  /** Returns if the word is in the trie. */
-  bool search(string word) {
-    return find(word, root);
-  }
-    
   ~WordDictionary() {
-    delete root;
+    delete root; 
   }
-private:
- bool find(string& key, TrieNode* r) {
-    TrieNode *p = r;// TrieNode must find start from p
-    int size = key.length();
-    for (int i = 0; i < size; i++) {
-      if (!p)
-	break;
-      if (key[i] == '.') {
-	string remain = key.substr(i+1);
-	TrieNode *t = p;
-	for (int j = 0; j < 26; j++) {
-	  p = t->next[j];
-	  if (find(remain, p))
+  /** Adds a word into the data structure. */
+  void addWord(string word) {
+    TrieNode *cur = root;
+    for (char c : word) {
+      if (!cur->next[c - 'a']) {
+	cur->next[c - 'a'] = new TrieNode();
+      }
+      cur = cur->next[c - 'a'];
+    }
+    cur->isWord = true;
+  }
+    
+  /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+  bool search(string word) {
+    return searchHelper(word, root);
+  }
+  bool searchHelper(string word, TrieNode* cur) {
+    for (int i = 0; i < word.size(); i++) {
+      if (!cur)
+	return false;
+      if (word[i] == '.') {
+	string remain = word.substr(i + 1);
+	for (TrieNode *t : cur->next) {
+	  if (searchHelper(remain, t))
 	    return true;
 	}
+	return false;
       } else {
-	p = p->next[key[i] - 'a'];
+	cur = cur->next[word[i] - 'a'];
       }
     }
-    return p && p->isWord;
+    return cur && cur->isWord;
   }
   TrieNode *root;
 };
@@ -70,7 +71,7 @@ int main()
 {
   WordDictionary obj;
   obj.addWord("a");
-  // obj.addWord("a");
+  obj.addWord("aa");
   //obj.addWord("mad");
   cout << obj.search("a") << endl;
   cout << obj.search("aa") << endl;
