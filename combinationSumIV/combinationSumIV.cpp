ac590fb4edce9ad6ff8fe0e909a84800bb5ec940
jingminglake <yaxionh@39.232.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-07 15:46:57 -0500

    leetcode 377

--- combinationSumIV/combinationSumIV.cpp
+++ combinationSumIV/combinationSumIV.cpp
@@ -8,15 +8,16 @@ using namespace std;
 class Solution {
 public:
   int combinationSum4(vector<int>& nums, int target) {
-    int size = nums.size();
     vector<int> dp(target + 1, 0);
     dp[0] = 1;
+    sort(nums.begin(), nums.end());
     for (int i = 1; i <= target; i++) {
-      for (int j = 0; j < size; j++) {
-	if (i >= nums[j])
-	  dp[i] += dp[i - nums[j]];
+      for (int num : nums) {
+	if (num > i)
+	  break;
+	dp[i] += dp[i - num];
       }
-    }//for
+    }
     return dp[target];
   }
 };
