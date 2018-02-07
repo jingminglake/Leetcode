c8b852824f1eeec05fa599d2e5673e56ad3adab6
jingminglake <yaxionh@39.232.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-07 16:23:29 -0500

    leetcode 322

--- coinChange/coinChange.cpp
+++ coinChange/coinChange.cpp
@@ -8,17 +8,16 @@ using namespace std;
 class Solution{
 public:
   int coinChange(vector<int>& coins, int amount) {
-    long maxAmount = amount + 1;
-    vector<long> dp(maxAmount, maxAmount);
+    vector<long> dp(amount + 1, INT_MAX);
     dp[0] = 0;
     for (int i = 1; i <= amount; i++) {
       for (int coin : coins) {
 	if (i >= coin) {
-	  dp[i] = min(dp[i], dp[i - coin] + 1);
+	  dp[i] = min (dp[i], dp[i - coin] + 1);
 	}
-      }//for
-    }//for
-    return dp[amount] == maxAmount ? -1 : dp[amount];
+      }
+    }
+    return dp[amount] >= INT_MAX ? -1 : dp[amount];
   }
 };
 
