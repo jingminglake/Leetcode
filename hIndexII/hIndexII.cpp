a54bbf77500a20151b200e29a3f24f8d3943bc25
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-02-09 14:16:18 -0500

    leetcode 275

--- hIndexII/hIndexII.cpp
+++ hIndexII/hIndexII.cpp
@@ -1,4 +1,4 @@
-// clang++ hIndex.cpp -std=c++11
+// clang++ hIndexII.cpp -std=c++11
 
 #include <iostream>
 #include <vector>
@@ -7,18 +7,16 @@ using namespace std;
 class Solution{
 public:
   int hIndex(vector<int>& citations) {
-    int left=0, len = citations.size(), right= len-1,  mid;
-    while(left<=right)
-      {
-	mid = left + (right - left) / 2;
-	if(citations[mid] == (len-mid)) 
-	  return citations[mid];
-	else if(citations[mid] > (len-mid)) 
-	  right = mid - 1;
-	else 
-	  left = mid + 1;
+    int left = 0, right = citations.size() - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (citations[mid] >= citations.size() - mid) {
+	right = mid - 1;
+      } else {
+	left = mid + 1;
       }
-    return len - (right + 1); 
+    }
+    return citations.size() - left;
   }
 };
 
