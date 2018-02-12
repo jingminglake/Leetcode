915d1592e20565793e4a1fac706306263c781dfb
jingminglake <yaxionh@25.236.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-12 16:00:27 -0500

    leetcode 80

--- removeDupFromSortedArrayII/removeDupFromSortedArrayII.cpp
+++ removeDupFromSortedArrayII/removeDupFromSortedArrayII.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
 class Solution{
 public:
   int removeDuplicates(vector<int>& nums) {
-    int size = nums.size();
-    if (size < 2)
-      return size;
-    int numOfDup = 0;
-    int current = nums[0];
-    int count = 1;
-    for (int i = 1; i < size; i++) {
-      if (nums[i] != current) {
-	count = 1;
-	current = nums[i];
-      } else if (count >= 2) {
-	numOfDup++;
+    if (nums.size() == 0)
+      return nums.size();
+    int left = 0, right = 1;
+    int count = 0;
+    while (left < nums.size() && right < nums.size()) {
+      if (nums[right] == nums[left]) {
 	count++;
-      } else if (count < 2) {
-	count++;
-      } 
-      // cout << i - numOfDup << "----" << i << endl;
-      nums[i - numOfDup] = nums[i];
+	if (count < 2) {
+	  nums[++left] = nums[right];
+	}
+      } else {
+	nums[++left] = nums[right];
+	count = 0;
+      }
+      right++;
     }
-    while (numOfDup--) {
-      nums.pop_back();
-    }
-    return nums.size();
+    return left + 1;
   }
 };
 
@@ -36,14 +29,8 @@ int main()
 {
   Solution s;
   int a[6] = {1,1,1,1,2,3};
-  vector<int> vec(a, a+3);
+  vector<int> vec(a, a+6);
   cout << s.removeDuplicates(vec);
-  cout << endl;
-  vector<int>::iterator it = vec.begin();
-  while (it != vec.end()) {
-    cout << *it << "";
-    ++it;
-  }
   cout << endl;
   return 0;
 }
