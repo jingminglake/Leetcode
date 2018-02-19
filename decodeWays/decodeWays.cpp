f33b591dd2ce9da67a4e53b0433f00f8a16d94d5
jingminglake <yaxionh@203.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-18 22:11:46 -0500

    leetcode 91

--- decodeWays/decodeWays.cpp
+++ decodeWays/decodeWays.cpp
@@ -2,35 +2,30 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int numDecodings(string s) {
-    int size = s.length();
-    if (size == 0)
-      return 0;
-    vector<int> m(size, 0);
-    m[0] = s[0] == '0' ? 0 : 1;
-    if (size == 1)
-      return m[0];
-    int oneBit = stoi(s.substr(1, 1));
-    int twoBits = stoi(s.substr(0, 2)); 
-    if (oneBit >= 1 && oneBit <= 9)
-      m[1] += m[0];
-    if (twoBits >= 10 && twoBits <= 26)
-      m[1] += 1;
-    if (size == 2)
-      return m[1];
-    for (int i = 2; i < size; i++) {
-      oneBit = stoi(s.substr(i, 1));
-      if(oneBit >= 1 && oneBit <= 9) {
-	m[i] += m[i-1];
-      }
-      twoBits = stoi(s.substr(i-1, 2));
-      if (twoBits >= 10 && twoBits <= 26)
-	m[i] += m[i-2];
+    int numDecodings(string s) {
+        if (s.length() == 0)
+            return 0;
+        vector<int> dp(s.length(), 0);
+        dp[0] = (s[0] == '0') ? 0 : 1;
+        if (s.length() == 1) {
+            return dp[0];
+        }
+        int num = stoi(s.substr(0, 2));
+        if (s[1] != '0')
+            dp[1] = dp[0];
+        if (num >= 10 && num <= 26)
+            dp[1]++;
+        for (int i = 2; i < s.size(); i++) {
+            if (s[i] != '0')
+                dp[i] = dp[i - 1];
+            int num = stoi(s.substr(i - 1, 2));
+            if (num >= 10 && num <= 26)
+                dp[i] += dp[i - 2];
+        }
+        return dp[s.length() - 1]; 
     }
-    return m[size-1];
-  }
 };
 
 int main()
