ca78a1aef3a0bf2852b5f53f5195077b6242f178
jingminglake <yaxionh@2.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-19 15:14:29 -0500

    leetcode 697

--- degreeOfanArray/degreeOfanArray.cpp
+++ degreeOfanArray/degreeOfanArray.cpp
@@ -3,30 +3,27 @@
 #include <unordered_map>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-  int findShortestSubArray(vector<int>& nums) {
-    int res = INT_MAX;
-    unordered_map<int, int> m;
-    unordered_map<int, pair<int, int> > m2;
-    int degree = 0;
-    for (int i = 0; i < nums.size(); i++) {
-      m[nums[i]]++;
-      degree = max(degree, m[nums[i]]);
-      if ( m2.count(nums[i]) ) {
-	m2[nums[i]].second = i;
-      } else {
-	m2[nums[i]] = make_pair(i, i);
-      }
+    int findShortestSubArray(vector<int>& nums) {
+        int res = INT_MAX;
+        unordered_map<int, int> m; // num -> freq
+        unordered_map<int, pair<int, int> > m2; // num -> (firstLoc, LastLoc)
+        int degree = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            degree = max (degree, ++m[nums[i]]);
+            if (m2.count(nums[i]) == 0)
+                m2[nums[i]] = make_pair(i, i);
+            else
+                m2[nums[i]].second = i;
+        }
+        for (int num : nums) {
+            if (m[num] == degree) {
+                res = min (res, m2[num].second - m2[num].first + 1);
+            }
+        }
+        return res;
     }
-    for (auto& it : m) {
-      if (it.second == degree) {
-	res = min(res, m2[it.first].second - m2[it.first].first + 1);
-      }
-    }
-    return res;
-  }
 };
 
 int main()
