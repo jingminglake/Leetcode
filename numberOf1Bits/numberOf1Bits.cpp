489a31e300070a609e61a60becee593a151db46a
jingminglake <yaxionh@2.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-19 21:28:21 -0500

    leetcode 191

--- numberOf1Bits/numberOf1Bits.cpp
+++ numberOf1Bits/numberOf1Bits.cpp
@@ -5,16 +5,13 @@ using namespace std;
 class Solution{
 public:
   int hammingWeight(uint32_t n) {
-    int ans = 0;
-    int i = 0;
-    int size = CHAR_BIT * sizeof(n);
-    while (i < size) {
+    int res = 0;
+    while (n) {
       if ((n & 1) == 1)
-	ans++;
-      n >>= 1;
-      i++;
+	res++;
+      n = n >> 1;
     }
-    return ans;
+    return res;
   }
 };
 
