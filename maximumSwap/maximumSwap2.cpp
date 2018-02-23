2a2f3d9a6e4d89b71d72682ea128ef79e26472ab
jingminglake <yaxionh@95.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-22 21:18:53 -0500

    leetcode 670

--- maximumSwap/maximumSwap2.cpp
+++ maximumSwap/maximumSwap2.cpp
@@ -10,12 +10,13 @@ public:
   int maximumSwap(int num) {
     string str = to_string(num);
     vector<int> hash(10, 0);
-    for (int i = 0; i < str.size(); i++)
+    for (int i = 0; i < str.length(); i++) {
       hash[str[i] - '0'] = i;
-    for (int i = 0; i < str.size(); i++) {
-      for (int j = hash.size() - 1; j >= str[i] - '0'; j--) {
-	if (j != str[i] - '0' && hash[j] > i) {
-	  swap(str[i], str[hash[j]]);
+    }
+    for (int i = 0; i < str.length(); i++) {
+      for (int j = 9; j > str[i] - '0'; j--) {
+	if (hash[j] > i) {
+	  swap (str[hash[j]], str[i]);
 	  return stoi(str);
 	}
       }
