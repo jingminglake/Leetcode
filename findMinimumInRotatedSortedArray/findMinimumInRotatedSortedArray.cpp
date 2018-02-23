81060673ece5febd7fc26a6d47fc0ff881029c84
jingminglake <yaxionh@139.233.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-23 18:16:34 -0500

    leetcode 153

--- findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp
+++ findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp
@@ -9,13 +9,12 @@ public:
     int left = 0, right = nums.size() - 1;
     while (left + 1 < right) {
       int mid = left + (right - left) / 2;
-      if (nums[mid] > nums[left] && nums[mid] > nums[right]) {
+      if (nums[mid] > nums[right])
 	left = mid;
-      } else {                
+      else 
 	right = mid;
-      }
     }
-    return nums[left] < nums[right] ? nums[left] : nums[right];
+    return min (nums[left], nums[right]);
   }
 };
 
