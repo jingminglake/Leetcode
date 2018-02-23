6990b357f7e7b673ff9467cd497c4da3fd298b75
jingminglake <yaxionh@139.233.21.198.tigernet.wifi.dyn.clemson.edu>
2018-02-23 12:20:49 -0500

    leetcode 92

--- reverseLinkedListII/reverseLinkedListII.cpp
+++ reverseLinkedListII/reverseLinkedListII.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
 using namespace std;
 
 struct ListNode {
---
+++ reverseLinkedListII/reverseLinkedListII_2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution{
+public:
+  ListNode* reverseBetween(ListNode* head, int m, int n) {
+    if (!head || !head->next)
+      return head;
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode *pre = &dummy;
+    ListNode *cur = head;
+    for (int i = 0; i < m - 1; i++) {
+      pre = cur;
+      cur = cur->next;
+    }
+    ListNode *newHead = nullptr;
+    ListNode *nex = nullptr;
+    for (int i = 0; i < n - m + 1; i++) {
+      nex = cur->next;
+      cur->next = newHead;
+      newHead = cur;
+      cur = nex;
+    }
+    pre->next->next = cur;
+    pre->next = newHead;
+    return dummy.next;
+  }
+};
+
+int main()
+{
+  int a[5] = {1, 2, 3, 4, 5};
+  ListNode *l = new ListNode(a[0]);
+  ListNode *tail = l;
+  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+    ListNode *temp = new ListNode(a[i]);
+    tail->next = temp;
+    tail = temp;
+  }
+  Solution s;
+  ListNode* res = s.reverseBetween(l, 2, 4);
+  while (res) {
+    cout << res->val << " ";
+    res = res->next;
+  }
+  cout << endl;
+  return 0;
+}
