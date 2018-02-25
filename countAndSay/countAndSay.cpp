ddf1fe27d02c6980c783e1a3aae074fec4e2d117
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-02-25 00:39:45 -0500

    leetcode 38

--- countAndSay/countAndSay.cpp
+++ countAndSay/countAndSay.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
   string countAndSay(int n) {
-    string res;
-    if (n == 1) {
-      res = "1";
+    string res = "";
+    if (n <= 0)
       return res;
-    }
-    string tempRes = countAndSay(n - 1);
-    char curBit = tempRes[0];
-    int count = 1;
-    for (int i = 1; i < tempRes.size(); i++) {
-      if (tempRes[i] == curBit) {
-	count++;
-      } else {
-	res += to_string(count) + curBit;
-	curBit = tempRes[i];
-	count = 1;
+    res = "1";
+    if (n == 1)
+      return res;
+    while (n > 1) {
+      string temp;
+      char c = res[0];
+      int count = 1;
+      for (int i = 1; i < res.length(); i++) {
+	if (res[i] == res[i - 1]) {
+	  count++;
+	} else {
+	  temp += to_string(count) + c;
+	  c = res[i];
+	  count = 1;
+	}
       }
+      temp += to_string(count) + c;
+      res = temp;
+      n--;
     }
-    res += to_string(count) + curBit;
     return res;
   }
 };
