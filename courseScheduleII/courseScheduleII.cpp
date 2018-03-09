33ff1e00918c3e953390fb00ed8dcc149e077a9f
jingminglake <yaxionh@27.232.21.198.tigernet.wifi.dyn.clemson.edu>
2018-03-08 22:05:00 -0500

    leetcode 210

--- courseScheduleII/courseScheduleII.cpp
+++ courseScheduleII/courseScheduleII.cpp
@@ -3,40 +3,38 @@
 #include <queue>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<int> findOrder(int numCourses, vector<pair<int, int> >& prerequisites) {
-    vector<int> res;
-    vector<vector<int> > adj(numCourses, vector<int>());
-    vector<int> indegree(numCourses, 0);
-    for (int i = 0; i < prerequisites.size(); i++) {
-      int curCourse = prerequisites[i].first;
-      int preCourse = prerequisites[i].second;
-      indegree[curCourse]++;
-      adj[preCourse].push_back(curCourse);
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<int> res;
+        if (numCourses == 0)
+            return res;
+        vector<vector<int> > m(numCourses, vector<int>());
+        vector<int> indegree(numCourses, 0);
+        for (auto& p : prerequisites) {
+            m[p.second].push_back(p.first);
+            indegree[p.first]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+        int count = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            res.push_back(node);
+            for (int neighbor : m[node]) {
+                if (--indegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+        if (count != numCourses)
+            res.clear();
+        return res;
     }
-    queue<int> q;
-    for (int i = 0; i < indegree.size(); i++) {
-      if (indegree[i] == 0)
-	q.push(i);
-    }
-    int count = 0;
-    while (!q.empty()) {
-      int course = q.front();
-      q.pop();
-      res.push_back(course);
-      count++;
-      for (int i = 0; i < adj[course].size(); i++) {
-	if (--indegree[adj[course][i]] == 0)
-	  q.push(adj[course][i]);
-      }
-    }//while
-    
-    if(count != numCourses) {
-      res.clear();
-    }
-    return res;
-  }
 };
 
 int main()
