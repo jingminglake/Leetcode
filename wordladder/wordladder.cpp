e936f4c18d75637f884ef497d4456a43ca34a4e5
jingminglake <yaxionh@27.232.21.198.tigernet.wifi.dyn.clemson.edu>
2018-03-08 21:44:55 -0500

    leetcode 127

--- wordladder/wordladder.cpp
+++ wordladder/wordladder.cpp
@@ -4,48 +4,50 @@
 #include <unordered_set>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-    int res = 0;
-    unordered_set<string> s;
-    for (string& word : wordList)
-      s.insert(word);
-    if (!bfs(beginWord, endWord, s, res)) {
-      res = 0;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int res = 0;
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (!wordSet.count(endWord))
+            return res;
+        if (!bfs(beginWord, endWord, wordSet, res))
+            return 0;
+        else
+            return res;
     }
-    return res;
-  }
-  bool bfs(string beginWord, string endWord, unordered_set<string>& s, int& res) {
-    res++;
-    unordered_set<string> start, end;
-    start.insert(beginWord);
-    end.insert(endWord);
-    while (!start.empty() && !end.empty()) {
-      if (start.size() < end.size())
-	start.swap(end);
-      unordered_set<string> temp;
-      int size = start.size();
-      res++;
-      for (string word : start) {
-	for (int m = 0; m < word.size(); m++)
-	  for (int n = 0; n < 26; n++) {
-	    string next = word;
-	    if ('a' + n == next[m])
-	      continue;
-	    next[m] = 'a' + n;
-	    if ( s.count(next) ) {
-	      if ( end.count(next) )
-		return true;
-	      temp.insert(next);
-	      s.erase( s.find(next) );
-	    }
-	  }
-      }
-      start = temp;
+    bool bfs(string beginWord, string endWord, unordered_set<string>& wordSet, int& res) {
+        unordered_set<string> start, end;
+        start.insert(beginWord);
+        end.insert(endWord);
+        res++;
+        while (!start.empty() && !end.empty()) {
+            if (start.size() > end.size())
+                start.swap(end);
+            unordered_set<string> next;
+            res++;
+            for (string word : start) {
+                for (int i = 0; i < word.length(); i++) {
+                    string nextWord = word;
+                    for (int j = 0; j < 26; j++) {
+                        if (word[i] == 'a' + j)
+                            continue;
+                        nextWord[i] = 'a' + j;
+                        if (!wordSet.count(nextWord))
+                            continue;
+                        if (end.count(nextWord)) {
+                            return true;
+                        } else {
+                            next.insert(nextWord);
+                        }
+                    }//for
+                }//for
+		wordSet.erase(word);
+            }//for
+            start = next;
+        }//while
+        return false;
     }
-    return false;
-  }
 };
 
 int main()
