b6d27bb75cbfbb9f32ae16bec18a18dac90b2534
jingminglake <yaxionh@27.239.21.198.tigernet.wifi.dyn.clemson.edu>
2018-03-11 16:06:24 -0400

    leetcode 332

---
+++ reconstructItinerary/reconstructItinerary2.cpp
@@ -0,0 +1,42 @@
+// clang++ reconstructItinerary2.cpp -std=c++11
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<pair<string, string> > tickets) {
+        vector<string> res;
+        if (tickets.size() == 0)
+            return res;
+        unordered_map<string, multiset<string> > neighbors;
+        for (pair<string, string>& ticket : tickets) {
+            neighbors[ticket.first].insert(ticket.second);
+        }
+        dfs(res, "JFK", neighbors);
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    void dfs(vector<string>& res, string start, unordered_map<string, multiset<string> >& neighbors) {
+        while (!neighbors[start].empty()) {
+            string next = *neighbors[start].begin();
+            neighbors[start].erase(neighbors[start].begin());
+            dfs(res, next, neighbors);
+        }
+        res.push_back(start);
+    }
+};
+
+int main()
+{
+  Solution s;
+  vector<pair<string, string> > tickets = {{"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"}, {"ATL", "JFK"}, {"ATL", "SFO"}};
+  vector<string> res = s.findItinerary(tickets);
+  for (string& ss : res)
+    cout << ss << " ";
+  cout << endl;
+  return 0;
+}
