20757fc8fd6a4bedd7ff7310afb21a263b98973f
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-03-14 10:40:19 -0400

    leetcode 256

--- paintHouse/paintHouse.cpp
+++ paintHouse/paintHouse.cpp
@@ -2,30 +2,21 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int minCost(vector<vector<int>>& costs) {
-    if (costs.size() == 0)
-      return 0;
-    vector<vector<int> > dp(costs.size(), vector<int>(3, INT_MAX));
-    for (int i = 0; i < costs.size(); i++) {
-      for (int j = 0; j < 3; j++) {
-	if (i == 0) {
-	  dp[0][j] = costs[0][j];
-	  continue;
-	}
-	for (int k = 0; k < 3; k++) {
-	  if (j == k)
-	    continue;
-	  dp[i][j] = min(dp[i][j], costs[i][j] + dp[i - 1][k]);
-	}
-      }
+    int minCost(vector<vector<int>>& costs) {
+        if (costs.size() == 0)
+            return 0;
+        int len = costs.size();  
+        vector<vector<int> > dp(len + 1, vector<int>(3, 0));
+        for(int i = 1; i <= len; i++)  
+        {  
+            dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i-1][0];  
+            dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i-1][1];  
+            dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i-1][2];  
+        }  
+        return min(dp[len][0], min(dp[len][1], dp[len][2]));  
     }
-    if (dp[costs.size() - 1][0] < dp[costs.size() - 1][1])
-      return min (dp[costs.size() - 1][0], dp[costs.size() - 1][2]);
-    else
-      return min (dp[costs.size() - 1][1], dp[costs.size() - 1][2]);
-  }
 };
 
 int main()
