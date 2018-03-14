93a2c91b3240787d87bcbac4462af05f6c8eeed4
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-03-14 02:21:51 -0400

    leetcode 518

---
+++ coinChangeII/coinChangeII_2.cpp
@@ -0,0 +1,28 @@
+// clang++ coinChangeII_2.cpp -std=c++11
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int> > dp(coins.size() + 1, vector<int>(amount + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.size(); i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        return dp[coins.size()][amount];
+    }
+};
+
+int main()
+{
+  Solution s;
+  vector<int> coins = {1,2,5};
+  cout << s.change(11, coins) << endl;;
+  return 0;
+}
