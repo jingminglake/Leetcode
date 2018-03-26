fce4ec4e7b14e6ac4b14cc8971a2c71d4503053c
jingminglake <yaxionh@124.239.21.198.tigernet.wifi.dyn.clemson.edu>
2018-03-26 00:03:56 -0400

    leetcode 345

--- reverseVowelsOfASrting/reverseVowelsOfAString.cpp
+++ reverseVowelsOfASrting/reverseVowelsOfAString.cpp
@@ -1,39 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  string reverseString(string s) {
-    int i = 0, j = s.length() - 1;
-    if (i == j)
-      return s;
-    char vowel[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-    unordered_set<char> set(vowel, vowel+10);
-    
-    while (i < j) {
-      if (set.find(s[i]) == set.end()) {
-	i++;
-	continue;
-      }
-      if (set.find(s[j]) == set.end()) {
-	j--;
-	continue;
-      }
-      swap(s[i], s[j]);
-      i++;
-      j--;
+    string reverseVowels(string s) {
+        int left = 0, right = s.length() - 1;
+        vector<char> vec = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        unordered_set<char> vowels(vec.begin(), vec.end());
+        while (left < right) {
+            while (left < right && !vowels.count(s[left]))
+                left++;
+            while (left < right && !vowels.count(s[right]))
+                right--;
+            swap(s[left++], s[right--]);
+        }
+        return s;
     }
-    return s;
-  }
 };
 
 int main()
 {
   Solution s;
   string str = "aA";
-  cout << s.reverseString(str) << endl;
+  cout << s.reverseVowels(str) << endl;
   return 0;
 }
