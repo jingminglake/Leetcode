b5f45cec7adf09bb6c69db16e5162161a5e39005
jingminglake <yaxionh@60.235.21.198.tigernet.wifi.dyn.clemson.edu>
2018-04-09 22:15:27 -0400

    leetcode 779

---
+++ kthSymbolInGrammar/kthSymbolInGrammar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int kthGrammar(int N, int K) {
+        if (K == 1)
+            return 0;
+        if (K % 2 == 0)
+            return kthGrammar(N - 1, K / 2) == 0 ? 1 : 0;
+        else 
+            return kthGrammar(N - 1, (K + 1) / 2) == 0 ? 0 : 1;
+    }
+};
+
+int main() {
+  Solution s;
+  cout << s.kthGrammar(5, 10) << endl;
+  return 0;
+}
