19df9b90ec6a2ffea9145f34c607c6a322238058
jingminglake <yaxionh@2.238.21.198.tigernet.wifi.dyn.clemson.edu>
2018-04-18 17:34:21 -0400

    leetcode 360

---
+++ sortTransformedArray/sortTransformedArray.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
+        int n = nums.size();
+        vector<int> res(n, 0);
+        int left = 0, right = n - 1;
+        int index = a >= 0 ? n - 1 : 0;
+        while (left <= right) {
+            if (a >= 0) {
+                if (f(nums[left], a, b, c) >= f(nums[right], a, b, c)) {
+                    res[index--] = f(nums[left++], a, b, c);
+                } else {
+                    res[index--] = f(nums[right--], a, b, c);
+                }
+            } else {
+                if (f(nums[left], a, b, c) <= f(nums[right], a, b, c)) {
+                    res[index++] = f(nums[left++], a, b, c);
+                } else {
+                    res[index++] = f(nums[right--], a, b, c);
+                }
+            }
+        }
+        return res;
+    }
+    long f(int x, int a, int b, int c) {
+        return a * x * x + b * x + c;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {-4, -2, 2, 4};
+    int a = 1, b = 3, c = 5;
+    for (int i : s.sortTransformedArray(nums, a, b, c))
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
