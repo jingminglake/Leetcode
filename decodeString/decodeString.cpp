01f9162a2f9c08d3ba8692eafdeee74948a99863
jingminglake <yaxionh@223.238.21.198.tigernet.wifi.dyn.clemson.edu>
2018-04-24 21:34:24 -0400

    leetcode 394

--- decodeString/decodeString.cpp
+++ decodeString/decodeString.cpp
@@ -3,51 +3,49 @@
 #include <stack>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  string decodeString(string s) {
-    string ans;
-    int size = s.length();
-    stack<int> kStack;
-    stack<string> encoded_stringStack;
-    int i = 0;
-    while (i < size) {
-      char c = s[i];
-      if (isalpha(c)) {
-	while (isalpha(s[i])) {
-	  ans += s[i++];
-	}
-      } else if (isdigit(c)) {
-	 int start = i;
-	 while (isdigit(s[i]))
-	   i++;
-	 int k = i > start ? stoi(s.substr(start, i - start)) : 0;
-	 kStack.push(k);
-      } else if (c == '[') {
-	encoded_stringStack.push(ans);
-	ans = "";
-	i++;
-      } else if (c == ']') {
-	string temp = encoded_stringStack.top();
-	encoded_stringStack.pop();
-	int repeatTimes =  kStack.top();
-	kStack.pop();
-	for (int j = 0; j < repeatTimes; j++) {
-	  temp += ans;
-	}
-	ans = temp;
-	i++;
-      }
+    string decodeString(string s) {
+        if (s.length() == 0)
+            return s;
+        string res;
+        stack<string> strStk;
+        stack<int> numStk;
+        int i = 0;
+        while (i < s.length()) {
+            if (s[i] == ']') {
+                string temp = strStk.top();
+                strStk.pop();
+                for (int k = 0; k < numStk.top(); k++) {
+                    temp += res;
+                }
+                res = temp;
+                numStk.pop();
+                i++;
+            } else if (isdigit(s[i])) {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i])) {
+                    n = 10 * n + s[i] - '0';
+                    i++;
+                }
+                numStk.push(n);
+            } else if (s[i] == '[') {
+                strStk.push(res);
+                res = "";
+                i++;
+            } else {
+                res += s[i++];
+            }
+        }
+        return res;
     }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string ss = "3[a2[c]]";
-  cout << s.decodeString(ss);
-  cout << endl;
-  return 0;
+    Solution s;
+    string ss = "3[a2[c]]";
+    cout << s.decodeString(ss);
+    cout << endl;
+    return 0;
 }
