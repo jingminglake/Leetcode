9e1193c68d8a7bd6473d99b3c9b4e8a78903b2ce
jingminglake <yaxionh@97.236.21.198.tigernet.wifi.dyn.clemson.edu>
2018-05-20 17:49:26 -0400

    leetcode 144

---
+++ binaryTreePreorderTraversal/binaryTreePreorderTraversal2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (!root)
+            return res;
+        stack<TreeNode*> s;
+        TreeNode* cur = root;
+        while (cur || !s.empty()) {
+            while (cur) {
+                res.push_back(cur->val);
+                s.push(cur);
+                cur = cur->left;
+            }
+            cur = s.top();
+            s.pop();
+            cur = cur->right;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+  Solution s;
+  int tree[6] = {1, 9999, 2, 9999, 9999, 3};
+  int size = sizeof(tree)/sizeof(tree[0]);
+  vector<TreeNode *> vec;
+  for (int i = 0; i < size; i++) {
+    if (tree[i] != 9999) {
+      vec.push_back(new TreeNode(tree[i]));
+    } else {
+      vec.push_back(NULL);
+    }
+  }
+  for (int i = 0; i < size/2; i++) {
+    if(!vec[i])
+      continue;
+    if (i*2 + 1 < size)
+      vec[i]->left = vec[i*2 + 1];
+    if (i*2 + 2 < size)
+      vec[i]->right = vec[i*2 + 2];
+  }
+  vector<int> res = s.preorderTraversal(vec[0]);
+  for (int i : res)
+    cout << i << " ";
+  cout << endl;
+  for (TreeNode *t : vec) {
+    delete t;
+  }
+  vec.clear();
+  return 0;
+}
