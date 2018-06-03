3d4dac5b08689c4057b3c1ea34ae85509c6b109c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-02 20:29:05 -0400

    leetcode 451

--- sortCharactersByFrequency/sortCharactersByFrequency.cpp
+++ sortCharactersByFrequency/sortCharactersByFrequency.cpp
@@ -3,36 +3,32 @@
 #include <unordered_map>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  string frequencySort(string s) {
-    string ans;
-    int size = s.length();
-    unordered_map<char, int> m;
-    for (int i = 0; i < size; i++)
-      m[s[i]]++;
-    vector<vector<int> > bucket(size+1, vector<int>(0));
-    unordered_map<char, int>::iterator it = m.begin();
-    while (it != m.end()) {
-      bucket[it->second].push_back(it->first);
-      ++it;
+    string frequencySort(string s) {
+        string res;
+        if (s.empty())
+            return res;
+        unordered_map<int, int> m;
+        for (char c : s)
+            m[c]++;
+        vector<string> bucket(s.length() + 1, "");
+        for (auto& p : m) {
+            bucket[p.second] += string(p.second, p.first);
+        }
+        for (int i = bucket.size() - 1; i >= 0; i--) {
+            if (!bucket[i].empty())
+                res += bucket[i];
+        }
+        return res;
     }
-    for (int i = size; i > 0; i--) {
-      for (int j = 0; j < bucket[i].size(); j++) {
-	for (int k = i-1; k >= 0; k--) {
-	  ans += bucket[i][j];
-	}
-      }
-    }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string ss = "tree";
-  cout << s.frequencySort(ss);
-  cout << endl;
-  return 0;
+    Solution s;
+    string ss = "tree";
+    cout << s.frequencySort(ss);
+    cout << endl;
+    return 0;
 }
