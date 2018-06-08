fd134955b343be244c027833e85044b497b8add7
jingminglake <yaxionh@97.236.21.198.tigernet.wifi.dyn.clemson.edu>
2018-06-07 22:54:32 -0400

    leetcode 526

--- beautifulArrangement/beautifulArrangement.cpp
+++ beautifulArrangement/beautifulArrangement.cpp
@@ -6,31 +6,31 @@ class Solution {
 public:
     int countArrangement(int N) {
         int res = 0;
-        vector<bool> visited(N + 1, false);
+        vector<int> visited(N + 1, false);
         dfs(N, visited, res, 1);
         return res;
     }
-    void dfs(int N, vector<bool>& visited, int& res, int index) {
-        if (index > N) {
+    void dfs (int& N, vector<int>& visited, int& res, int index) {
+        if (index - 1 == N) {
             res++;
             return;
         }
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                if (i % index == 0 || index % i == 0) {
-                    visited[i] = true;
-                    dfs(N, visited, res, index + 1);
-                    visited[i] = false;
-                }
-            }
+            if (visited[i])
+                continue;
+            if (i % index != 0 && index % i != 0)
+                continue;
+            visited[i] = true;
+            dfs (N, visited, res, index + 1);
+            visited[i] = false;
         }
     }
 };
 
 int main()
 {
-  Solution s;
-  cout << s.countArrangement(3);
-  cout << endl;
-  return 0;
+    Solution s;
+    cout << s.countArrangement(3);
+    cout << endl;
+    return 0;
 }
