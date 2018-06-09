731ba35f9006463be1683e7f37aff1b71a42fe33
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-09 19:03:44 -0400

    leetcode 490

--- theMaze/theMaze.cpp
+++ theMaze/theMaze.cpp
@@ -1,53 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-    if (maze.size() == 0)
-      return false;
-    // directions : (0, -1) -> left, (0, 1) -> right, (-1, 0) -> up, (1, 0) ->down
-    vector<vector<int>> dp(maze.size(), vector<int>(maze[0].size(), -1));
-    return dfs(maze, start[0], start[1], destination, dp);
-  }
-  bool dfs(vector<vector<int>>& maze, int i, int j, vector<int>& destination, vector<vector<int>>& dp) {
-    if (i == destination[0] && j == destination[1]) {
-      return true;
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
+        vector<pair<int, int> > dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        visited[start[0]][start[1]] = true;
+        return dfs(maze, start[0], start[1], destination, visited, dirs);
     }
-    if (dp[i][j] != -1)
-      return dp[i][j];
-    bool res = false;
-    maze[i][j] = -1;
-    vector<pair<int, int> > dirs = {make_pair(0, -1), make_pair(0, 1), make_pair(-1, 0), make_pair(1, 0)};
-    for (auto& dir : dirs) {
-      int m = i;
-      int n = j;
-      while (m >= 0 && m < maze.size() && n >= 0 && n < maze[0].size() && maze[m][n] != 1) {
-	m += dir.first;
-	n += dir.second;
-      }
-      m -= dir.first;
-      n -= dir.second;
-      if (maze[m][n] == 0)
-	res |= dfs(maze, m, n, destination, dp);
+    bool dfs (vector<vector<int> >& maze, int start_i, int start_j, vector<int>& destination, vector<vector<bool> >& visited, vector<pair<int, int> >& dirs) {
+        if (start_i == destination[0] && start_j == destination[1])
+            return true;
+        for (auto& dir : dirs) {
+            int next_i = start_i + dir.first;
+            int next_j = start_j + dir.second;
+            while (next_i >= 0 && next_i < maze.size() && next_j >= 0 && next_j < maze[0].size() && maze[next_i][next_j] != 1) {
+                next_i += dir.first;
+                next_j += dir.second;
+            }
+            next_i -= dir.first;
+            next_j -= dir.second;
+            if (next_i < 0 || next_i >= maze.size() || next_j < 0 || next_j >= maze[0].size() || maze[next_i][next_j] == 1 || visited[next_i][next_j])
+                continue;
+            visited[next_i][next_j] = true;
+            if (dfs (maze, next_i, next_j, destination, visited, dirs))
+                return true;
+        }
+        return false;
     }
-    dp[i][j] = res;
-    return dp[i][j];
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<vector<int> > maze = { {0,0,1,0,0},
-				{0,0,0,0,0},
-				{0,0,0,1,0},
-				{1,1,0,1,1},
-				{0,0,0,0,0}};
-  vector<int> start = {0, 4};
-  vector<int> dest = {3, 2};
-  cout << s.hasPath(maze, start, dest) << endl;
-  return 0;
+    Solution s;
+    vector<vector<int> > maze = { {0,0,1,0,0},
+                                  {0,0,0,0,0},
+                                  {0,0,0,1,0},
+                                  {1,1,0,1,1},
+                                  {0,0,0,0,0}};
+    vector<int> start = {0, 4};
+    vector<int> dest = {3, 2};
+    cout << s.hasPath(maze, start, dest) << endl;
+    return 0;
 }
