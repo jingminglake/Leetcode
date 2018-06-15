c0ffe7732ace39881b5d2257d3636e64d6f3a5aa
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-15 18:06:05 -0400

    leetcode 542

--- 01Matrix/01Matrix.cpp
+++ 01Matrix/01Matrix.cpp
@@ -6,19 +6,21 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        if (matrix.size() == 0)
+        if (matrix.size() == 0 || matrix[0].size() == 0)
             return matrix;
+        int m = matrix.size();
+        int n = matrix[0].size();
         queue<pair<int, int> > q;
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    q.push(make_pair(i, j));
+                    q.emplace(i, j);
                 } else {
                     matrix[i][j] = INT_MAX;
                 }
             }
         }
-        vector<vector<int> > dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        vector<pair<int, int> > dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
         int dis = 0;
         while (!q.empty()) {
             int size = q.size();
@@ -27,12 +29,12 @@ public:
                 pair<int, int> cur = q.front();
                 q.pop();
                 for (auto& dir : dirs) {
-                    int nextI = cur.first + dir[0];
-                    int nextJ = cur.second + dir[1];
-                    if (nextI < 0 || nextI >= matrix.size() || nextJ < 0 || nextJ >= matrix[0].size() || matrix[nextI][nextJ] < dis)
+                    int next_i = cur.first + dir.first;
+                    int next_j = cur.second + dir.second;
+                    if (next_i < 0 || next_i >= m || next_j < 0 || next_j >= n || matrix[next_i][next_j] != INT_MAX)
                         continue;
-                    matrix[nextI][nextJ] = dis;
-                    q.push(make_pair(nextI, nextJ));
+                    matrix[next_i][next_j] = dis;
+                    q.emplace(next_i, next_j);
                 }
             }
         }
@@ -42,22 +44,22 @@ public:
 
 int main()
 {
-  Solution s;
-  int a1[3] = {0,0,0};
-  int a2[3] = {0,1,0};
-  int a3[3] = {1,1,1};
-  vector<int> vec1(a1, a1+3);
-  vector<int> vec2(a2, a2+3);
-  vector<int> vec3(a3, a3+3);
-  vector<vector<int> > matrix;
-  matrix.push_back(vec1);
-  matrix.push_back(vec2);
-  matrix.push_back(vec3);
-  s.updateMatrix(matrix);
-  for (vector<int>& v : matrix) {
-    for (int i : v)
-      cout << i << " ";
-    cout << endl;
-  }
-  return 0;
+    Solution s;
+    int a1[3] = {0,0,0};
+    int a2[3] = {0,1,0};
+    int a3[3] = {1,1,1};
+    vector<int> vec1(a1, a1+3);
+    vector<int> vec2(a2, a2+3);
+    vector<int> vec3(a3, a3+3);
+    vector<vector<int> > matrix;
+    matrix.push_back(vec1);
+    matrix.push_back(vec2);
+    matrix.push_back(vec3);
+    s.updateMatrix(matrix);
+    for (vector<int>& v : matrix) {
+        for (int i : v)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
 }
