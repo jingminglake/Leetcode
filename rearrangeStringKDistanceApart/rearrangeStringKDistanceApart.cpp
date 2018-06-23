a254e85b1734a3621e73b8ebc26bf23006649953
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-23 19:00:24 -0400

    leetcode 358

--- rearrangeStringKDistanceApart/rearrangeStringKDistanceApart.cpp
+++ rearrangeStringKDistanceApart/rearrangeStringKDistanceApart.cpp
@@ -11,29 +11,32 @@ public:
     string rearrangeString(string s, int k) {
         if (k == 0)
             return s;
+        string res(s.length(), ' ');
         unordered_map<char, int> m;
-        for (char c : s)
+        for (char c : s) {
             m[c]++;
+        }
         priority_queue<pair<int, char> > pq;
-        for (auto& p : m)
-            pq.push({p.second, p.first});
+        for (auto& p : m) {
+            pq.emplace(p.second, p.first);
+        }
+        int index = 0;
         int len = s.length();
-        string res;
         while (!pq.empty()) {
+            if (pq.size() < min(k, len))
+                return "";
             vector<pair<int, char> > next;
-            int n = min (k, len);
-            for (int i = 0; i < n; i++) {
-                if (pq.empty())
-                    return "";
+            for (int i = 0; i < min (k, len); i++) {
                 pair<int, char> p = pq.top();
                 pq.pop();
-                res += p.second;
-                if (--p.first > 0)
-                    next.push_back(p);
+                res[index++] = p.second;
+                if (p.first - 1 > 0)
+                    next.emplace_back(p.first - 1, p.second);
                 len--;
             }
-            for (auto& p : next)
-                pq.push(p);
+            for (auto& p : next) {
+                pq.emplace(p.first, p.second);
+            }
         }
         return res;
     }
@@ -41,9 +44,9 @@ public:
 
 int main()
 {
-  Solution s;
-  string str = "aaadbbcc";
-  int k = 2;
-  cout << s.rearrangeString(str, k) << endl;;
-  return 0;
+    Solution s;
+    string str = "aaadbbcc";
+    int k = 2;
+    cout << s.rearrangeString(str, k) << endl;;
+    return 0;
 }
