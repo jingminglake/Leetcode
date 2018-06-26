64cd5e2f231cbf94edfcc039f37d4421b063653c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-25 23:54:08 -0400

    leetcode 505

--- theMazeII/theMazeII.cpp
+++ theMazeII/theMazeII.cpp
@@ -7,46 +7,48 @@ class Solution {
 public:
     class Point {
     public:
-        Point(int _x, int _y, int _l) : x(_x), y(_y), l(_l) {
-            
+        int x, y, d;
+        Point(int _x, int _y, int _d) : x(_x), y(_y), d(_d) {}
+    };
+    class Compare {
+    public:
+        bool operator()(const Point& p1, const Point& p2) const {
+            return p1.d > p2.d;
         }
-        int x;
-        int y;
-        int l;
     };
     int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
         int m = maze.size();
         if (m == 0)
-            return 0;
+            return -1;
         int n = maze[0].size();
+        if (n == 0)
+            return -1;
+        priority_queue<Point, vector<Point>, Compare> pq;
+        pq.emplace(start[0], start[1], 0);
         vector<vector<int> > dist(m, vector<int>(n, INT_MAX));
-        vector<vector<int> > dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        auto comp = [&](const Point& p1, const Point& p2) {
-            return p1.l > p2.l;
-        };
-        typedef priority_queue<Point, vector<Point>, decltype(comp) > my_pq;
-        my_pq pq(comp);
-        pq.push(Point(start[0], start[1], 0));
+        vector<pair<int, int> > dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
         while (!pq.empty()) {
             Point p = pq.top();
             pq.pop();
-            if (dist[p.x][p.y] <= p.l)
+            if (dist[p.x][p.y] <= p.d)
                 continue;
-            dist[p.x][p.y] = p.l;
-            for (int i = 0; i < dirs.size(); i++) {
-                int xx = p.x, yy = p.y, l = p.l;
-                while (xx >= 0 && xx < m && yy >= 0 && yy < n && maze[xx][yy] == 0) {
-                    xx += dirs[i][0];
-                    yy += dirs[i][1];
-                    l++;
+            dist[p.x][p.y] = p.d;
+            for (auto& dir : dirs) {
+                int next_i = p.x;
+                int next_j = p.y;
+                int next_dist = p.d;
+                while (next_i >= 0 && next_i < m && next_j >= 0 && next_j < n && maze[next_i][next_j] == 0) {
+                    next_i += dir.first;
+                    next_j += dir.second;
+                    next_dist++;
                 }
-                xx -= dirs[i][0];
-                yy -= dirs[i][1];
-                l--;
-                pq.push(Point(xx, yy, l));
+                next_i -= dir.first;
+                next_j -= dir.second;
+                next_dist--;
+                pq.emplace(next_i, next_j, next_dist);
             }
-        } // while
-        return dist[destination[0]][destination[1]] == INT_MAX ? -1 : dist[destination[0]][destination[1]]; 
+        }
+        return dist[destination[0]][destination[1]] == INT_MAX ? -1 : dist[destination[0]][destination[1]];
     }
 };
 
