07443a085575e835f230a6d840c4151581a7d084
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-29 01:11:00 -0400

    leetcode 464

--- canIWin/canIWin.cpp
+++ canIWin/canIWin.cpp
@@ -5,36 +5,33 @@
 #include <unordered_map>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool canIWin(int maxChoosableInteger, int desiredTotal) {
-    if (maxChoosableInteger >= desiredTotal)
-      return true;
-    if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal)
-      return false;
-    unordered_map<int, bool> m;
-    return canWin(maxChoosableInteger, desiredTotal, 0, m);
-  }
-  bool canWin(int length, int total, int used, unordered_map<int, bool>& m) {
-    if (m.count(used))
-      return m[used];
-    for (int i = 0; i < length; i++) {
-      int cur = (1 << i); // 2^i, length <= 20
-      if ((cur & used) == 0) { // not used
-	if (total <= i + 1 || !canWin(length, total - (i + 1), cur | used, m)) {
-	  m[used] = true;
-	  return true;
-	}
-      }
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger >= desiredTotal)
+            return true;
+        if ((maxChoosableInteger + 1) * maxChoosableInteger < 2 * desiredTotal)
+            return false;
+        unordered_map<int, bool> m;
+        return dfs(maxChoosableInteger, desiredTotal, 0, m);
     }
-    m[used] = false;
-    return false;
-  }
+    bool dfs(int& maxChoosableInteger, int desiredTotal, int used, unordered_map<int, bool>& m) {
+        if (m.count(used))
+            return m[used];
+        for (int i = maxChoosableInteger; i >= 1; i--) {
+            if ((used & (1 << i)) != 0)
+                continue;
+            int next_used = used | (1 << i);
+            if (i >= desiredTotal || !dfs(maxChoosableInteger, desiredTotal - i, next_used, m)) 
+                return m[used] = true;
+        }
+        return m[used] = false;
+    }
 };
 
 int main()
 {
-  Solution s;
-  cout << s.canIWin(10, 11) << endl;
-  return 0;
+    Solution s;
+    cout << s.canIWin(10, 11) << endl;
+    return 0;
 }
