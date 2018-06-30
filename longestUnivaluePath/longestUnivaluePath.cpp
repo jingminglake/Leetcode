b5c88b8b00f38a4efec8d6b524cc45e2a803a9a9
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-30 13:20:21 -0400

    leetcode 687

--- longestUnivaluePath/longestUnivaluePath.cpp
+++ longestUnivaluePath/longestUnivaluePath.cpp
@@ -3,61 +3,64 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+class Solution {
 public:
-  int longestUnivaluePath(TreeNode *root) {
-    int res = 0;
-    if (root)
-      dfs(root, res);
-    return res;
-  }
-  int dfs(TreeNode* node, int& res) {
-    if (!node)
-      return 0;
-    int left = dfs(node->left, res);
-    int right = dfs(node->right, res);
-    int l = 0, r = 0;
-    if (node->left && node->left->val == node->val)
-      l = left + 1;
-    if (node->right && node->right->val == node->val)
-      r = right + 1;
-    res = max(res, l + r);
-    return max(l, r);
-  }
+    int longestUnivaluePath(TreeNode* root) {
+        if (!root)
+            return 0;
+        int res = 1;
+        helper(root, res);
+        return res - 1;
+    }
+    int helper(TreeNode *root, int& res) {
+        if (!root)
+            return 0;
+        int res_left = helper(root->left, res);
+        int res_right = helper(root->right, res);
+        res = max (res, res_left);
+        res = max (res, res_right);
+        int l = 0, r = 0;
+        if (root->left && root->val == root->left->val)
+            l = res_left;
+        if (root->right && root->val == root->right->val)
+            r = res_right;
+        res = max (res, l + r + 1);
+        return max (l, r) + 1;
+    }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {5,4,5,1,1,9999,5};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[7] = {5,4,5,1,1,9999,5};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.longestUnivaluePath(vec[0]) << endl;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.longestUnivaluePath(vec[0]) << endl;
   
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
