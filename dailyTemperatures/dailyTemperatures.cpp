58229752c8f05032495f17fcc066038feafbc55e
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-06-30 18:01:03 -0400

    leetcode 739

--- dailyTemperatures/dailyTemperatures.cpp
+++ dailyTemperatures/dailyTemperatures.cpp
@@ -6,9 +6,10 @@ using namespace std;
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> res(temperatures.size(), 0);
+        int n = temperatures.size();
+        vector<int> res(n, 0);
         stack<int> s;
-        for (int i = 0; i < temperatures.size(); i++) {
+        for (int i = 0; i < n; i++) {
             while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
                 res[s.top()] = i - s.top();
                 s.pop();
@@ -20,11 +21,11 @@ public:
 };
 
 int main() {
-  Solution s;
-  vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
-  for (int i : s.dailyTemperatures(temperatures) ) {
-    cout << i << " ";
-  }
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+    for (int i : s.dailyTemperatures(temperatures) ) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
 }
