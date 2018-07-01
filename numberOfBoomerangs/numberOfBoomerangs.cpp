5a76020853a2337fa668d3b07266c8bf76f270c0
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-01 17:06:45 -0400

    leetcode 447

--- numberOfBoomerangs/numberOfBoomerangs.cpp
+++ numberOfBoomerangs/numberOfBoomerangs.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <cmath>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int numberOfBoomerangs(vector<pair<int, int> >& points) {
-    int ans = 0;
-    int size = points.size();
-    unordered_map<double, int> map;
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < size; j++) {
-	if (i == j)
-	  continue;
-	map[hypot(points[i].first - points[j].first, points[i].second - points[j].second)]++;
-      }
-      for (auto val : map)
-	if (val.second > 1)
-	  ans += val.second * (val.second - 1);
-      map.clear();
+    int numberOfBoomerangs(vector<pair<int, int>>& points) {
+        int res = 0;
+        if (points.size() < 3)
+            return res;
+        unordered_map<int64_t, int> m;
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = 0; j < points.size(); j++) {
+                if (j == i)
+                    continue;
+                int dist_x = points[j].first - points[i].first;
+                int dist_y = points[j].second - points[i].second;
+                int64_t dist = dist_x * dist_x + dist_y * dist_y;
+                m[dist]++;
+            }
+            for (auto& p : m) {
+                res += p.second * (p.second - 1);
+            }
+            m.clear();
+        }
+        return res;
     }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<pair<int, int> > vec;
-  vec.push_back(make_pair(0,0));
-  vec.push_back(make_pair(1,0));
-  vec.push_back(make_pair(2,0));
-  cout << s.numberOfBoomerangs(vec);
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<pair<int, int> > vec;
+    vec.push_back(make_pair(0,0));
+    vec.push_back(make_pair(1,0));
+    vec.push_back(make_pair(2,0));
+    cout << s.numberOfBoomerangs(vec);
+    cout << endl;
+    return 0;
 }
