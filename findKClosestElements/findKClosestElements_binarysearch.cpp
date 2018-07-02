9a675713e72dc1e604ce65e67ddeb333b5a36752
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-01 22:50:53 -0400

    leetcode 658

--- findKClosestElements/findKClosestElements_binarysearch.cpp
+++ findKClosestElements/findKClosestElements_binarysearch.cpp
@@ -6,19 +6,13 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int> res;
-        if (arr.size() == 0)
-            return res;
         int left = 0, right = arr.size() - k;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            //if (x > arr[mid]) {
-                if (x - arr[mid] > arr[mid + k] - x)
-                    left = mid + 1;
-                else
-                    right = mid;
-            //} else {
-            //    right = mid - 1;
-            //}
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
         res.assign(arr.begin() + left, arr.begin() + left + k);
         return res;
@@ -27,10 +21,10 @@ public:
 
 int main()
 {
-  Solution s;
-  vector<int> nums = {1,2,3,4,5};
-  for (int n : s.findClosestElements(nums, 4, 3))
-    cout << n << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> nums = {1,2,3,4,5};
+    for (int n : s.findClosestElements(nums, 4, 3))
+        cout << n << " ";
+    cout << endl;
+    return 0;
 }
