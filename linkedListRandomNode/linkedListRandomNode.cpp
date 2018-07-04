53ba3268c88022d26c57daf3cfb4415c9dea9c30
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-04 16:26:39 -0400

    leetcode 382

--- linkedListRandomNode/linkedListRandomNode.cpp
+++ linkedListRandomNode/linkedListRandomNode.cpp
@@ -11,23 +11,23 @@ public:
     /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
     Solution(ListNode* head) {
-        h = head;
+        head_ = head;
         srand(time(0));
     }
     
     /** Returns a random node's value. */
     int getRandom() {
-        ListNode* cur = h;
-        int res = cur->val;
-        int i = 1;
-        while (cur->next) {
-            cur = cur->next;
-            if (rand() % (++i) == 0)
+        int res = 0, len = 0;
+        ListNode* cur = head_;
+        while (cur) {
+            len++;
+            if (rand() % len == 0)
                 res = cur->val;
+            cur = cur->next;
         }
         return res;
     }
-    ListNode *h;
+    ListNode *head_;
 };
 
 /**
