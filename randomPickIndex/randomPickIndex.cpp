c0e8fc5b510cdacd36174b2a62bf6819fcb600e7
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-04 16:12:49 -0400

    leetcode 398

--- randomPickIndex/randomPickIndex.cpp
+++ randomPickIndex/randomPickIndex.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     Solution(vector<int> nums) {
-        num = nums;
+        nums_ = nums;
+        srand(time(0));
     }
     
     int pick(int target) {
         int res = -1, count = 0;
-        for (int i = 0; i < num.size(); i++) {
-            if (num[i] == target) {
+        for (int i = 0; i < nums_.size(); i++) {
+            if (nums_[i] == target) {
                 count++;
                 if (rand() % count == 0)
                     res = i;
@@ -15,5 +16,11 @@ public:
         }
         return res;
     }
-    vector<int> num;
+    vector<int> nums_;
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
