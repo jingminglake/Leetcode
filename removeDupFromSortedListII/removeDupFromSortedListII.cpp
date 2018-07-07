9faf09eeb68603df426cf9ea7adfbb5989c4f7c4
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-07 17:51:15 -0400

    leetcode 82

--- removeDupFromSortedListII/removeDupFromSortedListII.cpp
+++ removeDupFromSortedListII/removeDupFromSortedListII.cpp
@@ -1,70 +1,57 @@
 #include <iostream>
-#include <cstddef>
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-  ListNode* deleteDuplicates(ListNode* head) {
-    if (!head)
-      return head;
-    ListNode dummy(0);
-    dummy.next = head;
-    ListNode *newHead = &dummy;
-    ListNode *prev = newHead;
-    ListNode *cur = newHead->next;
-    bool isDup = false;
-    while (cur) {
-      ListNode *temp = cur->next;
-      if (!temp)
-	break;
-      while (temp && temp->val == cur->val) {
-	isDup = true;
-	temp = temp->next;
-      }
-      if (isDup) {
-	ListNode *ln = prev->next;
-	while (ln != temp) {
-	  ListNode *q = ln;
-	  ln = ln->next;
-	  delete q;
-	}
-	prev->next = temp;
-	cur = temp;
-	isDup = false;
-      } else {
-	prev = cur;
-	cur = temp;
-      }
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode *prev = &dummy;
+        while (head) {
+            while (head->next && head->val == head->next->val) {
+                ListNode *n = head;
+                head = head->next;
+                delete n;
+            }
+            if (prev->next == head) {
+                prev = prev->next;
+            } else {
+                prev->next = head->next;
+                delete head;
+            }
+            head = prev->next;
+        }
+        return dummy.next;
     }
-    return dummy.next;
-  }
 };
 
 int main()
 {
-  int a[7] = {1, 2, 3, 3, 4, 4, 5};
-  ListNode *l = new ListNode(a[0]);
-  ListNode *tail = l;
-  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
-    ListNode *temp = new ListNode(a[i]);
-    tail->next = temp;
-    tail = temp;
-  }
-  Solution s;
-  ListNode * h = s.deleteDuplicates(l);
-  while (h) {
-    ListNode *p = h;
-    h = h->next;
-    cout << "delete " << p->val << " ";
-    delete p;
-  }
-  cout << endl;
+    int a[7] = {1, 2, 3, 3, 4, 4, 5};
+    ListNode *l = new ListNode(a[0]);
+    ListNode *tail = l;
+    for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+        ListNode *temp = new ListNode(a[i]);
+        tail->next = temp;
+        tail = temp;
+    }
+    Solution s;
+    ListNode * h = s.deleteDuplicates(l);
+    while (h) {
+        ListNode *p = h;
+        h = h->next;
+        cout << "delete " << p->val << " ";
+        delete p;
+    }
+    cout << endl;
 
-  return 0;
+    return 0;
 }
