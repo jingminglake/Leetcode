30102a0dcd6228771ee216ac5064f5bd9e16fbed
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-07 16:02:36 -0400

    leetcode 206

--- reverseLinkedList/reverseLinkedList.cpp
+++ reverseLinkedList/reverseLinkedList.cpp
@@ -1,47 +1,48 @@
 #include <iostream>
-#include <cstddef>
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-  ListNode* reverseList(ListNode* head) {
-    ListNode *newHead = nullptr;
-    ListNode* nex = nullptr;
-    while (head) {
-      nex = head->next;
-      head->next = newHead;
-      newHead = head;
-      head = nex;
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode *prev = nullptr;
+        while (head) {
+            ListNode *nex = head->next;
+            head->next = prev;
+            prev = head;
+            head = nex;
+        }
+        return prev;
     }
-    return newHead;
-  }
 };
 
 int main()
 {
-  int a[5] = {0, 1, 2, 3, 4};
-  ListNode *l = new ListNode(a[0]);
-  ListNode *tail = l;
-  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
-    ListNode *temp = new ListNode(a[i]);
-    tail->next = temp;
-    tail = temp;
-  }
-  Solution s;
-  ListNode * h = s.reverseList(l);
-  while (h) {
-    ListNode *p = h;
-    h = h->next;
-    cout << "delete " << p->val << " ";
-    delete p;
-  }
-  cout << endl;
+    int a[5] = {0, 1, 2, 3, 4};
+    ListNode *l = new ListNode(a[0]);
+    ListNode *tail = l;
+    for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+        ListNode *temp = new ListNode(a[i]);
+        tail->next = temp;
+        tail = temp;
+    }
+    Solution s;
+    ListNode * h = s.reverseList(l);
+    while (h) {
+        ListNode *p = h;
+        h = h->next;
+        cout << "delete " << p->val << " ";
+        delete p;
+    }
+    cout << endl;
 
-  return 0;
+    return 0;
 }
