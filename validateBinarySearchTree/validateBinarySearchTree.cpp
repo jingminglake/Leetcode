ddaf15f7008d5893a648723d39059f8d7a605bea
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-07 19:49:49 -0400

    leetcode 98

--- validateBinarySearchTree/validateBinarySearchTree.cpp
+++ validateBinarySearchTree/validateBinarySearchTree.cpp
@@ -3,56 +3,57 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+class Solution {
 public:
-  bool isValidBST(TreeNode* root) {
-    long max = LONG_MIN;
-    return isValidHelper(root, max);
-  }
-  bool isValidHelper(TreeNode* root, long& max) {
-    if (!root)
-      return true;
-    bool leftRes = isValidHelper(root->left, max);
-    if (root->val <= max)
-      return false;
-    else
-      max = root->val;
-    bool rightRes = isValidHelper(root->right, max);
-    return leftRes && rightRes;
-  }
+    bool isValidBST(TreeNode* root) {
+        if (!root)
+            return true;
+        int64_t prev = INT64_MIN;
+        return isValidBSTHelper(root, prev);
+    }
+    bool isValidBSTHelper(TreeNode* root, int64_t& prev) {
+        if (!root)
+            return true;
+        bool leftRes = isValidBSTHelper(root->left, prev);
+        if (root->val <= prev)
+            return false;
+        prev = root->val;
+        bool rightRes = isValidBSTHelper(root->right, prev);
+        return leftRes && rightRes;
+    }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {10,5,15,9999,9999,6,20};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[7] = {10,5,15,9999,9999,6,20};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.isValidBST(vec[0]) << endl; 
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.isValidBST(vec[0]) << endl; 
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
