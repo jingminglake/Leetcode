c4a682481833c569eb98233a023c17eaa1448091
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-11 15:18:38 -0400

    leetcode 496

--- nextGreaterElementI/nextGreaterElementI.cpp
+++ nextGreaterElementI/nextGreaterElementI.cpp
@@ -4,41 +4,39 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
-    vector<int> ans;
-    stack<int> s;
-    unordered_map<int, int> m;
-    for (int num : nums) {
-      while (!s.empty() && s.top() < num) {
-	m.insert(make_pair(s.top(), num));
-	s.pop();
-      }
-      s.push(num);
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        vector<int> res;
+        unordered_map<int, int> m;
+        stack<int> s;
+        for (int n : nums) {
+            while (!s.empty() && n > s.top()) {
+                m[s.top()] = n;
+                s.pop();
+            }
+            s.push(n);
+        }
+        for (int n : findNums) {
+            if (m.count(n))
+                res.push_back(m[n]);
+            else
+                res.push_back(-1);
+        }
+        return res;
     }
-    unordered_map<int, int>::iterator it;
-    for (int num : findNums) {
-      it = m.find(num);
-      if (it != m.end())
-        ans.push_back(it->second);
-      else
-	ans.push_back(-1);
-    }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int a[3] = {4,1,2};
-  int b[4] = {1,3,4,2};
-  vector<int> vec1(a, a+3);
-  vector<int> vec2(b, b+4);
-  vector<int> res = s.nextGreaterElement(vec1, vec2);
-  for (int i : res)
-    cout << i << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    int a[3] = {4,1,2};
+    int b[4] = {1,3,4,2};
+    vector<int> vec1(a, a+3);
+    vector<int> vec2(b, b+4);
+    vector<int> res = s.nextGreaterElement(vec1, vec2);
+    for (int i : res)
+        cout << i << " ";
+    cout << endl;
+    return 0;
 }
