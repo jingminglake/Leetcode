7068417f5a283272867851fb9745d298891c0c0f
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-11 15:09:56 -0400

    leetcode 340

--- longestSubstringWithAtMostKDistinctCharaters/longestSubstringWithAtMostKDistinctCharacters.cpp
+++ longestSubstringWithAtMostKDistinctCharaters/longestSubstringWithAtMostKDistinctCharacters.cpp
@@ -6,20 +6,18 @@ class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
         int res = 0;
-        if (k == 0 || s.length() == 0)
-            return res;
-        unordered_map<char, int> m;
         int left = 0, right = 0;
+        unordered_map<char, int> m;
         while (right < s.length()) {
             m[s[right]] = right;
             right++;
             if (m.size() > k) {
-                int leftMost = s.length();
+                int left_min = s.length();
                 for (auto& p : m) {
-                    leftMost = min (leftMost, p.second);
+                    left_min = min (left_min, p.second);
                 }
-                m.erase(s[leftMost]);
-                left = leftMost + 1;
+                m.erase(s[left_min]);
+                left = left_min + 1;
             }
             res = max (res, right - left);
         }
@@ -34,4 +32,5 @@ int main() {
     int k = 2;
     cout << s.lengthOfLongestSubstringKDistinct(str1, k) << endl;
     cout << s.lengthOfLongestSubstringKDistinct(str2, k) << endl;
-    return 0;}
+    return 0;
+}
