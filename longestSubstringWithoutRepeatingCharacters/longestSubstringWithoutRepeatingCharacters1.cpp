df5df2c365edc4062589f1f61375b4ee3342e0bf
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-11 15:59:19 -0400

    leetcode 3

--- longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters1.cpp
+++ longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters1.cpp
@@ -4,32 +4,29 @@ using namespace std;
 
 class Solution {
 public:
-  int lengthOfLongestSubstring(string s) {
-    unordered_set<char> hs;
-    int maxLength = 0;
-    int n = s.length();
-    int i = 0, j = 0;
-    while (i < n && j < n) {
-      unordered_set<char>::const_iterator it = hs.find(s[j]);
-      if ( it != hs.end() ) { //find!
-	hs.erase(s[i++]);
-      }else {
-	cout << s[j] << "---1---" << endl;
-	hs.insert(s[j++]);
-	if (hs.size() > maxLength)
-	  maxLength = hs.size();
-      }
+    int lengthOfLongestSubstring(string s) {
+        int res = 0;
+        int left = 0, right = 0;
+        unordered_set<char> char_set;
+        while (right < s.length()) {
+            if (!char_set.count(s[right])) {
+                char_set.insert(s[right]);
+                right++;
+            } else {
+                char_set.erase(s[left]);
+                left++;
+            }
+            res = max (res, right - left);
+        }
+        return res;
     }
-    return maxLength;
-  }
 };
 
 
 
 int main()
 {
-  string s;
-  cin >> s;
-  Solution sl;
-  cout << sl.lengthOfLongestSubstring(s) << endl;
+    string s = "pwwkew";
+    Solution sl;
+    cout << sl.lengthOfLongestSubstring(s) << endl;
 }
