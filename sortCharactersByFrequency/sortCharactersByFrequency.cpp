856aa6e442c789d3fa068d464100081025b84ee1
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-26 13:21:27 -0400

    leetcode 451

--- sortCharactersByFrequency/sortCharactersByFrequency.cpp
+++ sortCharactersByFrequency/sortCharactersByFrequency.cpp
@@ -7,19 +7,15 @@ class Solution {
 public:
     string frequencySort(string s) {
         string res;
-        if (s.empty())
-            return res;
-        unordered_map<int, int> m;
+        unordered_map<char, int> m;
         for (char c : s)
             m[c]++;
         vector<string> bucket(s.length() + 1, "");
         for (auto& p : m) {
             bucket[p.second] += string(p.second, p.first);
         }
-        for (int i = bucket.size() - 1; i >= 0; i--) {
-            if (!bucket[i].empty())
-                res += bucket[i];
-        }
+        for (int i = bucket.size() - 1; i > 0; i--)
+            res += bucket[i];
         return res;
     }
 };
