3783d6b4eba1dad6a02003f7122cd7ea102e97b0
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-27 00:49:00 -0400

    leetcode 686

--- repeatedStringMatch/repeatedStringMatch.cpp
+++ repeatedStringMatch/repeatedStringMatch.cpp
@@ -2,30 +2,30 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-  int repeatedStringMatch(string A, string B) {
-    int res = 0;
-    string AS = "";
-    while (AS.length() < B.length()) {
-      AS += A;
-      res++;
+    int repeatedStringMatch(string A, string B) {
+        int res = 1;
+        string A_ = A;
+        while (A.length() < B.length()) {
+            A += A_;
+            res++;
+        }
+        if (A.find(B) != string::npos)
+            return res;
+        A += A_;
+        res++;
+        if (A.find(B) != string::npos)
+            return res;
+        return -1;
     }
-    if (AS.find(B) != string::npos)
-      return res;
-    AS += A;
-    if (AS.find(B) != string::npos)
-      return res + 1;
-    return -1;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string A = "abcd";
-  string B = "cdabcdab";
-  cout << s.repeatedStringMatch(A, B) << endl;
-  return 0;
+    Solution s;
+    string A = "abcd";
+    string B = "cdabcdab";
+    cout << s.repeatedStringMatch(A, B) << endl;
+    return 0;
 }
