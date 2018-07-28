476115334087b5b08688a923d693cf86bcc436bf
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-28 17:20:39 -0400

    leetcode 235

--- lowestCommonAncestorOfABinarySearchTree/lowestCommonAncestorOfABinarySearchTree.cpp
+++ lowestCommonAncestorOfABinarySearchTree/lowestCommonAncestorOfABinarySearchTree.cpp
@@ -1,59 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+class Solution {
 public:
-  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    TreeNode *ans;
-    if (!p || !q)
-      return ans;
-    ans = root;
-    int minV = min(p->val, q->val);
-    int maxV = max(p->val, q->val);
-    while (ans) {
-      if (ans->val > maxV)
-	ans = ans->left;
-      else if (ans->val < minV)
-	ans = ans->right;
-      else
-	return ans;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode *cur = root;
+        while (cur) {
+            if (cur->val > p->val && cur->val > q->val)
+                cur = cur->left;
+            else if (cur->val < p->val && cur->val < q->val)
+                cur = cur->right;
+            else
+                return cur;
+        }
+        return nullptr;
     }
-    return ans;
-  }
-  bool checkExist(vector<int>& nums, TreeNode* p, TreeNode* q){
-  }
 };
 
 int main()
 {
-  Solution s;
-  int tree[11] = {6,2,8,0,4,7,9,9999,9999,3,5};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[11] = {6,2,8,0,4,7,9,9999,9999,3,5};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.lowestCommonAncestor(vec[0], vec[1], vec[4])->val << endl;
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.lowestCommonAncestor(vec[0], vec[1], vec[4])->val << endl;
+    return 0;
 }
