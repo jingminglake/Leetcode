40fbcde9c01cf9dd5a7aa184cb7b2ad2d48d09f9
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-07-30 10:08:37 -0700

    leetcode 270

--- closestBSTValue/closestBSTValue.cpp
+++ closestBSTValue/closestBSTValue.cpp
@@ -4,51 +4,54 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+class Solution {
 public:
-  int closestValue(TreeNode *root, double target) {
-    int closest = root->val;
-    while (root) {
-      if (abs(root->val - target) < abs(closest - target)) {
-	closest = root->val;
-      }
-      root = target < root->val ? root->left : root->right;
+    int closestValue(TreeNode* root, double target) {
+        int res = root->val;
+        double closest = std::numeric_limits<double>::max();
+        TreeNode* cur = root;
+        while (cur) {
+            if (abs(cur->val - target) < closest) {
+                closest = abs(cur->val - target);
+                res = cur->val;
+            }
+            cur = cur->val > target ? cur->left : cur->right;
+        }
+        return res;
     }
-    return closest;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {10,5,15,9999,9999,6,20};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[7] = {10,5,15,9999,9999,6,20};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.closestValue(vec[0], 7) << endl; 
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.closestValue(vec[0], 7) << endl; 
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
--- closestBSTValue/closestBSTValue2.cpp
+++ closestBSTValue/closestBSTValue2.cpp
@@ -4,61 +4,60 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+class Solution {
 public:
-  int closestValue(TreeNode *root, double target) {
-    if (!root) {
-      return INT_MAX;
+    int closestValue(TreeNode* root, double target) {
+        int res = root->val;
+        double closest = std::numeric_limits<double>::max();
+        helper(root, target, res, closest);
+        return res;
     }
-    if (root->val < target) {
-      if (!root->right)
-	return root->val;
-      int valR = closestValue(root->right, target);
-      if (abs(valR - target) < abs(root->val - target))
-	return valR;
-      return root->val;
-    } else {
-      if (!root->left)
-	return root->val;
-      int valL = closestValue(root->left, target);
-      if (abs(valL - target) < abs(root->val - target))
-	return valL;
-      return root->val;
+    void helper(TreeNode* root, double& target, int& res, double& closest) {
+        if (!root)
+            return;
+        if (abs(root->val - target) < closest) {
+            closest = abs(root->val - target);
+            res = root->val;
+        }
+        if (root->val > target) {
+            helper(root->left, target, res, closest);
+        } else {
+            helper(root->right, target, res, closest);
+        }
     }
-  }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {10,5,15,9999,9999,6,20};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[7] = {10,5,15,9999,9999,6,20};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.closestValue(vec[0], 7) << endl; 
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.closestValue(vec[0], 7) << endl; 
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
