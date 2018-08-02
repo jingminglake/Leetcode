2c4c4af6284e17c937e3fdd13e1bb72c233a1cb0
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-01 17:41:25 -0700

    leetcode 78

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<vector<int> > subsets(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    vector<vector<int> > ans;
-    vector<int> subset;
-    genSubsets(nums, 0, subset, ans);
-    return ans;
-  }
-  void genSubsets(vector<int>& nums, int level, vector<int>& subset, vector<vector<int> >& ans) {
-    if (level == nums.size()) {
-      ans.push_back(subset);
-      return;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > res;
+        vector<int> path;
+        dfs(nums, path, 0, res);
+        return res;
     }
-    subset.push_back(nums[level]);
-    genSubsets(nums, level+1, subset, ans);
-    subset.pop_back();
-    genSubsets(nums, level+1, subset, ans);
-    return;
-  }
+    void dfs(vector<int>& nums, vector<int>& path, int start, vector<vector<int> >& res) {
+        if (start == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        dfs (nums, path, start + 1, res);
+        path.push_back(nums[start]);
+        dfs (nums, path, start + 1, res);
+        path.pop_back();
+    }
 };
 
 int main()
 {
-  Solution s;
-  int a[3] = {1,2,3};
-  vector<int> vec(a, a+3);
-  vector<vector<int> > res = s.subsets(vec);
-  vector<vector<int> >::iterator it = res.begin();
-  while (it != res.end()) {
-    for (int i : *it)
-      cout << i << " ";
-    cout << endl;
-    ++it;
-  }
-  return 0;
+    Solution s;
+    int a[3] = {1,2,3};
+    vector<int> vec(a, a+3);
+    vector<vector<int> > res = s.subsets(vec);
+    vector<vector<int> >::iterator it = res.begin();
+    while (it != res.end()) {
+        for (int i : *it)
+            cout << i << " ";
+        cout << endl;
+        ++it;
+    }
+    return 0;
 }
