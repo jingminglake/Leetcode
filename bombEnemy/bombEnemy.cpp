dcb0313667870fb1c8794da4bbd185e73c433674
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-02 11:17:13 -0700

    leetcode 361

--- bombEnemy/bombEnemy.cpp
+++ bombEnemy/bombEnemy.cpp
@@ -6,30 +6,30 @@ class Solution {
 public:
     int maxKilledEnemies(vector<vector<char>>& grid) {
         int res = 0;
-        if (grid.size() == 0)
+        if (grid.size() == 0 || grid[0].size() == 0)
             return res;
         int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int> > > dp(m, vector<vector<int> >(n, vector<int>(4, 0)) );
+        vector<vector<int> > dp_left(m, vector<int>(n, 0)), dp_right = dp_left, dp_up = dp_left, dp_down = dp_left;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int up = (i == 0 || grid[i][j] == 'W') ? 0 : dp[i - 1][j][0];
-                int left = (j == 0 || grid[i][j] == 'W') ? 0 : dp[i][j - 1][1];
-                dp[i][j][0] = grid[i][j] == 'E' ? up + 1: up;
-                dp[i][j][1] = grid[i][j] == 'E' ?left + 1 : left;
+                int left = (j == 0 || grid[i][j] == 'W') ? 0 : dp_left[i][j - 1];
+                int up = (i == 0 || grid[i][j] == 'W') ? 0 : dp_up[i - 1][j];
+                dp_left[i][j] = grid[i][j] == 'E' ? left + 1 : left;
+                dp_up[i][j] = grid[i][j] == 'E' ? up + 1 : up;
             }
         }
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                int down = (i == m - 1 || grid[i][j] == 'W') ? 0 : dp[i + 1][j][2];
-                int right = (j == n - 1 || grid[i][j] == 'W') ? 0 : dp[i][j + 1][3];
-                dp[i][j][2] = grid[i][j] == 'E' ? down + 1: down;
-                dp[i][j][3] = grid[i][j] == 'E' ? right + 1 : right;
+                int right = (j == n - 1 || grid[i][j] == 'W') ? 0 : dp_right[i][j + 1];
+                int down = (i == m - 1 || grid[i][j] == 'W') ? 0 : dp_down[i + 1][j];
+                dp_right[i][j] = grid[i][j] == 'E' ? right + 1 : right;
+                dp_down[i][j] = grid[i][j] == 'E' ? down + 1 : down;
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '0') {
-                    res = max (res, dp[i][j][0] + dp[i][j][1] + dp[i][j][2] + dp[i][j][3]);
+                    res = max (res, dp_left[i][j] + dp_up[i][j] + dp_right[i][j] + dp_down[i][j]);
                 }
             }
         }
