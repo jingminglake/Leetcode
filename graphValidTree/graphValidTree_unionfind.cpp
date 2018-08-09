d17fcdff9ec14a18edb214d55cd0177a5bae5692
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-08 18:00:08 -0700

    leetcode 261

--- graphValidTree/graphValidTree_unionfind.cpp
+++ graphValidTree/graphValidTree_unionfind.cpp
@@ -1,42 +1,40 @@
- #include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
-        for (int i = 0; i < n; i++) {
-            father.push_back(i);
-        }
-        for (pair<int, int>& edge : edges) {
-            if (find(edge.first) == find(edge.second))
+        for (int i = 0; i < n; i++)
+            parent.push_back(i);
+        count = n;
+        for (auto& e : edges) {
+            int p1 = find_op(e.first);
+            int p2 = find_op(e.second);
+            if (p1 != p2) {
+                parent[p1] = p2;
+                count--;
+            } else 
                 return false;
-            else
-                union_op(edge.first, edge.second);
         }
-        return n == edges.size() + 1;
+        return count == 1;
     }
-    int find(int x) {
-        if (father[x] == x)
-            return x;
-        return father[x] = find(father[x]);
+    int find_op(int node) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = find_op(parent[node]);
     }
-    void union_op(int x, int y) {
-        int root_x = find(x);
-        int root_y = find(y);
-        if (root_x != root_y)
-            father[root_x] = root_y;
-    }
-    vector<int> father;
+    int count;
+    vector<int> parent;
 };
 
 int main()
 {
-  Solution s;
-  vector<pair<int, int> > edges = {{0,1},
-				{0,2},
-				{0,3},
-				{1,4}};
-  cout <<  s.validTree(5, edges) << endl;
-  return 0;
+    Solution s;
+    vector<pair<int, int> > edges = {{0,1},
+                                     {0,2},
+                                     {0,3},
+                                     {1,4}};
+    cout <<  s.validTree(5, edges) << endl;
+    return 0;
 }
