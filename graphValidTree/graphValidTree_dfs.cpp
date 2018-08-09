3742d4f38f9c94596b419fb5f650161eacf95b46
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-08 17:46:39 -0700

    leetcode 261

--- graphValidTree/graphValidTree_dfs.cpp
+++ graphValidTree/graphValidTree_dfs.cpp
@@ -1,4 +1,4 @@
- #include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -7,39 +7,41 @@ public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
         vector<vector<int> > m(n, vector<int>());
         vector<bool> visited(n, false);
-        for (auto edge : edges) {
+        for (auto& edge : edges) {
             m[edge.first].push_back(edge.second);
             m[edge.second].push_back(edge.first);
         }
-        if (!dfs(0, -1, m, visited))
+        // check cycle
+        visited[0] = true;
+        if (dfs(0, -1, m, visited))
             return false;
-        for (auto v : visited) {
+        for (bool v : visited) {
             if (!v)
                 return false;
         }
         return true;
     }
-    bool dfs(int node, int preNode, vector<vector<int> >& m, vector<bool>& visited) {
-        if (visited[node])
-            return false;
-        visited[node] = true;
-        for (auto neighbor : m[node]) {
-            if (neighbor != preNode) {
-                if (!dfs(neighbor, node, m, visited))
-                    return false;
-            }
+    bool dfs(int node, int parent, vector<vector<int> >& m, vector<bool>& visited) {
+        for (int neighbor : m[node]) {
+            if (neighbor == parent)
+                continue;
+            if (visited[neighbor])
+                return true;
+            visited[neighbor] = true;
+            if (dfs(neighbor, node, m, visited))
+                return true;
         }
-        return true;
+        return false;
     }
 };
 
 int main()
 {
-  Solution s;
-  vector<pair<int, int> > edges = {{0,1},
-				{0,2},
-				{0,3},
-				{1,4}};
-  cout <<  s.validTree(5, edges) << endl;
-  return 0;
+    Solution s;
+    vector<pair<int, int> > edges = {{0,1},
+                                     {0,2},
+                                     {0,3},
+                                     {1,4}};
+    cout <<  s.validTree(5, edges) << endl;
+    return 0;
 }
