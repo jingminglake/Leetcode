026a59eea6ac8be2dde989cf78df60079b021c00
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-08 17:12:41 -0700

    leetcode 305

--- numberOfIslandsII/numberOfIslands_2.cpp
+++ numberOfIslandsII/numberOfIslands_2.cpp
@@ -7,39 +7,40 @@ class Solution {
 public:
     int count;
     unordered_map<int, int> parent;
-    int find_op(int index) {
-        if (!parent.count(index))
-            return parent[index] = index;
-        else if (parent[index] == index)
-            return index;
+    int find_op(int node) {
+        if (!parent.count(node))
+            return parent[node] = node;
+        else if (parent[node] == node)
+            return node;
         else
-            return parent[index] = find_op(parent[index]);
+            return parent[node] = find_op(parent[node]);
     }
-    void union_op(int index_1, int index_2) {
-        int parent_1 = find_op(index_1);
-        int parent_2 = find_op(index_2);
-        if (parent_1 != parent_2) {
-            parent[parent_1] = parent_2;
+    void union_op(int node1, int node2) {
+        int p1 = find_op(node1);
+        int p2 = find_op(node2);
+        if (p1 != p2) {
+            parent[p1] = p2;
             count--;
         }
     }
     vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
         vector<int> res;
-        if (m < 0 || n < 0)
+        if (m <= 0 || n <= 0)
             return res;
-        count = 0;
         vector<pair<int, int> > dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (auto& pos : positions) {
-            int index_1 = n * pos.first + pos.second;
-            parent[index_1] = index_1;
+        for (auto& p : positions) {
+            int node = p.first * n + p.second;
+            parent[node] = node;
             count++;
-            for (auto& dir : dirs) {
-                int x = pos.first + dir.first;
-                int y = pos.second + dir.second;
-                int index_2 = n * x + y;
-                if (x < 0 || x >= m || y < 0 || y >= n || !parent.count(index_2))
+            for (auto& d : dirs) {
+                int neighbor_x = p.first + d.first;
+                int neighbor_y = p.second + d.second;
+                if (neighbor_x < 0 || neighbor_x >= m || neighbor_y < 0 || neighbor_y >= n)
                     continue;
-                union_op(index_1, index_2);
+                int neighbor = neighbor_x * n + neighbor_y;
+                if (!parent.count(neighbor))
+                    continue;
+                union_op(node, neighbor);
             }
             res.push_back(count);
         }
@@ -49,11 +50,11 @@ public:
 
 int main()
 {
-  Solution s;
-  int m = 3, n = 3;
-  vector<pair<int, int> > positions = {{0,0}, {0,1}, {1,2}, {2,1}};
-  for (int i : s.numIslands2(m, n, positions))
-      cout << i << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    int m = 3, n = 3;
+    vector<pair<int, int> > positions = {{0,0}, {0,1}, {1,2}, {2,1}};
+    for (int i : s.numIslands2(m, n, positions))
+        cout << i << " ";
+    cout << endl;
+    return 0;
 }
