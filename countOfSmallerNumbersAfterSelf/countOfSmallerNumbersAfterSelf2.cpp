501915d829d3e8f92c1e6aaf9f9fe75066a61cfb
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-11 20:37:13 -0700

    leetcode 315

--- countOfSmallerNumbersAfterSelf/countOfSmallerNumbersAfterSelf2.cpp
+++ countOfSmallerNumbersAfterSelf/countOfSmallerNumbersAfterSelf2.cpp
@@ -5,11 +5,13 @@ using namespace std;
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        vector<int> res(nums.size()), temp;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            int d = distance(temp.begin(), lower_bound(temp.begin(), temp.end(), nums[i]));
-            res[i] = d;
-            temp.insert(temp.begin() + d, nums[i]);
+        vector<int> res(nums.size());
+        vector<int> temp;
+        for(int i = nums.size()-1; i >= 0; i--)
+        {
+            auto it = lower_bound(temp.begin(), temp.end(), nums[i]);
+            res[i] = it - temp.begin();
+            temp.insert(it, nums[i]);
         }
         return res;
     }
