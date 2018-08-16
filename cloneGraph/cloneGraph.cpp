fdc4bcb313af81a51236c7a088ea3d0f77b94924
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-16 15:33:52 -0700

    leetcode 133

--- cloneGraph/cloneGraph.cpp
+++ cloneGraph/cloneGraph.cpp
@@ -5,53 +5,53 @@
 using namespace std;
 
 struct UndirectedGraphNode {
-  int label;
-  vector<UndirectedGraphNode *> neighbors;
-  UndirectedGraphNode(int x) : label(x) {};
+    int label;
+    vector<UndirectedGraphNode *> neighbors;
+    UndirectedGraphNode(int x) : label(x) {};
 };
 
 class Solution{
 public:
-  UndirectedGraphNode *cloneGraph(struct UndirectedGraphNode *node) {
-    if (!node)
-      return nullptr;
-    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> m;
-    // clone nodes
-    queue<UndirectedGraphNode*> q;
-    q.push(node);
-    while (!q.empty()) {
-      UndirectedGraphNode *temp = q.front();
-      UndirectedGraphNode *cloneNode = new UndirectedGraphNode(temp->label);
-      m.insert(make_pair(temp, cloneNode));
-      q.pop();
-      for (UndirectedGraphNode* neighborNode : temp->neighbors) {
-	if (m.find(neighborNode) == m.end())
-	  q.push(neighborNode);
-      }
+    UndirectedGraphNode *cloneGraph(struct UndirectedGraphNode *node) {
+        if (!node)
+            return nullptr;
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> m;
+        // clone nodes
+        queue<UndirectedGraphNode*> q;
+        q.push(node);
+        while (!q.empty()) {
+            UndirectedGraphNode *temp = q.front();
+            UndirectedGraphNode *cloneNode = new UndirectedGraphNode(temp->label);
+            m.insert(make_pair(temp, cloneNode));
+            q.pop();
+            for (UndirectedGraphNode* neighborNode : temp->neighbors) {
+                if (m.find(neighborNode) == m.end())
+                    q.push(neighborNode);
+            }
+        }
+        // clone edges
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode*>::iterator it = m.begin();
+        while (it != m.end()) {
+            for (UndirectedGraphNode* n: it->first->neighbors) {
+                it->second->neighbors.push_back(m[n]);
+            }
+            ++it;
+        }
+        return m[node];
     }
-    // clone edges
-    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*>::iterator it = m.begin();
-    while (it != m.end()) {
-      for (UndirectedGraphNode* n: it->first->neighbors) {
-	it->second->neighbors.push_back(m[n]);
-      }
-      ++it;
-    }
-    return m[node];
-  }
 };
 
 int main()
 {
-  Solution s;
-  struct UndirectedGraphNode n0(0), n1(1), n2(2);
-  n0.neighbors.push_back(&n1);
-  n0.neighbors.push_back(&n2);
-  n1.neighbors.push_back(&n0);
-  n1.neighbors.push_back(&n2);
-  n2.neighbors.push_back(&n1);
-  n2.neighbors.push_back(&n0);
-  n2.neighbors.push_back(&n2);
-  UndirectedGraphNode *res = s.cloneGraph(&n0);
-  return 0;
+    Solution s;
+    struct UndirectedGraphNode n0(0), n1(1), n2(2);
+    n0.neighbors.push_back(&n1);
+    n0.neighbors.push_back(&n2);
+    n1.neighbors.push_back(&n0);
+    n1.neighbors.push_back(&n2);
+    n2.neighbors.push_back(&n1);
+    n2.neighbors.push_back(&n0);
+    n2.neighbors.push_back(&n2);
+    UndirectedGraphNode *res = s.cloneGraph(&n0);
+    return 0;
 }
--- cloneGraph/cloneGraph2.cpp
+++ cloneGraph/cloneGraph2.cpp
@@ -4,47 +4,45 @@
 using namespace std;
 
 struct UndirectedGraphNode {
-  int label;
-  vector<UndirectedGraphNode *> neighbors;
-  UndirectedGraphNode(int x) : label(x) {};
+    int label;
+    vector<UndirectedGraphNode *> neighbors;
+    UndirectedGraphNode(int x) : label(x) {};
 };
 
-class Solution{
+class Solution {
 public:
-  UndirectedGraphNode *cloneGraph(struct UndirectedGraphNode *node) {
-    if (!node)
-      return node;
-    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> m;
-    cloneGraphHelper(node, m);
-    for (auto& p : m) {
-      for (UndirectedGraphNode* n : p.first->neighbors) {
-	p.second->neighbors.push_back(m[n]);
-      }
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if (!node)
+            return node;
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode* > m;
+        dfs(node, m);
+        for (auto& p : m) {
+            for (auto& n : p.first->neighbors)
+                p.second->neighbors.push_back(m[n]);
+        }
+        return m[node];
     }
-    return m[node];
-  }
-  void cloneGraphHelper(UndirectedGraphNode *node, unordered_map<UndirectedGraphNode*, UndirectedGraphNode*>& m) {
-    UndirectedGraphNode *newNode = new UndirectedGraphNode(node->label);
-    m[node] = newNode;
-    for (UndirectedGraphNode* neighor_node : node->neighbors) {
-      if (m.count(neighor_node))
-	continue;
-      cloneGraphHelper(neighor_node, m);
+    void dfs(UndirectedGraphNode* node, unordered_map<UndirectedGraphNode*, UndirectedGraphNode* >& m) {
+        UndirectedGraphNode* node_c = new UndirectedGraphNode(node->label);
+        m[node] = node_c;
+        for (auto& n : node->neighbors) {
+            if (!m.count(n))
+                dfs (n, m);
+        }
     }
-  }
 };
 
 int main()
 {
-  Solution s;
-  struct UndirectedGraphNode n0(0), n1(1), n2(2);
-  n0.neighbors.push_back(&n1);
-  n0.neighbors.push_back(&n2);
-  n1.neighbors.push_back(&n0);
-  n1.neighbors.push_back(&n2);
-  n2.neighbors.push_back(&n1);
-  n2.neighbors.push_back(&n0);
-  n2.neighbors.push_back(&n2);
-  UndirectedGraphNode *res = s.cloneGraph(&n0);
-  return 0;
+    Solution s;
+    struct UndirectedGraphNode n0(0), n1(1), n2(2);
+    n0.neighbors.push_back(&n1);
+    n0.neighbors.push_back(&n2);
+    n1.neighbors.push_back(&n0);
+    n1.neighbors.push_back(&n2);
+    n2.neighbors.push_back(&n1);
+    n2.neighbors.push_back(&n0);
+    n2.neighbors.push_back(&n2);
+    UndirectedGraphNode *res = s.cloneGraph(&n0);
+    return 0;
 }
