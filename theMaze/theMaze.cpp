3f9a1cff87949c55f8ef52cb402eb38e842168df
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-16 21:24:30 -0700

    leetcode 490

--- theMaze/theMaze.cpp
+++ theMaze/theMaze.cpp
@@ -5,30 +5,36 @@ using namespace std;
 class Solution {
 public:
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        if (start == destination)
+            return true;
         vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
-        vector<pair<int, int> > dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        vector<pair<int, int> > dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         visited[start[0]][start[1]] = true;
-        return dfs(maze, start[0], start[1], destination, visited, dirs);
+        return dfs(maze, start[0], start[1], destination, dirs, visited);
     }
-    bool dfs (vector<vector<int> >& maze, int start_i, int start_j, vector<int>& destination, vector<vector<bool> >& visited, vector<pair<int, int> >& dirs) {
-        if (start_i == destination[0] && start_j == destination[1])
+    bool dfs(vector<vector<int> >& maze, int i, int j, vector<int>& destination, vector<pair<int, int> >& dirs, vector<vector<bool> >& visited) {
+        if (i == destination[0] && j == destination[1])
             return true;
         for (auto& dir : dirs) {
-            int next_i = start_i + dir.first;
-            int next_j = start_j + dir.second;
-            while (next_i >= 0 && next_i < maze.size() && next_j >= 0 && next_j < maze[0].size() && maze[next_i][next_j] != 1) {
+            int next_i = i + dir.first;
+            int next_j = j + dir.second;
+            while (isValid(maze, next_i, next_j)) {
                 next_i += dir.first;
                 next_j += dir.second;
             }
             next_i -= dir.first;
             next_j -= dir.second;
-            if (next_i < 0 || next_i >= maze.size() || next_j < 0 || next_j >= maze[0].size() || maze[next_i][next_j] == 1 || visited[next_i][next_j])
+            if (!isValid(maze, next_i, next_j) || visited[next_i][next_j])
                 continue;
             visited[next_i][next_j] = true;
-            if (dfs (maze, next_i, next_j, destination, visited, dirs))
+            if (dfs(maze, next_i, next_j, destination, dirs, visited)) {
                 return true;
+            }
         }
         return false;
+    }
+    bool isValid(vector<vector<int> >& maze, int i, int j) {
+        return i >= 0 && i < maze.size() && j >= 0 && j < maze[0].size() && maze[i][j] == 0;
     }
 };
 
