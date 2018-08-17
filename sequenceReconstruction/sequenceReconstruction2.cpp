a6f723c0be91e7d2dc2dc6e8565a6a7e09a4dba4
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-16 23:53:45 -0700

    leetcode 444

--- sequenceReconstruction/sequenceReconstruction2.cpp
+++ sequenceReconstruction/sequenceReconstruction2.cpp
@@ -1,47 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
     bool sequenceReconstruction(vector<int>& org, vector<vector<int>>& seqs) {
-        unordered_map<int, vector<int> > neighbors;
-        unordered_map<int, int> indegree;
-        for (vector<int>& seq : seqs) {
+        int n = org.size();
+        vector<vector<int> > neighbors(n + 1);
+        unordered_set<int> nodes(org.begin(), org.end()), nodes_2 = nodes;
+        vector<int> indegree(n + 1, 0);
+        for (auto& seq : seqs) {
             for (int i = 0; i < seq.size(); i++) {
-                if (!indegree.count(seq[i]))
-                    indegree[seq[i]] = 0;
-                if (i > 0) {
+                if (!nodes.count(seq[i]))
+                    return false;
+                nodes_2.erase(seq[i]);
+                if (i != 0) {
                     neighbors[seq[i - 1]].push_back(seq[i]);
                     indegree[seq[i]]++;
                 }
             }
         }
-        if (org.size() != indegree.size()) {
+        if (nodes_2.size())
             return false;
-        }
         queue<int> q;
-        for (auto& p : indegree) {
-            if (p.second == 0) {
-                q.push(p.first);
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
+        if (q.size() != 1)
+            return false;
         int index = 0;
         while (!q.empty()) {
-            if (q.size() > 1)
+            if (q.size() != 1)
                 return false;
-            int cur = q.front();
+            int node = q.front();
             q.pop();
-            if (org[index++] != cur)
+            if (org[index++] != node)
                 return false;
-            for (int neighbor : neighbors[cur]) {
-                if (--indegree[neighbor] == 0)
+            for (int neighbor : neighbors[node]) {
+                if (--indegree[neighbor] == 0) {
                     q.push(neighbor);
+                }
             }
         }
-        return index == org.size();
+        return index == n; 
     }
 };
 
