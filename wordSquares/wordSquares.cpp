b77e9230cffe931c85ce3cbb862a47036677042d
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-16 17:33:51 -0700

    leetcode 425

--- wordSquares/wordSquares.cpp
+++ wordSquares/wordSquares.cpp
@@ -2,39 +2,29 @@
 #include <vector>
 using namespace std;
 
+class TrieNode {
+public:
+    TrieNode() {
+        memset(next, 0, sizeof(next));
+    }
+    ~TrieNode() {
+        for (TrieNode* t : next)
+            delete t;
+    }
+    TrieNode* next[26];
+    vector<int> word_indexes;
+};
 class Solution {
 public:
-    class TrieNode {
-    public:
-        TrieNode(){
-            memset(next, 0, sizeof(next));
-        }
-        ~TrieNode() {
-            for (int i = 0; i < 26; i++)
-                delete next[i];
-        }
-        TrieNode *next[26];
-        vector<string> startWith;
-    };
-    void addWord(string& word) {
-        TrieNode *cur = root;
-        for (char c : word) {
-            if (!cur->next[c - 'a'])
-                cur->next[c - 'a'] = new TrieNode();
-            cur->next[c - 'a']->startWith.push_back(word);
-            cur = cur->next[c - 'a'];
-        }
-    }
-    vector<vector<string> > wordSquares(vector<string>& words) {
+    vector<vector<string>> wordSquares(vector<string>& words) {
         vector<vector<string> > res;
         if (words.size() == 0)
             return res;
         root = new TrieNode();
-        for (string& word : words) {
-            addWord(word);
-        }
+        for (int i = 0; i < words.size(); i++)
+            add(words, i);
         vector<string> path;
-        for (string& word : words) {  // first word do not have pre word, so add first
+        for (string& word : words) {
             path.push_back(word);
             dfs(1, words, path, res);
             path.pop_back();
@@ -42,30 +32,40 @@ public:
         delete root;
         return res;
     }
-    vector<string> getStartWith(string& prefix) {
-        TrieNode *cur = root;
-        for (char c : prefix) {
-            if (!cur->next[c - 'a'])
-                return vector<string>();
-            cur = cur->next[c - 'a'];
-        }
-        return cur->startWith;
-    }
     void dfs(int i, vector<string>& words, vector<string>& path, vector<vector<string> >& res) {
         if (i == words[0].size()) {
             res.push_back(path);
             return;
         }
         string prefix;
-        for (int k = 0; k < i; k++)
-            prefix += path[k][i];
-        for (string& word : getStartWith(prefix)) {
-            path.push_back(word);
-            dfs(i + 1, words, path, res);
+        for (int j = 0; j < i; j++) {
+            prefix += path[j][i];
+        }
+        for (int word_index : getWordIndexes(prefix)) {
+            path.push_back(words[word_index]);
+            dfs (i + 1, words, path, res);
             path.pop_back();
         }
     }
     TrieNode *root;
+    void add(vector<string>& words, int index) {
+        TrieNode* cur = root;
+        for (char c : words[index]) {
+            cur->word_indexes.push_back(index);
+            if (!cur->next[c - 'a'])
+                cur->next[c - 'a'] = new TrieNode();
+            cur = cur->next[c - 'a'];
+        }
+    }
+    vector<int> getWordIndexes(string& prefix) {
+        TrieNode *cur = root;
+        for (char c : prefix) {
+            if (!cur->next[c - 'a'])
+                return vector<int>();
+            cur = cur->next[c - 'a'];
+        }
+        return cur->word_indexes;
+    }
 };
 
 int main() {
