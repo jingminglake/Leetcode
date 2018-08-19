ab2bb76aa7ba3cba52f3a0015f4b771a2714e856
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-19 15:56:53 -0700

    leetcode 417

--- pacificAtlanticWaterFlow/pacificAtlanticWaterFlow.cpp
+++ pacificAtlanticWaterFlow/pacificAtlanticWaterFlow.cpp
@@ -7,47 +7,42 @@ class Solution {
 public:
     vector<pair<int, int>> pacificAtlantic(vector<vector<int>>& matrix) {
         vector<pair<int, int> > res;
-        int m = matrix.size();
-        if (m == 0)
+        if (matrix.size() == 0 || matrix[0].size() == 0)
             return res;
-        int n = matrix[0].size();
-        vector<vector<bool> > pacificCanFlow(m, vector<bool>(n, false));
-        vector<vector<bool> > atlanticCanFlow(m, vector<bool>(n, false));
-        queue<pair<int, int> > q1, q2;
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<bool> > pacific_can_flow(m, vector<bool>(n, false));
+        vector<vector<bool> > atlantic_can_flow(m, vector<bool>(n, false));
+        queue<pair<int, int> > q_pacific, q_atlantic;
         for (int i = 0; i < m; i++) {
-            q1.push({i, 0});
-            q2.push({i, n - 1});
-            pacificCanFlow[i][0] = true;
-            atlanticCanFlow[i][n - 1] = true;
+            q_pacific.emplace(i, 0);
+            q_atlantic.emplace(i, n - 1);
         }
-        for (int j = 0; j < n; j++) {
-            q1.push({0, j});
-            q2.push({m - 1, j});
-            pacificCanFlow[0][j] = true;
-            atlanticCanFlow[m - 1][j] = true;
+        for (int i = 0; i < n; i++) {
+            q_pacific.emplace(0, i);
+            q_atlantic.emplace(m - 1, i);
         }
-        bfs (matrix, q1, pacificCanFlow);
-        bfs (matrix, q2, atlanticCanFlow);
+        vector<pair<int, int> > dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        bfs(q_pacific, dirs, pacific_can_flow, matrix);
+        bfs(q_atlantic, dirs, atlantic_can_flow, matrix);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (pacificCanFlow[i][j] && atlanticCanFlow[i][j])
+                if (pacific_can_flow[i][j] && atlantic_can_flow[i][j])
                     res.push_back({i, j});
             }
         }
         return res;
     }
-    void bfs(vector<vector<int>>& matrix, queue<pair<int, int> >& q, vector<vector<bool> >& canFlow) {
-        vector<pair<int, int> > dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    void bfs(queue<pair<int, int> >& q, vector<pair<int, int> >& dirs, vector<vector<bool> >& can_flow, vector<vector<int>>& matrix) {
         while (!q.empty()) {
             pair<int, int> p = q.front();
             q.pop();
+            can_flow[p.first][p.second] = true;
             for (auto& dir : dirs) {
-                int r = p.first + dir.first;
-                int c = p.second + dir.second;
-                if (r < 0 || r >= matrix.size() || c < 0 || c >= matrix[0].size() || canFlow[r][c] || matrix[r][c] < matrix[p.first][p.second])
+                int next_i = p.first + dir.first;
+                int next_j = p.second + dir.second;
+                if (next_i < 0 || next_i >= matrix.size() || next_j < 0 || next_j >= matrix[0].size() || matrix[next_i][next_j] < matrix[p.first][p.second] || can_flow[next_i][next_j])
                     continue;
-                canFlow[r][c] = true;
-                q.push({r, c});
+                q.emplace(next_i, next_j);
             }
         }
     }
@@ -55,26 +50,26 @@ public:
 
 int main()
 {
-  Solution s;
-  int a1[5] = {1, 2, 2, 3, 5};
-  int a2[5] = {3, 2, 3, 4, 4};
-  int a3[5] = {2, 4, 5, 3, 1};
-  int a4[5] = {6, 7, 1, 4, 5};
-  int a5[5] = {5, 1, 1, 2, 4};
-  vector<int> vec1(a1, a1+5);
-  vector<int> vec2(a2, a2+5);
-  vector<int> vec3(a3, a3+5);
-  vector<int> vec4(a4, a4+5);
-  vector<int> vec5(a5, a5+5);
-  vector<vector<int> > matrix;
-  matrix.push_back(vec1);
-  matrix.push_back(vec2);
-  matrix.push_back(vec3);
-  matrix.push_back(vec4);
-  matrix.push_back(vec5);
-  vector<pair<int,int> > res = s.pacificAtlantic(matrix);
-  for (pair<int,int>& p : res)
-    cout << "(" << p.first << "," << p.second << ")";
-  cout << endl;
-  return 0;
+    Solution s;
+    int a1[5] = {1, 2, 2, 3, 5};
+    int a2[5] = {3, 2, 3, 4, 4};
+    int a3[5] = {2, 4, 5, 3, 1};
+    int a4[5] = {6, 7, 1, 4, 5};
+    int a5[5] = {5, 1, 1, 2, 4};
+    vector<int> vec1(a1, a1+5);
+    vector<int> vec2(a2, a2+5);
+    vector<int> vec3(a3, a3+5);
+    vector<int> vec4(a4, a4+5);
+    vector<int> vec5(a5, a5+5);
+    vector<vector<int> > matrix;
+    matrix.push_back(vec1);
+    matrix.push_back(vec2);
+    matrix.push_back(vec3);
+    matrix.push_back(vec4);
+    matrix.push_back(vec5);
+    vector<pair<int,int> > res = s.pacificAtlantic(matrix);
+    for (pair<int,int>& p : res)
+        cout << "(" << p.first << "," << p.second << ")";
+    cout << endl;
+    return 0;
 }
