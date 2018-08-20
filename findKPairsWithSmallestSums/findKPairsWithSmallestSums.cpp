441f518a90386c431ca7ee4c9b0a3ca1f14ba14a
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-20 14:42:12 -0700

    leetcode 373

--- findKPairsWithSmallestSums/findKPairsWithSmallestSums.cpp
+++ findKPairsWithSmallestSums/findKPairsWithSmallestSums.cpp
@@ -5,48 +5,45 @@
 #include <queue>
 using namespace std;
 
-class Compare {
+class Solution {
 public:
-  bool operator()(const tuple<int, int, int>& p1, const tuple<int, int, int>& p2) {
-    return get<0>(p1) + get<1>(p1) > get<0>(p2) + get<1>(p2);
-  }
-};
-
-class Solution{
-public:
-  vector<pair<int, int> > kthSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int> >, Compare > pq;
-    vector<pair<int, int> > ans;
-    int nums1Size = nums1.size();
-    int nums2Size = nums2.size();
-    if (nums1Size == 0 || nums2Size == 0 || k == 0)
-      return ans;
-    for (int i = 0; i < nums1Size && i < k; i++) {
-      pq.push(make_tuple(nums1[i], nums2[0], 0));
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int, int> > res;
+        if (nums1.size() == 0 || nums2.size() == 0 || k == 0)
+            return res;
+        auto comp = [&] (const tuple<int, int, int64_t>& t1, const tuple<int, int, int64_t>& t2) {
+            return get<2>(t1) > get<2>(t2);
+        };
+        typedef priority_queue<tuple<int, int, int64_t>, vector<tuple<int, int, int64_t> >, decltype(comp) > my_pq;
+        my_pq pq(comp);
+        for (int i = 0; i < nums2.size(); i++) {
+            pq.emplace(0, i, nums1[0] + nums2[i]);
+        }
+        for (int i = 0; i < k; i++) {
+            if (!pq.empty()) {
+                tuple<int, int, int64_t> t = pq.top();
+                pq.pop();
+                int x = get<0>(t), y = get<1>(t);
+                res.emplace_back(nums1[x], nums2[y]);
+                if (x + 1 < nums1.size())
+                    pq.emplace(x + 1, y, nums1[x + 1] + nums2[y]);
+            }
+        }
+        return res;
     }
-    while (k-- > 0 && !pq.empty()) {
-      tuple<int, int, int> t = pq.top();
-      pq.pop();
-      ans.push_back(make_pair(get<0>(t), get<1>(t)));
-      if (get<2>(t) == nums2Size - 1)
-	continue;
-      pq.push(make_tuple(get<0>(t), nums2[get<2>(t) + 1], get<2>(t) + 1));
-    }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> nums1 = {1,1,2};
-  vector<int> nums2 = {1,2,3};
-  vector<vector<int> > matrix;
-  matrix.push_back(nums1);
-  matrix.push_back(nums2);
-  vector<pair<int,int> > res = s.kthSmallestPairs(nums1, nums2, 3);
-  for (pair<int, int>& p : res)
-    cout << "(" << p.first << ", " << p.second << ")";
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> nums1 = {1,1,2};
+    vector<int> nums2 = {1,2,3};
+    vector<vector<int> > matrix;
+    matrix.push_back(nums1);
+    matrix.push_back(nums2);
+    vector<pair<int,int> > res = s.kSmallestPairs(nums1, nums2, 3);
+    for (pair<int, int>& p : res)
+        cout << "(" << p.first << ", " << p.second << ")";
+    cout << endl;
+    return 0;
 }
