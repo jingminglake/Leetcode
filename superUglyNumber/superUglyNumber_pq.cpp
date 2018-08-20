9e088337f86034e0e40d6ccee601f043553cc333
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-20 16:33:25 -0700

    leetcode 313

--- superUglyNumber/superUglyNumber_pq.cpp
+++ superUglyNumber/superUglyNumber_pq.cpp
@@ -8,36 +8,33 @@ using namespace std;
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        vector<long> uglys(n);
-        int pSize = primes.size();
-        uglys[0] = 1;
-        auto Comp = [&](const tuple<long, int, int>& p1, const tuple<long, int, int>& p2) {
-            return get<0>(p1) > get<0>(p2);
+        vector<int64_t> res(n, 1);
+        auto comp = [&](const tuple<int, int, int64_t>& t1, const tuple<int, int, int64_t>& t2) {
+            return get<2>(t1) > get<2>(t2);
         };
-        typedef priority_queue<tuple<long, int, int>, vector<tuple<long, int, int> >, decltype(Comp) > my_pq;
-        my_pq pq(Comp);
-        for (int i = 0; i < pSize; i++) {
-            pq.push(make_tuple(primes[i], 0, primes[i]));
-        }
+        typedef priority_queue<tuple<int, int, int64_t>, vector<tuple<int, int, int64_t> >, decltype(comp) > my_pq;
+        my_pq pq(comp);
+        for (int prime : primes)
+            pq.emplace(prime, 0, prime);
+        int64_t cur_min = 1;
         for (int i = 1; i < n; i++) {
-            long minUgly = get<0>(pq.top());
-            uglys[i] = minUgly;
-            while (!pq.empty() && get<0>(pq.top()) == minUgly) {
-                int p = get<1>(pq.top()) + 1;
-                int prime = get<2>(pq.top());
-                pq.push(make_tuple(uglys[p] * prime, p, prime));
+            cur_min = res[i] = get<2>(pq.top());
+            while (!pq.empty() && get<2>(pq.top()) == cur_min) {
+                int prime = get<0>(pq.top());
+                int index = get<1>(pq.top());
+                pq.emplace(prime, index + 1, prime * res[index + 1]);
                 pq.pop();
             }
         }
-        return uglys[n - 1];
+        return res[n - 1];
     }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> primes = {2,7,13,19};
-  cout << s.nthSuperUglyNumber(12, primes);
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> primes = {2,7,13,19};
+    cout << s.nthSuperUglyNumber(12, primes);
+    cout << endl;
+    return 0;
 }
