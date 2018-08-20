04d89ed1e40b3194f295bf116c922348bf196b70
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-20 14:16:42 -0700

    leetcode 378

--- kthSmallestElementInASortedMatrix/kthSmallestElementInASortedMatrix_1.cpp
+++ kthSmallestElementInASortedMatrix/kthSmallestElementInASortedMatrix_1.cpp
@@ -5,48 +5,45 @@
 #include <queue>
 using namespace std;
 
-class Compare {
-public:
-    bool operator()(const pair<int, int>& p1, const pair<int, int>& p2) const {
-        return p1.first > p2.first;
-    }
-};
-
 class Solution {
 public:
+    class Compare {
+    public:
+        bool operator()(tuple<int, int, int>& t1, tuple<int, int, int>& t2) const{
+            return get<0>(t1) > get<0>(t2);
+        }
+    };
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int res = 0;
         if (matrix.size() == 0 || matrix[0].size() == 0)
             return 0;
-        int m = matrix.size();
-        int n = matrix[0].size();
-        priority_queue<pair<int, int>, vector<pair<int, int> >, Compare > pq;
-        for (int i = 0; i < matrix.size(); i++)
-            pq.emplace(matrix[i][0], i * n);
-        for (int index = 0; index < k; index++) {
-            pair<int, int> p = pq.top();
+        int n = matrix.size();
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int> >, Compare > pq;
+        for (int i = 0; i < n; i++)
+            pq.emplace(matrix[i][0], i, 0);
+        for (int i = 0; i < k - 1; i++) {
+            tuple<int, int, int> p = pq.top();
             pq.pop();
-            res = p.first;
-            int i = p.second / n;
-            int j = p.second % n;
-            if (j + 1 < n)
-                pq.emplace(matrix[i][j + 1], i * n + j + 1);
+            int x = get<1>(p), y = get<2>(p);
+            if (y + 1 < n) {
+                pq.emplace(matrix[x][y + 1], x, y + 1);
+            }
         }
-        return res;
+        return get<0>(pq.top());
     }
+    
 };
 
 int main()
 {
-  Solution s;
-  vector<int> row1 = {1,5,9};
-  vector<int> row2 = {10,11,13};
-  vector<int> row3 = {12,13,15};
-  vector<vector<int> > matrix;
-  matrix.push_back(row1);
-  matrix.push_back(row2);
-  matrix.push_back(row3);
-  cout << s.kthSmallest(matrix, 8);
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> row1 = {1,5,9};
+    vector<int> row2 = {10,11,13};
+    vector<int> row3 = {12,13,15};
+    vector<vector<int> > matrix;
+    matrix.push_back(row1);
+    matrix.push_back(row2);
+    matrix.push_back(row3);
+    cout << s.kthSmallest(matrix, 8);
+    cout << endl;
+    return 0;
 }
