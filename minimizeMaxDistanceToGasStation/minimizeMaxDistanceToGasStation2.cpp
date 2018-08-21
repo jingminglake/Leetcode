9ff45dc8c4efab534f45fa4861d6c2af1be9d417
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-20 17:13:27 -0700

    leetcode 774

--- minimizeMaxDistanceToGasStation/minimizeMaxDistanceToGasStation2.cpp
+++ minimizeMaxDistanceToGasStation/minimizeMaxDistanceToGasStation2.cpp
@@ -2,38 +2,39 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 class Solution {
 public:
     double minmaxGasDist(vector<int>& stations, int K) {
-        if (stations.size() < 2)
-            return 0;
-        double left = 0, right = 1e8;
+        double left = 0, right = 0;
+        for (int i = 0; i < stations.size() - 1; i++)
+            right = max (right, (double)stations[i + 1] - stations[i]);
         while (right - left > 1e-6) {
             double mid = left + (right - left) / 2.0;
-            if (canSplit(mid, stations, K))
+            if (splitByMaxLen(stations, mid) <= K)
                 right = mid;
             else
                 left = mid;
         }
         return left;
     }
-    bool canSplit(double maxLen, vector<int>& stations, int K) {
-        int num = 0;
+    int splitByMaxLen(vector<int>& stations, double maxLen) {
+        int res = 0;
         for (int i = 0; i < stations.size() - 1; i++) {
-            num += (stations[i + 1] - stations[i]) / maxLen;
+            int gap = stations[i + 1] - stations[i];
+            if (gap > maxLen)
+                res += int (gap / maxLen);
         }
-        return num <= K;
+        return res;
     }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> stations = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int k = 9;
-  cout << s.minmaxGasDist(stations, k) << endl;;
-  return 0;
+    Solution s;
+    vector<int> stations = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int k = 9;
+    cout << s.minmaxGasDist(stations, k) << endl;;
+    return 0;
 }
