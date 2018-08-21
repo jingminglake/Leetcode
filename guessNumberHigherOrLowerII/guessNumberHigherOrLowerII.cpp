099fd991e52ce18b63641e3e2fd19500ca5e30e4
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-21 16:39:17 -0700

    leetcode 375

--- guessNumberHigherOrLowerII/guessNumberHigherOrLowerII.cpp
+++ guessNumberHigherOrLowerII/guessNumberHigherOrLowerII.cpp
@@ -2,36 +2,34 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 
 class Solution {
 public:
-  int getMoneyAmount(int n) {
-    vector<vector<int> > hash(n + 1, vector<int>(n + 1, 0));
-    return getMoneyAmountHelper(hash, 1, n);
-  }
-  int getMoneyAmountHelper(vector<vector<int> > &hash, int start, int end) {
-    if (start >= end)
-      return 0;
-    if (hash[start][end] != 0)
-      return hash[start][end];
-    int res = INT_MAX;
-    for (int x = start; x <= end; x++) {
-      res = min (res, x + max(getMoneyAmountHelper(hash, start, x - 1), getMoneyAmountHelper(hash, x + 1, end)));
+    int getMoneyAmount(int n) {
+        vector<vector<int> > dp(n + 1, vector<int>(n + 1, -1));
+        return helper(1, n, dp);
     }
-    hash[start][end] = res;
-    return res;
-  }
+    int helper(int start, int end, vector<vector<int> >& dp) {
+        if (start >= end)
+            return 0;
+        if (dp[start][end] != -1)
+            return dp[start][end];
+        int res = INT_MAX;
+        for (int i = start; i <= end; i++) {
+            res = min (res, max(helper(start, i - 1, dp), helper(i + 1, end, dp)) + i);
+        }
+        return dp[start][end] = res;
+    }
 };
 
 int main()
 {
-  Solution s;
-  vector<vector<int> > pairs = {{1, 2},
-				{2, 3},
-                                {3, 4}};
-  cout << s.getMoneyAmount(10) << endl;
-  return 0;
+    Solution s;
+    vector<vector<int> > pairs = {{1, 2},
+                                  {2, 3},
+                                  {3, 4}};
+    cout << s.getMoneyAmount(10) << endl;
+    return 0;
 }
