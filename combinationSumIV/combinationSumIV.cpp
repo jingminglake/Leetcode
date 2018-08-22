823bd0729f45c83dd61706ce57085a706607a90c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-22 14:38:15 -0700

    leetcode 377

--- combinationSumIV/combinationSumIV.cpp
+++ combinationSumIV/combinationSumIV.cpp
@@ -2,30 +2,27 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-  int combinationSum4(vector<int>& nums, int target) {
-    vector<int> dp(target + 1, 0);
-    dp[0] = 1;
-    sort(nums.begin(), nums.end());
-    for (int i = 1; i <= target; i++) {
-      for (int num : nums) {
-	if (num > i)
-	  break;
-	dp[i] += dp[i - num];
-      }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (i >= num)
+                    dp[i] += dp[i - num];
+            }
+        }
+        return dp[target];
     }
-    return dp[target];
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> nums = {1, 2, 3};
-  cout << s.combinationSum4(nums, 4) << endl;
-  return 0;
+    Solution s;
+    vector<int> nums = {1, 2, 3};
+    cout << s.combinationSum4(nums, 4) << endl;
+    return 0;
 }
