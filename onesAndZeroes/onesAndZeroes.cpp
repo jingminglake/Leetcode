aec52ce989127702d58a40b3cc29892178b39f85
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-22 11:11:14 -0700

    leetcode 474

--- onesAndZeroes/onesAndZeroes.cpp
+++ onesAndZeroes/onesAndZeroes.cpp
@@ -2,36 +2,34 @@
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int findMaxForm(vector<string>& strs, int m, int n) {
-    vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
-    for (string &str : strs) {
-      int zeroes = 0, ones = 0;
-      for (char &c : str) {
-	if (c == '0')
-	  zeroes++;
-        else if (c == '1')
-	  ones++;
-      }
-      for (int i = m; i >= zeroes; i--) {
-	for (int j = n; j >= ones; j--) {
-	  dp[i][j] = max(dp[i][j], dp[i - zeroes][j - ones] + 1);
-	}//for
-      }//for
-    }//for
-    return dp[m][n];
-  }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
+        for (string& str : strs) {
+            int num_of_zeroes = 0, num_of_ones = 0;
+            for (char c : str) {
+                if (c == '0')
+                    num_of_zeroes++;
+                else if (c == '1')
+                    num_of_ones++;
+            }
+            for (int i = m; i >= num_of_zeroes; i--) {
+                for (int j = n; j >= num_of_ones; j--) {
+                    dp[i][j] = max (dp[i][j], dp[i - num_of_zeroes][j - num_of_ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 };
 
 int main()
 {
-  Solution s;
-  vector<string> arr = {"10", "0001", "111001", "1", "0"};
-  cout << s.findMaxForm(arr, 5, 3) << endl;
-  return 0;
+    Solution s;
+    vector<string> arr = {"10", "0001", "111001", "1", "0"};
+    cout << s.findMaxForm(arr, 5, 3) << endl;
+    return 0;
 }
