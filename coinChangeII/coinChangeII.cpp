9fdf787084be6457528e038879b8e0addf99f9c6
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-22 14:22:54 -0700

    leetcode 518

--- coinChangeII/coinChangeII.cpp
+++ coinChangeII/coinChangeII.cpp
@@ -2,28 +2,27 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int coinChange(vector<int>& coins, int amount) {
-    vector<int> dp(amount + 1, 0);
-    dp[0] = 1;
-    for (int coin : coins) {
-      for (int i = 1; i <= amount; i++) {
-	if (i >= coin)
-	  dp[i] += dp[i - coin];
-      }
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = 0; j <= amount; j++) {
+                if (j >= coin)
+                    dp[j] += dp[j - coin];
+            }
+        }
+        return dp[amount];
     }
-    return dp[amount];
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> coins = {1,2,5};
-  cout << s.coinChange(coins, 11) << endl;;
-  return 0;
+    Solution s;
+    vector<int> coins = {1,2,5};
+    cout << s.change(11, coins) << endl;;
+    return 0;
 }
