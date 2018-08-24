eb7f36b7df0980e7376cc45ea689c6bb205e2ca4
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-24 15:01:26 -0700

    leetcode 402

--- removeKDigits/removeKDigits.cpp
+++ removeKDigits/removeKDigits.cpp
@@ -6,26 +6,28 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         string res;
-        int resLen = num.size() - k;
+        int n = num.length(), drop = k;
         for (char c : num) {
-            while (k && res.length() && res.back() > c) {
+            while (!res.empty() && (res.back() > c) && drop) {
                 res.pop_back();
-                k--;
+                drop--;
             }
             res.push_back(c);
         }
-        res.resize(resLen);
-        res.erase(0, res.find_first_not_of('0'));
+        res.resize(n - k);
+        int pos = res.find_first_not_of('0');
+        res.erase(0, pos);
+        //res = res.substr(pos);
         if (res.empty())
-            res = "0";
+            return "0";
         return res;
     }
 };
 
 int main() {
-  Solution s;
-  string num = "10200";
-  int k = 1;
-  cout << s.removeKdigits(num, k) << endl;
-  return 0;
+    Solution s;
+    string num = "10200";
+    int k = 1;
+    cout << s.removeKdigits(num, k) << endl;
+    return 0;
 }
