d125d893ee43d4eb66a6868d353d6f90119a4bb8
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-24 14:17:41 -0700

    leetcode 522

--- longestUncommonSubsequenceII/longestUncommonSubsequenceII.cpp
+++ longestUncommonSubsequenceII/longestUncommonSubsequenceII.cpp
@@ -1,54 +1,59 @@
-// clang++ splitArrayIntoConsecutiveSubsequences2.cpp -std=c++11
+// clang++ longestUncommonSubsequenceII.cpp -std=c++11
 
 #include <iostream>
 #include <vector>
 #include <unordered_set>
 using namespace std;
 
-class Comp {
-public:
-    bool operator()(const string& s1, const string& s2) const {
-        if (s1.length() == s2.length())
-            return s1 > s2;
-        return s1.length() > s2.length();
-    }
-};
 class Solution {
 public:
+    class Compare {
+    public:
+        bool operator()(const string& s1, const string& s2) const {
+            return s1.length() > s2.length() || (s1.length() == s2.length() && s1 < s2);
+        }
+    };
     int findLUSlength(vector<string>& strs) {
-        sort(strs.begin(), strs.end(), Comp());
+        sort(strs.begin(), strs.end(), Compare());
         unordered_set<string> s;
         for (int i = 0; i < strs.size(); i++) {
+            if (i + 1 < strs.size() && strs[i] == strs[i + 1]) {
+                s.insert(strs[i]);
+                continue;
+            }
             if (s.count(strs[i]))
                 continue;
-            bool isSub = false;
-            if (i == strs.size() - 1 || strs[i] != strs[i + 1]) {
-                for (string str : s) {
-                    int j = 0;
-                    for (char c : str) {
-                        if (c == strs[i][j])
-                            j++;
-                        if (j == strs[i].size())
-                            break;
-                    }
-                    if (j == strs[i].size()) {
-                        isSub = true;
-                        break;
-                    }
-                }
-                if (!isSub)
-                    return strs[i].size();
-            }
+            if (!isSubseqInSet(strs[i], s))
+                return strs[i].size();
             s.insert(strs[i]);
-        } // for
+        }
         return -1;
     }
+    bool isSubseqInSet(string& str, unordered_set<string>& s) {
+        for (string prev : s) {
+            if (isSubseq(prev, str))
+                return true;
+        }
+        return false;
+    }
+    bool isSubseq(string& str, string& pattern) {
+        int p1 = 0, p2 = 0;
+        while (p1 < str.length() && p2 < pattern.length()) {
+            if (str[p1] == pattern[p2]) {
+                p1++; p2++;
+            } else {
+                p1++;
+            }
+        }
+        return p2 == pattern.length();
+    }
 };
+
 int main()
 {
-  Solution s;
-  vector<string> strs = {"aba", "cdc", "eae"};
-  cout << s.findLUSlength(strs);
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<string> strs = {"aba", "cdc", "eae"};
+    cout << s.findLUSlength(strs);
+    cout << endl;
+    return 0;
 }
