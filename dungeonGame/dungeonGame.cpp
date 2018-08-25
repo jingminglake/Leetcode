a918d3c36b3ab91736cc0c21b8f96af223c4b80c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-25 15:11:58 -0700

    leetcode 174

--- dungeonGame/dungeonGame.cpp
+++ dungeonGame/dungeonGame.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int calculateMinimumHP(vector<vector<int>>& dungeon) {
-    int M = dungeon.size();
-    int N = dungeon[0].size();
-    // hp[i][j] represents the min hp needed at position (i, j)
-    // Add dummy row and column at bottom and right side
-    vector<vector<int> > hp(M + 1, vector<int>(N + 1, INT_MAX));
-    hp[M][N - 1] = 1;
-    hp[M - 1][N] = 1;
-    for (int i = M - 1; i >= 0; i--) {
-      for (int j = N - 1; j >= 0; j--) {
-	int need = min(hp[i + 1][j], hp[i][j + 1]) - dungeon[i][j];
-	hp[i][j] = need <= 0 ? 1 : need;
-      }
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int> > dp(m + 1, vector<int>(n + 1, INT_MAX));
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1)
+                    dp[i][j] = dungeon[i][j] > 0 ? 1 : -dungeon[i][j] + 1;
+                else {
+                    int initial = -dungeon[i][j] + min(dp[i + 1][j], dp[i][j + 1]);
+                    dp[i][j] = initial <= 0 ? 1 : initial;
+                }
+            }
+        }
+        return dp[0][0];
     }
-    return hp[0][0];
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<vector<int> > dungeon = { {-2,-3, 3},
-				   {-5,-10,1},
-				   {10,30,-5}};
-  cout << s.calculateMinimumHP(dungeon) << endl;
-  return 0;
+    Solution s;
+    vector<vector<int> > dungeon = { {-2,-3, 3},
+                                     {-5,-10,1},
+                                     {10,30,-5}};
+    cout << s.calculateMinimumHP(dungeon) << endl;
+    return 0;
 }
