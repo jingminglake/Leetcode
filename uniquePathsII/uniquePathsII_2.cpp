75c91d581ff73a07ec597ebc521f26965b073144
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-24 20:26:03 -0700

    leetcode 63

--- uniquePathsII/uniquePathsII_2.cpp
+++ uniquePathsII/uniquePathsII_2.cpp
@@ -13,16 +13,18 @@ public:
         int n = obstacleGrid[0].size();
         if (n == 0 || obstacleGrid[0][0] == 1)
             return 0;
-        vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
+        vector<int> dp(n + 1, 0);
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == 1 && j == 1)
-                    dp[i][j] = 1;
+                    dp[j] = 1;
                 else if (obstacleGrid[i - 1][j - 1] == 0)
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    dp[j] += dp[j - 1];
+                else
+                    dp[j] = 0;
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
 
