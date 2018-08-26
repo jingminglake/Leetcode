43fd00e4b1ee0cc475fe6c5de6b2e3e3cc8faf37
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-25 17:26:00 -0700

    leetcode 480

--- slidingWindowMedian/slidingWindowMedian.cpp
+++ slidingWindowMedian/slidingWindowMedian.cpp
@@ -13,36 +13,33 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             if (i >= k) { // if i >= k then remove element at first
                 if (leftS.count(nums[i - k])) {
-                  leftS.erase(leftS.find(nums[i - k]));
+                    leftS.erase(leftS.find(nums[i - k]));
                 } else if (rightS.count(nums[i - k])){
-                  rightS.erase(rightS.find(nums[i - k]));
+                    rightS.erase(rightS.find(nums[i - k]));
                 }
             }
             // adjust
-            if (!leftS.empty() && (leftS.size() - 2 == rightS.size())) { // ex. 6,4 => 5,5
+            if (leftS.size() - 2 == rightS.size()) { // ex. 6,4 => 5,5
                 rightS.insert(*leftS.rbegin());
                 leftS.erase(--leftS.end());
+            } else if (leftS.size() + 1 == rightS.size()) {
+                leftS.insert(*rightS.begin());
+                rightS.erase(rightS.begin());
             }
             // add element
-            if (leftS.size() <= rightS.size()) {
-                if (leftS.empty() || nums[i] <= *leftS.rbegin())
-                  leftS.insert(nums[i]);
-                else {
-                  rightS.insert(nums[i]);
-                  leftS.insert(*rightS.begin());
-                  rightS.erase(rightS.begin());
-                }
-            } else {
-                leftS.insert(nums[i]);
-                rightS.insert(*leftS.rbegin());
-                leftS.erase(--leftS.end());
+            leftS.insert(nums[i]);
+            rightS.insert(*leftS.rbegin());
+            leftS.erase(--leftS.end());
+            if (leftS.size() < rightS.size()) {
+                leftS.insert(*rightS.begin());
+                rightS.erase(rightS.begin());
             }
             // get median
             if (i >= k - 1) {
                 if (k % 2)
-                  res.push_back(*leftS.rbegin());
+                    res.push_back(*leftS.rbegin());
                 else
-                  res.push_back((*leftS.rbegin() + *rightS.begin()) / 2.0);
+                    res.push_back((*leftS.rbegin() + *rightS.begin()) / 2.0);
             }
         }
         return res;
@@ -51,11 +48,11 @@ public:
 
 int main()
 {
-  Solution s;
-  vector<int> nums = {1,3,-1,-3,5,3,6,7};
-  vector<double> vec = s.medianSlidingWindow(nums, 3);
-  for (double d : vec)
-    cout << d << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    vector<double> vec = s.medianSlidingWindow(nums, 3);
+    for (double d : vec)
+        cout << d << " ";
+    cout << endl;
+    return 0;
 }
