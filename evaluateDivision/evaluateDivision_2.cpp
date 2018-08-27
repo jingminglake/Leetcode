a8b650d64713e2b763d64145964ed21b5fedbd43
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-27 12:05:21 -0700

    leetcode 399

--- evaluateDivision/evaluateDivision_2.cpp
+++ evaluateDivision/evaluateDivision_2.cpp
@@ -7,37 +7,36 @@ using namespace std;
 class Solution {
 public:
     vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
-        unordered_map<string, vector<pair<string, double> > > edges;
-        for (int i = 0; i < equations.size(); i++) {
-            edges[equations[i].first].emplace_back(equations[i].second, values[i]);
-            edges[equations[i].second].emplace_back(equations[i].first, 1 / values[i]);
-        }
         vector<double> res(queries.size(), -1.0);
+        unordered_map<string, vector<pair<string, double> > > neighbors;
+        for (int i = 0; i < equations.size(); i++) {
+            neighbors[equations[i].first].emplace_back(equations[i].second, values[i]);
+            neighbors[equations[i].second].emplace_back(equations[i].first, 1.0 / values[i]);
+        }
         for (int i = 0; i < queries.size(); i++) {
-            if (!edges.count(queries[i].first) || !edges.count(queries[i].second)) {
+            if (!neighbors.count(queries[i].first) || !neighbors.count(queries[i].second))
                 continue;
-            }
+            double equation_res = 1.0;
             unordered_set<string> visited;
-            double curValue = 1.0;
             visited.insert(queries[i].first);
-            if (dfs(queries[i].first, queries[i].second, visited, edges, curValue))
-                res[i] = curValue;
-            visited.erase(queries[i].first);
+            if (dfs(queries[i].first, queries[i].second, neighbors, equation_res, visited)) {
+                res[i] = equation_res;
+            }
         }
         return res;
     }
-    bool dfs(string start, string& end, unordered_set<string>& visited, unordered_map<string, vector<pair<string, double> > >& edges, double& curValue) {
+    bool dfs(string start, string& end, unordered_map<string, vector<pair<string, double> > >& neighbors, double& res, unordered_set<string>& visited) {
         if (start == end)
             return true;
-        for (int i = 0; i < edges[start].size(); i++) {
-            if (!visited.count(edges[start][i].first)) {
-                visited.insert(edges[start][i].first);
-                curValue *= edges[start][i].second;
-                if (dfs(edges[start][i].first, end, visited, edges, curValue))
-                    return true;
-                visited.erase(edges[start][i].first);
-                curValue /= edges[start][i].second;
-            }
+        for (auto& n : neighbors[start]) {
+            if (visited.count(n.first))
+                continue;
+            res *= n.second;
+            visited.insert(n.first);
+            if (dfs(n.first, end, neighbors, res, visited))
+                return true;
+            res /= n.second;
+            visited.erase(n.first);
         }
         return false;
     }
