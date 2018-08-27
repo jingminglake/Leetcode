521b9f51cd8e801dadce06c4adf10315b9e47e06
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-27 15:31:18 -0700

    leetcode 239

--- slidingWindowMaximum/slidingWindowMaximum.cpp
+++ slidingWindowMaximum/slidingWindowMaximum.cpp
@@ -3,36 +3,37 @@
 #include <iostream>
 #include <vector>
 #include <deque>
-#include <algorithm>
 using namespace std;
 
 
 class Solution {
 public:
-  vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    int size = nums.size();
-    vector<int> window;
-    deque<int> dq;
-    for (int i = 0; i < size; i++) {
-      while (!dq.empty() && dq.front() < i - k + 1) // i - k + 1 is the start point of window
-	dq.pop_front();
-      while (!dq.empty() && nums[dq.back()] < nums[i])
-	dq.pop_back();
-      dq.push_back(i);
-      if (i >= k - 1) // from k + 1, we start count the max
-	window.push_back(nums[dq.front()]);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n == 0)
+            return vector<int>();
+        vector<int> res(n - k + 1);
+        deque<int> dq;
+        for (int i = 0; i < n; i++) {
+            while (!dq.empty() && nums[i] > nums[dq.back()])
+                dq.pop_back();
+            while (!dq.empty() && dq.front() < i - k + 1)
+                dq.pop_front();
+            dq.push_back(i);
+            if (i >= k - 1)
+                res[i - k + 1] = nums[dq.front()];
+        }
+        return res;
     }
-    return window;
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<int>  nums = {1,3,-1,-3,5,3,6,7};
-  vector<int> res = s.maxSlidingWindow(nums, 3);
-  for (int i : res)
-    cout << i << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int>  nums = {1,3,-1,-3,5,3,6,7};
+    vector<int> res = s.maxSlidingWindow(nums, 3);
+    for (int i : res)
+        cout << i << " ";
+    cout << endl;
+    return 0;
 }
