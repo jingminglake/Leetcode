6daa5676e52eb48afbecb8e97d46f5cf9137ee46
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-27 14:31:10 -0700

    leetcode 480

--- slidingWindowMedian/slidingWindowMedian.cpp
+++ slidingWindowMedian/slidingWindowMedian.cpp
@@ -8,42 +8,50 @@ using namespace std;
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> res;
-        multiset<double> leftS, rightS;
+        int n = nums.size();
+        vector<double> res(n - k + 1);
         for (int i = 0; i < nums.size(); i++) {
-            if (i >= k) { // if i >= k then remove element at first
-                if (leftS.count(nums[i - k])) {
+            if (i >= k) {
+                // remove at first
+                if (leftS.count(nums[i - k]))
                     leftS.erase(leftS.find(nums[i - k]));
-                } else if (rightS.count(nums[i - k])){
+                else
                     rightS.erase(rightS.find(nums[i - k]));
+                // adjust to properity
+                if (leftS.size() - rightS.size() == 2) {
+                    rightS.insert(*leftS.rbegin());
+                    leftS.erase(--leftS.end());
+                } else if (leftS.size() + 1 == rightS.size()) {
+                    leftS.insert(*rightS.begin());
+                    rightS.erase(rightS.begin());
                 }
             }
-            // adjust
-            if (leftS.size() - 2 == rightS.size()) { // ex. 6,4 => 5,5
-                rightS.insert(*leftS.rbegin());
-                leftS.erase(--leftS.end());
-            } else if (leftS.size() + 1 == rightS.size()) {
-                leftS.insert(*rightS.begin());
-                rightS.erase(rightS.begin());
-            }
-            // add element
-            leftS.insert(nums[i]);
-            rightS.insert(*leftS.rbegin());
-            leftS.erase(--leftS.end());
-            if (leftS.size() < rightS.size()) {
-                leftS.insert(*rightS.begin());
-                rightS.erase(rightS.begin());
-            }
-            // get median
-            if (i >= k - 1) {
-                if (k % 2)
-                    res.push_back(*leftS.rbegin());
-                else
-                    res.push_back((*leftS.rbegin() + *rightS.begin()) / 2.0);
-            }
+            // add
+            addNum(nums[i]);
+            // getMedian
+            if (i + 1 >= k)
+                res[i - k + 1] = getMedian();
         }
         return res;
     }
+    void addNum(int num) {
+        leftS.insert(num);
+        rightS.insert(*leftS.rbegin());
+        leftS.erase(--leftS.end());
+        if (leftS.size() < rightS.size()) {
+            leftS.insert(*rightS.begin());
+            rightS.erase(rightS.begin());
+        }
+    }
+    double getMedian() {
+        if (leftS.empty())
+            return -1.0;
+        if (leftS.size() > rightS.size())
+            return *leftS.rbegin();
+        else
+            return ((double)*leftS.rbegin() + *rightS.begin()) / 2.0;
+    }
+    multiset<int> leftS, rightS;
 };
 
 int main()
