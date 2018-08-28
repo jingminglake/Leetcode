30ff18b44d0e6f2e4f22226ac847796573a03a35
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-28 14:35:22 -0700

    leetcode 843

--- guessTheWord/guessTheWord.cpp
+++ guessTheWord/guessTheWord.cpp
@@ -8,29 +8,30 @@
  */
 class Solution {
 public:
-    int numOfMatch(string& w1, string& w2) {
-        int res = 0;
-        for (int i = 0; i < w1.length(); i++)
-            if (w1[i] == w2[i])
-                res++;
-        return res;
-    }
-    void shrinkWordList(vector<string>& wordlist, string& word, int matches) {
-        vector<string> next_wordlist;
-        for (string& w : wordlist) {
-            if (numOfMatch(w, word) == matches)
-                next_wordlist.push_back(w);
-        }
-        wordlist = next_wordlist;
-    }
     void findSecretWord(vector<string>& wordlist, Master& master) {
         string word = wordlist[rand() % wordlist.size()];
         for (int i = 0; i < 10; i++) {
-            int matches = master.guess(word);
-            if (matches == 6)
+            int res = master.guess(word);
+            if (res == 6)
                 return;
-            shrinkWordList(wordlist, word, matches);
+            shrinkWordlist(wordlist, res, word);
             word = wordlist[rand() % wordlist.size()];
         }
+    }
+    void shrinkWordlist(vector<string>& wordlist, int res, string& word) {
+        vector<string> new_wordlist;
+        for (auto& w : wordlist) {
+            if (isSimilarWord(w, res, word))
+                new_wordlist.push_back(w);
+        }
+        wordlist = new_wordlist;
+    }
+    bool isSimilarWord(string& word1, int res, string& word2) {
+        int count = 0;
+        for (int i = 0; i < word1.size(); i++) {
+            if (word1[i] == word2[i])
+                count++;
+        }
+        return count == res;
     }
 };
