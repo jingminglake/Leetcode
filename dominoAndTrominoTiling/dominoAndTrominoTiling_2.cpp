4db1ed645b0dd7001e0eba0d51ba11de2d7be571
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-27 20:37:38 -0700

    leetcode 790

--- dominoAndTrominoTiling/dominoAndTrominoTiling_2.cpp
+++ dominoAndTrominoTiling/dominoAndTrominoTiling_2.cpp
@@ -6,19 +6,19 @@ class Solution {
 public:
     int numTilings(int N) {
         const int kMod = 1000000007;
-        vector<vector<long> > dp(N + 1, vector<long>(3, 0));
-        dp[0][0] = dp[1][0] = dp[1][1] = dp[1][2] = 1;
+        vector<long> dp1(N + 1, 0), dp2 = dp1, dp3 = dp1;
+        dp1[0] = dp1[1] = dp2[1] = dp3[1] = 1;
         for (int i = 2; i <= N; i++) {
-            dp[i][0] = (dp[i - 1][0] + dp[i - 2][0] + dp[i - 2][1] + dp[i - 2][2]) % kMod;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % kMod;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % kMod;
+            dp1[i] = (dp1[i - 1] + dp1[i - 2] + dp2[i - 2] + dp3[i - 2]) % kMod;
+            dp2[i] = (dp1[i - 1] + dp3[i - 1]) % kMod;
+            dp3[i] = (dp1[i - 1] + dp2[i - 1]) % kMod;
         }
-        return dp[N][0];
+        return dp1[N];
     }
 };
 
 int main() {
-  Solution s;
-  cout << s.numTilings(5) << endl;
-  return 0;
+    Solution s;
+    cout << s.numTilings(5) << endl;
+    return 0;
 }
