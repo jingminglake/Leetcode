e75608ae94fde0436a811c06be1bbc0c48aac968
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-28 22:33:03 -0700

    leetcode 287

--- findTheDuplicateNumber/findTheDuplicateNumber.cpp
+++ findTheDuplicateNumber/findTheDuplicateNumber.cpp
@@ -5,12 +5,14 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0], fast = nums[nums[0]];
-        while (slow != fast) {
+        int slow = nums[0], fast = nums[0];
+        while (1) {
             slow = nums[slow];
             fast = nums[nums[fast]];
+            if (slow == fast)
+                break;
         }
-        fast = 0;
+        fast = nums[0];
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
