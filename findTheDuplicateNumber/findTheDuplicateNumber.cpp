b787b0b02701f7c3436544cdd904aa0459cb1d3c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-28 22:16:17 -0700

    leetcode 287

--- findTheDuplicateNumber/findTheDuplicateNumber.cpp
+++ findTheDuplicateNumber/findTheDuplicateNumber.cpp
@@ -2,33 +2,29 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int findDuplicate(vector<int>& nums) {
-    int size = nums.size();
-    if (size <= 1)
-      return -1;
-    int slow = nums[0];
-    int fast = nums[nums[0]];
-    while (slow != fast) {
-      slow = nums[slow];
-      fast = nums[nums[fast]];
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
-    fast = 0;
-    while (fast != slow) {
-      fast = nums[fast];
-      slow = nums[slow];
-    }
-    return slow;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int a[4] = {1,2,3,1};
-  vector<int> vec(a, a+2);
-  cout <<  s.findDuplicate(vec);
-  cout << endl;
-  return 0;
+    Solution s;
+    int a[4] = {1,2,3,1};
+    vector<int> vec(a, a+2);
+    cout <<  s.findDuplicate(vec);
+    cout << endl;
+    return 0;
 }
