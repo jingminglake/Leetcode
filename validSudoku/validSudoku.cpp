e6ad4aef50a5fdad1a8750ec8aa6681fb4da4a7f
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-29 14:29:29 -0700

    leetcode 36

--- validSudoku/validSudoku.cpp
+++ validSudoku/validSudoku.cpp
@@ -2,34 +2,34 @@
 #include <vector>
 using namespace std;
 
-
-class Solution{
+class Solution {
 public:
-  bool isValidSudoku(vector<vector<char> >& board) {
-    bool rowMap[9][9] = {false};
-    bool columnMap[9][9] = {false};
-    bool subBoxMap[9][9] = {false};
-    for (int i = 0; i < 9; i++)
-      for (int j = 0; j < 9; j++)
-	if (board[i][j] != '.') {
-	  int num = board[i][j] - '0' - 1;
-	  int k = i / 3 * 3 + j / 3;
-	  if (rowMap[i][num] || columnMap[j][num] || subBoxMap[k][num])
-	    return false;
-	  rowMap[i][num] = columnMap[j][num] = subBoxMap[k][num] = true;
-	}
-    return true;
-  }
-
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int n = board.size();
+        vector<vector<bool> > rows(n, vector<bool>(n, false)), cols = rows, grid = cols;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.')
+                    continue;
+                int num = board[i][j] - '0' - 1;
+                int grid_num = i / 3 * 3 + j / 3;
+                if (rows[i][num] || cols[j][num] || grid[grid_num][num])
+                    return false;
+                else
+                    rows[i][num] = cols[j][num] = grid[grid_num][num] = true;
+            }
+        }
+        return true;
+    }
 };
 
 int main()
 {
-  vector<vector<char> > board(9, vector<char>(9));
-  for (int i = 0; i < 9; i++)
-    for (int j = 0; j < 9; j++)
-      board[i][j] = '0' + j + 1;
-  Solution s;
-  cout << s.isValidSudoku(board) << endl;
-  return 0;
+    vector<vector<char> > board(9, vector<char>(9));
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            board[i][j] = '0' + j + 1;
+    Solution s;
+    cout << s.isValidSudoku(board) << endl;
+    return 0;
 }
