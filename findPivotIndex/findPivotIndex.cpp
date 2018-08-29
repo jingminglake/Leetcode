fcd93b7fc168d98c90fb4fa64f3d9046adc2d04d
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-29 15:07:07 -0700

    leetcode 724

--- findPivotIndex/findPivotIndex.cpp
+++ findPivotIndex/findPivotIndex.cpp
@@ -2,34 +2,32 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-  int pivotIndex(vector<int>& nums) {
-    int size = nums.size();
-    if (size == 0)
-      return -1;
-    int sum = 0;
-    for (int n : nums)
-      sum += n;
-    int leftP = 0;
-    for (int i = 0; i < size; i++) {
-      leftP += nums[i];
-      if (sum - leftP == leftP - nums[i])
-	return i;
+    int pivotIndex(vector<int>& nums) {
+        if (nums.size() < 2)
+            return -1;
+        int64_t sum = 0;
+        for (int n : nums)
+            sum += n;
+        int left = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (left == sum - nums[i] - left)
+                return i;
+            left += nums[i];
+        }
+        return -1;
     }
-    return -1;
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> ss = {1,7,3,6,5,6};
-  vector<int> ss1 = {1};
-  vector<int> ss2 = {1,2,3};
-  cout << s.pivotIndex(ss) << endl;
-  cout << s.pivotIndex(ss1) << endl;
-  cout << s.pivotIndex(ss2) << endl;
-  return 0;
+    Solution s;
+    vector<int> ss = {1,7,3,6,5,6};
+    vector<int> ss1 = {1};
+    vector<int> ss2 = {1,2,3};
+    cout << s.pivotIndex(ss) << endl;
+    cout << s.pivotIndex(ss1) << endl;
+    cout << s.pivotIndex(ss2) << endl;
+    return 0;
 }
