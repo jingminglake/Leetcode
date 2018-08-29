bd23fe5359d3c4c0290b191867124c587a2dc5fc
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-28 17:03:12 -0700

    leetcode 300

--- longestIncreasingSubsequence/longestIncreasingSubsequence_nlogn.cpp
+++ longestIncreasingSubsequence/longestIncreasingSubsequence_nlogn.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int res = 0;
-        if (nums.size() == 0)
-            return res;
+        int n = nums.size();
+        if (n == 0)
+            return 0;
         vector<int> tail;
-        for (int i = 0; i < nums.size(); i++) {
-            int left = 0, right = tail.size();
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (tail[mid] < nums[i])
-                    left = mid + 1;
-                else
-                    right = mid;
-            }
-            if (right >= tail.size())
+        for (int i = 0; i < n; i++) {
+            int index = find_last_less_than(tail, nums[i]);
+            if (index == tail.size())
                 tail.push_back(nums[i]);
             else
-                tail[right] = nums[i];
+                tail[index] = nums[i];
         }
         return tail.size();
+    }
+    int find_last_less_than(vector<int>& tail, int target) {
+        int left = 0, right = tail.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (tail[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
     }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> nums = {10,9,2,5,3,7,101,18};
-  cout << s.lengthOfLIS(nums) << endl;
-  return 0;
+    Solution s;
+    vector<int> nums = {10,9,2,5,3,7,101,18};
+    cout << s.lengthOfLIS(nums) << endl;
+    return 0;
 }
