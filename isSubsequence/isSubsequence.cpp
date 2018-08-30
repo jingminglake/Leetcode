4bd29c280049298604a8d5ced44c2e42fb72b467
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-29 17:22:06 -0700

    leetcode 392

--- isSubsequence/isSubsequence.cpp
+++ isSubsequence/isSubsequence.cpp
@@ -2,33 +2,29 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool isSubsequence(string s, string t) {
-    int p1 = 0, p2 = 0;
-    int size1 = s.length();
-    int size2 = t.length();
-    if (size1 == 0)
-      return true;
-    while (p2 < size2) {
-      if (s[p1] == t[p2])
-	p1++;
-      if (p1 == size1)
-	return true;
-      p2++;
+    bool isSubsequence(string s, string t) {
+        int len_s = s.length(), len_t = t.length();
+        int p_s = 0, p_t = 0;
+        while (p_t < len_t) {
+            if (s[p_s] == t[p_t]) {
+                p_s++; p_t++;
+            } else {
+                p_t++;
+            }
+        }
+        return p_s == len_s;
     }
-    return false;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string s1 = "abc";
-  string s2 = "ahbgdc";
-  cout << s.isSubsequence(s1, s2) << endl;
-  return 0;
+    Solution s;
+    string s1 = "abc";
+    string s2 = "ahbgdc";
+    cout << s.isSubsequence(s1, s2) << endl;
+    return 0;
 }
