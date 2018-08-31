670e7748998dbce38a3452911d93d9dde9883340
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-31 16:33:43 -0700

    leetcode 403

--- frogJump/frogJump_map.cpp
+++ frogJump/frogJump_map.cpp
@@ -6,28 +6,24 @@ using namespace std;
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-        if (stones.size() == 0)
-            return false;
         unordered_map<int, unordered_map<int, bool> > dp;
         return dfs(0, 0, stones, dp);
     }
-    bool dfs(int i, int k, vector<int>& stones, unordered_map<int, unordered_map<int, bool> >& dp) {
-        if (dp.count(i) && dp[i].count(k))
-            return dp[i][k];
-        if (i == stones.size() - 1)
-            return dp[i][k] = true;
-        if (k < 0)
-            return dp[i][k] = false;
-        for (int j = i + 1; j < stones.size(); j++) {
-            int gap = stones[j] - stones[i];
-            if (gap == k - 1 && dfs(j, k - 1, stones, dp))
-                return true;
-            else if (gap == k && dfs(j, k, stones, dp))
-                return true;     
-            else if (gap == k + 1 && dfs(j, k + 1, stones, dp))
-                return true;
+    bool dfs(int start_index, int k, vector<int>& stones, unordered_map<int, unordered_map<int, bool> >& dp) {
+        if (start_index == stones.size() - 1)
+            return true;
+        if (dp.count(start_index) && dp[start_index].count(k))
+            return dp[start_index][k];
+        for (int i = start_index + 1; i < stones.size(); i++) {
+            int gap = stones[i] - stones[start_index];
+            if (gap == k - 1 && dfs(i, k - 1, stones, dp))
+                return dp[start_index][k] = true;
+            else if (gap == k && dfs(i, k, stones, dp))
+                return dp[start_index][k] = true;
+            else if (gap == k + 1 && dfs(i, k + 1, stones, dp))
+                return dp[start_index][k] = true;
         }
-        return dp[i][k] = false;
+        return dp[start_index][k] = false;
     }
 };
 
