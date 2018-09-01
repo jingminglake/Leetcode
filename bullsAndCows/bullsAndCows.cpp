af0c420c970adf9c69658957130258768afbab26
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-08-31 20:01:10 -0700

    leetcode 299

--- bullsAndCows/bullsAndCows.cpp
+++ bullsAndCows/bullsAndCows.cpp
@@ -1,38 +1,33 @@
 // clang++ bullsAndCows.cpp -std=c++11
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  string getHint(string secret, string guess) {
-    int numOfBulls = 0, numOfCows = 0;
-    int len = secret.length();
-    vector<int> hash(10, 0);
-    for (int i = 0; i < len; i++) {
-      if (secret[i] == guess[i]) {
-	numOfBulls++;
-      }
-      else {
-	++hash[secret[i] - '0'];
-      }
+    string getHint(string secret, string guess) {
+        int hash1[10] = {0}, hash2[10] = {0};
+        int bulls = 0;
+        for (int i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) {
+                bulls++;
+            }
+            hash1[secret[i] - '0']++;
+            hash2[guess[i] - '0']++;
+        }
+        int same = 0;
+        for (int i = 0; i < 10; i++)
+            same += min (hash1[i], hash2[i]);
+        return to_string(bulls) + "A" + to_string(same - bulls) + "B";
     }
-    for (int i = 0; i < len; i++) {
-      if (secret[i] != guess[i] && --hash[guess[i] - '0'] >= 0)
-	numOfCows++;
-    }
-    string ans = to_string(numOfBulls) + "A" + to_string(numOfCows) + "B";
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string s1 = "1122";
-  string s2 = "2211";
-  cout << s.getHint(s1, s2);
-  cout << endl;
-  return 0;
+    Solution s;
+    string s1 = "1122";
+    string s2 = "2211";
+    cout << s.getHint(s1, s2);
+    cout << endl;
+    return 0;
 }
