c4cd93166679956917eaa3ad73df96f134607a56
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-02 13:58:55 -0700

    leetcode 304

--- rangeSumQuery2D-Immutable/rangeSumQuery2D-Immutable.cpp
+++ rangeSumQuery2D-Immutable/rangeSumQuery2D-Immutable.cpp
@@ -6,23 +6,29 @@ using namespace std;
 
 class NumMatrix {
 public:
-  NumMatrix(vector<vector<int> > matrix) {
-    if (matrix.size() == 0 || matrix[0].size() == 0)
-      return;
-    int m = matrix.size();
-    int n = matrix[0].size();
-    dp = vector<vector<int> >(m + 1, vector<int>(n + 1, 0));
-    for (int i = 1; i <= m; i++) {
-      for (int j = 1; j <= n; j++) {
-	dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
-      }
+    NumMatrix(vector<vector<int>> matrix) {
+        m = matrix.size();
+        if (m == 0)
+            return;
+        n = matrix[0].size();
+        if (n == 0)
+            return;
+        dp = vector<vector<int> >(m + 1, vector<int> (n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
     }
-  }
-  int sumRegion(int row1, int col1, int row2, int col2) {
-    return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
-  }
-private:
-  vector<vector<int> > dp;
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        if (m == 0 || n == 0)
+            return -1;
+        return dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1];
+    }
+    vector<vector<int> > dp;
+    int m;
+    int n;
 };
 
 int main()
