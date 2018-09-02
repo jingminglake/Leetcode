09c6c20c2535197e19f26479162f5d7444b6d8ee
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-02 16:16:18 -0700

    leetcode 118

--- pascalTriangle/pascalTriangle.cpp
+++ pascalTriangle/pascalTriangle.cpp
@@ -2,36 +2,36 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-   vector<vector<int> > generate(int numRows) {
-     vector<vector<int> > ans;
-     if (numRows < 1)
-       return ans;
-     vector<int> prev(1,1);
-     ans.push_back(prev);
-     for (int i = 1; i < numRows; i++) {
-       vector<int> temp(i+1,1);
-       for (int j = 1; j < i; j++) {
-	 temp[j] = ans[i-1][j-1] + ans[i-1][j];
-       }
-       ans.push_back(temp);
-       //prev.swap(temp);
-     }
-     return ans;
-   }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> > res;
+        if (numRows == 0)
+            return res;
+        vector<int> pre_level = {1};
+        res.push_back(pre_level);
+        for (int i = 1; i < numRows; i++) {
+            vector<int> level(i + 1, 1);
+            for (int k = 1; k <= i - 1; k++) {
+                level[k] = pre_level[k - 1] + pre_level[k];
+            }
+            res.push_back(level);
+            pre_level = level;
+        }
+        return res;
+    }
 };
 
 int main()
 {
-  Solution s;
-  vector<vector<int> > vec = s.generate(5);
-  vector<vector<int> >::iterator it = vec.begin();
-  while (it != vec.end()) {
-    for (int i : *it)
-      cout << i << " ";
-    cout << endl;
-    ++it;
-  }
-  return 0;
+    Solution s;
+    vector<vector<int> > vec = s.generate(5);
+    vector<vector<int> >::iterator it = vec.begin();
+    while (it != vec.end()) {
+        for (int i : *it)
+            cout << i << " ";
+        cout << endl;
+        ++it;
+    }
+    return 0;
 }
