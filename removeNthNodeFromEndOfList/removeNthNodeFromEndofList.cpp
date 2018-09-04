d64dc3c9c349d847cd3af0d4e13b2952a35349d0
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-03 22:04:19 -0700

    leetcode 19

--- removeNthNodeFromEndOfList/removeNthNodeFromEndofList.cpp
+++ removeNthNodeFromEndOfList/removeNthNodeFromEndofList.cpp
@@ -1,56 +1,54 @@
 #include <iostream>
-#include <cstddef>
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {
-  }
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {
+    }
 };
 
-class Solution{
+
+class Solution {
 public:
-  ListNode* removeNthFromEnd(ListNode* head, int n) {
-    if (!head)
-      return head;
-    ListNode *pHead = new ListNode(0);
-    pHead->next = head;
-    ListNode *p = pHead, *q = pHead;
-    for (int i = 0; i < n; i++)
-      q = q->next;
-    while (q->next) {
-      p = p->next;
-      q = q->next;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* p1 = head;
+        while (n) {
+            p1 = p1->next;
+            n--;
+        }
+        ListNode* p2 = &dummy;
+        while (p1) {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        ListNode* to_delete = p2->next;
+        p2->next = p2->next->next;
+        delete to_delete;
+        return dummy.next;
     }
-    cout << "p: " << p->val << endl;
-    q = p->next;
-    p->next = q->next;
-    delete q;
-    head = pHead->next;
-    delete pHead;
-    return head;
-  }
 };
 
 int main()
 {
-  int a[5] = {1, 2, 3, 4, 5};
-  ListNode *head = new ListNode(a[0]);
-  ListNode *tail = head;
-  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
-    ListNode *temp = new ListNode(a[i]);
-    tail->next = temp;
-    tail = temp;
-  }
-  Solution s;
-  s.removeNthFromEnd(head, 2);
-  while (head != NULL) {
-    ListNode *p = head;
-    head = head->next;
-    cout << "delete " << p->val << " ";
-    delete p;
-  }
-  cout << endl;
-  return 0;
+    int a[5] = {1, 2, 3, 4, 5};
+    ListNode *head = new ListNode(a[0]);
+    ListNode *tail = head;
+    for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+        ListNode *temp = new ListNode(a[i]);
+        tail->next = temp;
+        tail = temp;
+    }
+    Solution s;
+    s.removeNthFromEnd(head, 2);
+    while (head != NULL) {
+        ListNode *p = head;
+        head = head->next;
+        cout << "delete " << p->val << " ";
+        delete p;
+    }
+    cout << endl;
+    return 0;
 }
