0ec66edef3754bacd58eaf3aaba47a9bd7c405a8
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-08 19:08:36 -0700

    leetcode 5

--- longestPalindromicSubstring/longestPalinadromicSubstring.cpp
+++ longestPalindromicSubstring/longestPalinadromicSubstring.cpp
@@ -1,42 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-  string longestPalindrome(string s) {
-    int len = s.length();
-    bool dp[len][len];
-    memset(dp, false, len*len*sizeof(bool));
-    int maxLen = 0;
-    int left = 0;
-    for (int j = 0; j < len; j++) {
-      for (int i = 0; i <= j; i++) {
-	if (i == j)
-	  dp[i][j] = true;
-	else if (j - i == 1)
-	  dp[i][j] = (s[i] == s[j]);
-	else 
-	  dp[i][j] = (s[i] == s[j]) && dp[i+1][j-1];
-                
-	if (dp[i][j]) {
-	  if (maxLen < j - i + 1) {
-	    maxLen = j - i + 1;
-	    left  = i;
-	  }
-	}
-      }
+    string longestPalindrome(string s) {
+        int len_s = s.length();
+        vector<vector<bool> > dp(len_s, vector<bool>(len_s, false));
+        int max_len = 0, start_index = 0;
+        for (int i = len_s - 1; i >= 0; i--) {
+            for (int j = i; j < len_s; j++) {
+                if (i == j)
+                    dp[i][j] = true;
+                else if (j - i == 1)
+                    dp[i][j] = (s[i] == s[j]);
+                else
+                    dp[i][j] = (s[i] == s[j]) && dp[i + 1][j - 1];
+                if (dp[i][j] && j - i + 1 > max_len) {
+                    max_len = j - i + 1;
+                    start_index = i;
+                }
+            }
+        }
+        return s.substr(start_index, max_len);
     }
-    return s.substr(left, maxLen);
-  }
 };
 
 
 int main()
 {
-  Solution s;
-  string ss = "cbbd";
-  cout << s.longestPalindrome(ss) << endl;
-  return 0;
+    Solution s;
+    string ss = "cbbd";
+    cout << s.longestPalindrome(ss) << endl;
+    return 0;
 }
