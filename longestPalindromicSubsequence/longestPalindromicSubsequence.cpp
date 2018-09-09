8b12d25f26c90e5e0b40deebc97063e44aaf5d5c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-08 21:21:20 -0700

    leetcode 516

--- longestPalindromicSubsequence/longestPalindromicSubsequence.cpp
+++ longestPalindromicSubsequence/longestPalindromicSubsequence.cpp
@@ -2,32 +2,31 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-  int longestPalindromeSubseq(string s) {
-    int size = s.length();
-    vector<vector<int> > dp(size, vector<int>(size, 0));
-    for (int i = size - 1; i >= 0; i--) {
-      dp[i][i] = 1;
-      for (int j = i + 1; j < size; j++) {
-	if (s[i] == s[j]) {
-	  dp[i][j] = dp[i + 1][j - 1] + 2;
-	} else {
-	  dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
-	}
-      }
+    int longestPalindromeSubseq(string s) {
+        int len_s = s.length();
+        vector<vector<int> > dp(len_s, vector<int>(len_s, 0));
+        for (int i = len_s - 1; i >= 0; i--) {
+            for (int j = i; j < len_s; j++) {
+                if (i == j)
+                    dp[i][j] = 1;
+                else if (s[i] == s[j])
+                    dp[i][j] = dp[i + 1][j - 1] + 2; 
+                else  
+                    dp[i][j] = max (dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][len_s - 1];
     }
-    return dp[0][size - 1];
-  }
 };
 
 int main()
 {
-  Solution s;
-  string ss = "bbbab";
-  cout << s.longestPalindromeSubseq(ss) << endl;
-  return 0;
+    Solution s;
+    string ss = "bbbab";
+    cout << s.longestPalindromeSubseq(ss) << endl;
+    return 0;
 }
