78cb6fc7c4e4fc955acd1b4fce5b776a5e5d0708
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-08 22:06:09 -0700

    leetcode 139

--- wordBreak/wordBreak.cpp
+++ wordBreak/wordBreak.cpp
@@ -5,34 +5,30 @@
 #include <unordered_set>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool wordBreak(string s, vector<string>& wordDict) {
-    int len = s.length();
-    vector<bool> dp(len + 1, false);
-    dp[0] = true;
-    unordered_set<string> set;
-    for (string& word : wordDict)
-      set.insert(word);
-    for (int i = 1; i < len + 1; i++) {
-      for (int j = i - 1; j >= 0; j--) {
-	if (dp[j]) {
-	  if (set.find(s.substr(j, i - j)) != set.end()) {
-	    dp[i] = true;
-	    break;
-	  }
-	}
-      }//for
-    }//for
-    return dp[len];
-  }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int len_s = s.length();
+        vector<int> dp(len_s + 1, false);
+        dp[0] = true;
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        for (int i = 1; i <= len_s; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[len_s];
+    }
 };
 
 int main()
 {
-  Solution s;
-  string ss = "leetcode";
-  vector<string> dict = {"leet", "code"};
-  cout << s.wordBreak(ss, dict) << endl;;
-  return 0;
+    Solution s;
+    string ss = "leetcode";
+    vector<string> dict = {"leet", "code"};
+    cout << s.wordBreak(ss, dict) << endl;;
+    return 0;
 }
