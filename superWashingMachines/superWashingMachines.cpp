de1d58bb91fb1517350e47f313abe9f57c3f6b92
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-08 22:56:05 -0700

    leetcode 517

--- superWashingMachines/superWashingMachines.cpp
+++ superWashingMachines/superWashingMachines.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-  int findMinMoves(vector<int>& machines) {
-    int len = machines.size();
-    vector<int> sum(len + 1, 0);
-    for (int i = 0; i < len; i++) {
-      sum[i+1] = sum[i] + machines[i];
+    int findMinMoves(vector<int>& machines) {
+        int len = machines.size();
+        vector<int> preSum(len + 1, 0);
+        for (int i = 0; i < len; i++) {
+            preSum[i + 1] = preSum[i] + machines[i];
+        }
+        if (preSum[len] % len)
+            return -1;
+        int avg = preSum[len] / len;
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            int left_need = i * avg - preSum[i];
+            int right_need = (len - i - 1) * avg - (preSum[len] - preSum[i] - machines[i]);
+            int moves = 0;
+            if (left_need > 0 && right_need > 0)
+                moves = left_need + right_need;
+            else
+                moves = max( abs(left_need), abs(right_need) );
+            res = max (res, moves);
+        }
+        return res;
     }
-    if (sum[len] % len)
-      return -1;
-    int avg = sum[len] / len;
-    int res = 0;
-    for (int i = 0; i < len; i++) {
-      int l = i * avg - sum[i];
-      int r = (len - i - 1) * avg - (sum[len] - sum[i] - machines[i]);
-      if (l > 0 && r > 0)
-	res = max (res, l + r);
-      else
-	res = max (res, max(abs(l), abs(r) ) );
-    }
-    return res;
-  }
 };
 
 
 
 int main()
 {
-  Solution sl;
-  vector<int> machines = {1,0,5};
-  cout << sl.findMinMoves(machines) << endl;
+    Solution sl;
+    vector<int> machines = {1,0,5};
+    cout << sl.findMinMoves(machines) << endl;
 }
