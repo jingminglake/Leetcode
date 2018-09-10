e6ac62311362eb5671444f0b3030fd4817bf9a84
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-10 15:20:28 -0700

    leetcode 198

--- houseRobber/houseRobber_2.cpp
+++ houseRobber/houseRobber_2.cpp
@@ -10,13 +10,13 @@ public:
             return 0;
         if (n == 1)
             return nums[0];
-        vector<int64_t> dp(n, INT_MIN);
+        vector<int64_t> dp(3, INT_MIN);
         dp[0] = nums[0];
         dp[1] = max (nums[0], nums[1]);
         for (int i = 2; i < n; i++) {
-            dp[i] = max (dp[i - 2] + nums[i], dp[i - 1]);
+            dp[i % 3] = max (dp[(i - 2) % 3] + nums[i], dp[(i - 1) % 3]);
         }
-        return dp[n - 1];
+        return dp[(n - 1) % 3];
     }
 };
 
