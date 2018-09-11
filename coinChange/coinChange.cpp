f10d518f789789aeaa30a41a87918630b0909fa9
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-10 21:37:55 -0700

    leetcode 322

--- coinChange/coinChange.cpp
+++ coinChange/coinChange.cpp
@@ -11,9 +11,9 @@ public:
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
-	            if (i >= coin && dp[i - coin] != INT_MAX) {
-	                dp[i] = min (dp[i], dp[i - coin] + 1);
-	            }
+                if (i >= coin && dp[i - coin] != INT_MAX) {
+                    dp[i] = min (dp[i], dp[i - coin] + 1);
+                }
             }
         }
         return dp[amount] == INT_MAX ? -1 : dp[amount];
