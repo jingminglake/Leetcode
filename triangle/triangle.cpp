60bdabd4dd280791ee49f6f5b30df4711ab69865
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-24 12:41:24 -0700

    leetcode 120

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int minimumTotal(vector<vector<int> >& triangle) {
-    int size = triangle.size();
-    vector<int> m(triangle.back());
-    for (int i = size-2; i >= 0; i--) {
-      int sizeR = triangle[i].size();
-      for (int j = 0; j < sizeR; j++) {
-	m[j] = min(m[j], m[j+1]) + triangle[i][j];
-      }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if (triangle.size() == 0 || triangle[0].size() == 0)
+            return 0;
+        int m = triangle.size(), n = triangle[m - 1].size();
+        vector<int> dp(n, 0);
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (i == m - 1)
+                    dp[j] = triangle[i][j];
+                else if (j + 1 < n)
+                    dp[j] = min (dp[j], dp[j + 1]) + triangle[i][j];
+            }
+        }
+        return dp[0];
     }
-    return m[0];
-  }
 };
 
 int main()
 {
-  Solution s;
-  int a1[1] = {2};
-  int a2[2] = {3,4};
-  int a3[3] = {6,5,7};
-  int a4[4] = {4,1,8,3};
-  vector<int> vec1(a1, a1+1);
-  vector<int> vec2(a2, a2+2);
-  vector<int> vec3(a3, a3+3);
-  vector<int> vec4(a4, a4+4);
-  vector<vector<int> > triangle;
-  triangle.push_back(vec1);
-  triangle.push_back(vec2);
-  triangle.push_back(vec3);
-  triangle.push_back(vec4);
-  cout << s.minimumTotal(triangle);
-  cout << endl;
-  return 0;
+    Solution s;
+    int a1[1] = {2};
+    int a2[2] = {3,4};
+    int a3[3] = {6,5,7};
+    int a4[4] = {4,1,8,3};
+    vector<int> vec1(a1, a1+1);
+    vector<int> vec2(a2, a2+2);
+    vector<int> vec3(a3, a3+3);
+    vector<int> vec4(a4, a4+4);
+    vector<vector<int> > triangle;
+    triangle.push_back(vec1);
+    triangle.push_back(vec2);
+    triangle.push_back(vec3);
+    triangle.push_back(vec4);
+    cout << s.minimumTotal(triangle);
+    cout << endl;
+    return 0;
 }
