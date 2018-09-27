b7d81b86327b6bdf3f65665191c337e4fced0162
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-27 12:34:39 -0700

    leetcode 78

--- subsets/subsets_2.cpp
+++ subsets/subsets_2.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<vector<int> > subsets(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    vector<vector<int> > ans;
-    vector<int> subset;
-    backTracking(nums, 0, subset, ans);
-    return ans;
-  }
-  void backTracking(vector<int>& nums, int start, vector<int>& subset, vector<vector<int> >& ans) {
-    ans.push_back(subset);
-    for (int i = start; i < nums.size(); i++) {
-      subset.push_back(nums[i]);
-      backTracking(nums, i + 1, subset, ans);
-      subset.pop_back();
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > res;
+        vector<int> path;
+        dfs(nums, 0, path, res);
+        return res;
     }
-  }
+    void dfs(vector<int>& nums, int start, vector<int>& path, vector<vector<int> >& res) {
+        res.push_back(path);
+        for (int i = start; i < nums.size(); i++) {
+            path.push_back(nums[i]);
+            dfs(nums, i + 1, path, res);
+            path.pop_back();
+        }
+    }
 };
 
 int main()
 {
-  Solution s;
-  int a[3] = {1,2,3};
-  vector<int> vec(a, a+3);
-  vector<vector<int> > res = s.subsets(vec);
-  vector<vector<int> >::iterator it = res.begin();
-  while (it != res.end()) {
-    for (int i : *it)
-      cout << i << " ";
-    cout << endl;
-    ++it;
-  }
-  return 0;
+    Solution s;
+    int a[3] = {1,2,3};
+    vector<int> vec(a, a+3);
+    vector<vector<int> > res = s.subsets(vec);
+    vector<vector<int> >::iterator it = res.begin();
+    while (it != res.end()) {
+        for (int i : *it)
+            cout << i << " ";
+        cout << endl;
+        ++it;
+    }
+    return 0;
 }
