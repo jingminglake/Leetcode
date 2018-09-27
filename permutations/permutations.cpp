572050e4d9301b93a8a34174163fa501a9d88217
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-27 14:03:04 -0700

    leetcode 46

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -4,42 +4,42 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > permute(vector<int>& nums) {
+    vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int> > res;
         vector<bool> visited(nums.size(), false);
         vector<int> path;
-        dfs(nums, visited, path, res);
+        dfs(nums, res, visited, path, 0);
         return res;
     }
-    void dfs(vector<int>& nums, vector<bool>& visited, vector<int>& path, vector<vector<int> >& res) {
-        if (path.size() == nums.size()) {
+    void dfs(vector<int>& nums, vector<vector<int> >& res, vector<bool>& visited, vector<int>& path, int index) {
+        if (index == nums.size()) {
             res.push_back(path);
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
-            if (visited[i])
-                continue;
-            visited[i] = true;
-            path.push_back(nums[i]);
-            dfs(nums, visited, path, res);
-            path.pop_back();
-            visited[i] = false;
+            if (!visited[i]) {
+                visited[i] = true;
+                path.push_back(nums[i]);
+                dfs(nums, res, visited, path, index + 1);
+                path.pop_back();
+                visited[i] = false;
+            }
         }
     }
 };
 
 int main()
 {
-  Solution s;
-  int a[3] = {1,2,3};
-  vector<int> vec(a, a+3);
-  vector<vector<int> > res = s.permute(vec);
-  vector<vector<int> >::iterator it = res.begin();
-  while (it != res.end()) {
-    for (int i : *it)
-      cout << i << " ";
-    cout << endl;
-    ++it;
-  }
-  return 0;
+    Solution s;
+    int a[3] = {1,2,3};
+    vector<int> vec(a, a+3);
+    vector<vector<int> > res = s.permute(vec);
+    vector<vector<int> >::iterator it = res.begin();
+    while (it != res.end()) {
+        for (int i : *it)
+            cout << i << " ";
+        cout << endl;
+        ++it;
+    }
+    return 0;
 }
