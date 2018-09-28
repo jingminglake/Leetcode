4b661b92fd6ee58ee780395d1c8a53ecd406ff78
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-09-28 16:33:56 -0700

    leetcode 698

--- partitionToKEqualSumSubsets/partitionToKEqualSumSubsets.cpp
+++ partitionToKEqualSumSubsets/partitionToKEqualSumSubsets.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool canPartitionKSubsets(vector<int>& nums, int k) {
-    int sum = 0;
-    for (int num : nums)
-      sum += num;
-    if (sum % k != 0)
-      return false;
-    vector<bool> visited(nums.size(), false);
-    return dfs(nums, k, sum / k, 0, 0, visited);
-  }
-  bool dfs(vector<int>& nums, int k, int target, int curSum, int start, vector<bool>& visited) {
-    if (k == 0)
-      return true;
-    if (curSum == target)
-      return dfs(nums, k - 1, target, 0, 0, visited);
-    for (int i = start; i < nums.size(); i++) {
-      if (!visited[i]) {
-	visited[i] = true;
-	if (dfs(nums, k, target, curSum + nums[i], i, visited))
-	  return true;
-	visited[i] = false;
-      }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        if (sum % k != 0)
+            return false;
+        string visited(nums.size(), '0');
+        unordered_map<int, unordered_map<string, bool> > m;
+        return dfs(nums, k, sum / k, 0, 0, visited, m);
     }
-    return false;
-  }
+    bool dfs(vector<int>& nums, int k, int target, int curSum, int start, string& visited, unordered_map<int, unordered_map<string, bool> >& m) {
+        if (m.count(k) && m[k].count(visited)) {
+            return m[k][visited];
+        }
+        if (k == 0)
+            return m[k][visited] = true;
+        if (curSum == target)
+            return dfs(nums, k - 1, target, 0, 0, visited, m);
+        for (int i = start; i < nums.size(); i++) {
+            if (visited[i] == '0') {
+                visited[i] = '1';
+                if (dfs(nums, k, target, curSum + nums[i], i, visited, m))
+                    return m[k][visited] = true;
+                visited[i] = '0';
+            }
+        }
+        return false;
+    }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> nums = {4,3,2,3,5,2,1};
-  int k = 4;
-  cout << s.canPartitionKSubsets(nums, k) << endl;
-  return 0;
+    Solution s;
+    vector<int> nums = {4,3,2,3,5,2,1};
+    int k = 4;
+    cout << s.canPartitionKSubsets(nums, k) << endl;
+    return 0;
 }
