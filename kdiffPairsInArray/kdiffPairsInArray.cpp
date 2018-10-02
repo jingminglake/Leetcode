bea8ef3efe18717a51c4e0dcf433a7eacf2d2861
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-10-01 22:59:00 -0700

    leetcode 532

--- kdiffPairsInArray/kdiffPairsInArray.cpp
+++ kdiffPairsInArray/kdiffPairsInArray.cpp
@@ -3,54 +3,34 @@
 #include <unordered_map>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int findPairs(vector<int>& nums, int k) {
-    int ans = 0;
-    if (k < 0)
-      return ans;
-    int size = nums.size();
-    if (size < 2)
-      return ans;
-    unordered_map<int, int> map;
-    map.insert(make_pair(nums[0], 1)); 
-    unordered_map<int, int>::iterator it;
-    for (int i = 1; i < nums.size(); i++) {
-      it = map.find(nums[i]);
-      if (it != map.end()) {//find
-	it->second++;
-	continue;
-      }
-      map.insert(make_pair(nums[i], 1));
-      if (k != 0) {
-	it = map.find(nums[i] + k);
-	if (it != map.end()) //find
-	  ans++;
-	it = map.find(nums[i] - k);
-	if (it != map.end()) //find
-	  ans++;
-      }	
+    int findPairs(vector<int>& nums, int k) {
+        if (nums.size() == 0 || k < 0)
+            return 0;
+        int res = 0;
+        unordered_map<int, int> m;
+        for (int num : nums)
+            m[num]++;
+        for (auto& p : m) {
+            if (k == 0) {
+                if (p.second >= 2)
+                    res++;
+            } else {
+                if (m.count(p.first + k))
+                    res++;
+            }
+        }
+        return res;
     }
-    if (k == 0) {
-      ans = 0;
-      it = map.begin();
-      while (it != map.end()) {
-	if (it->second > 1)
-	  ans++;
-	++it;
-      }
-    }
-      
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int a[5] = {1,3,1,5,4};
-  vector<int> vec(a, a+5);
-  cout << s.findPairs(vec, 0);
-  cout << endl;
-  return 0;
+    Solution s;
+    int a[5] = {1,3,1,5,4};
+    vector<int> vec(a, a+5);
+    cout << s.findPairs(vec, 0);
+    cout << endl;
+    return 0;
 }
