e6aec091ecb89a4dcf13b57e82f0769f6584ffbe
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-10-02 00:19:56 -0700

    leetcode 3

--- longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters3.cpp
+++ longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters3.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 #include <unordered_map>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-  int lengthOfLongestSubstring(string s) {
-    int res = 0;
-    if (s.length() == 0)
-      return res;
-    int left = 0, right = 0;
-    unordered_map<char, int> hash;
-    while (left < s.length() && right < s.length()) {
-      unordered_map<char, int>::iterator it = hash.find(s[right]);
-      if (it != hash.end()) {
-	left = max(left, it->second + 1);
-      }
-      hash[s[right]] = right;
-      res = max (res, right - left + 1);
-      right++;
+    int lengthOfLongestSubstring(string s) {
+        int res = 0;
+        if (s.length() == 0)
+            return res;
+        int left = 0, right = 0;
+        unordered_map<char, int> m;
+        while (right < s.length()) {
+            if (m.count(s[right])) {
+                left = max(left, m[s[right]] + 1);
+            }
+            m[s[right]] = right;
+            res = max (res, right - left + 1);
+            right++;
+        }
+        return res;
     }
-    return res;
-  }
 };
-
-
 
 int main()
 {
-  string s;
-  cout << "please input a string:" << endl;
-  cin >> s;
-  Solution sl;
-  cout << sl.lengthOfLongestSubstring(s) << endl;
+    string s;
+    cout << "please input a string:" << endl;
+    cin >> s;
+    Solution sl;
+    cout << sl.lengthOfLongestSubstring(s) << endl;
 }
