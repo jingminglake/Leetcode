c76d2ac6d7335e20f5db58aa6aaa74929a2b3c2c
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-10-08 13:24:44 -0700

    leetcode 215

--- kthLargestElementInAnArray/kthLargestElementInAnArray_2.cpp
+++ kthLargestElementInAnArray/kthLargestElementInAnArray_2.cpp
@@ -6,38 +6,24 @@
 using namespace std;
 
 
-class Solution{
+class Solution {
 public:
-  int findKthLargest(vector<int> nums, int k) {
-    int ans = 0;
-    priority_queue<int,vector<int>,greater<int>> q;
-    /*for (int num : nums) {
-      q.push(num);
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int> > pq;
+        for (int n : nums) {
+            pq.push(n);
+            if (pq.size() > k)
+                pq.pop();
+        }
+        return pq.top();
     }
-    for (int i = 0; i < k; i++) {
-      ans = q.top();
-      q.pop();
-      }
-    return ans;*/
-    for (int num : nums) {
-      if (q.size() < k) {
-	q.push(num);
-      } else {
-	if (q.top() < num) {
-	  q.pop();
-	  q.push(num);
-	}
-      }
-    }
-    return q.top();
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<int> vec = {3,2,1,5,6,4}; 
-  cout << s.findKthLargest(vec, 2);
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<int> vec = {3,2,1,5,6,4}; 
+    cout << s.findKthLargest(vec, 2);
+    cout << endl;
+    return 0;
 }
