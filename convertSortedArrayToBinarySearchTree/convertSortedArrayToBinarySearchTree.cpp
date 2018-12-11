4b186f9ee5f243274abe67c3f49e4083dca25c12
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2018-12-10 20:21:11 -0800

    leetcode 108

--- convertSortedArrayToBinarySearchTree/convertSortedArrayToBinarySearchTree.cpp
+++ convertSortedArrayToBinarySearchTree/convertSortedArrayToBinarySearchTree.cpp
@@ -1,55 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-  TreeNode* sortedArrayToBST(vector<int>& nums) {
-    if (nums.size() == 0)
-      return nullptr;
-    return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
-  }
-  TreeNode* sortedArrayToBSTHelper(vector<int>& nums, int start, int end) {
-    if (start > end)
-      return nullptr;
-    int mid = start + (end - start) / 2;
-    TreeNode* root = new TreeNode(nums[mid]);
-    root->left = sortedArrayToBSTHelper(nums, start, mid - 1);
-    root->right = sortedArrayToBSTHelper(nums, mid + 1, end);
-    return root;
-  }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0)
+            return nullptr;
+        return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
+    }
+    TreeNode* sortedArrayToBSTHelper(vector<int>& nums, int start, int end) {
+        if (start > end)
+            return nullptr;
+        int mid = start + (end - start) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBSTHelper(nums, start, mid - 1);
+        root->right = sortedArrayToBSTHelper(nums, mid + 1, end);
+        return root;
+    }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {1,2,3,4,5,6,7};
-  vector<int> nums(tree, tree+7);
-  TreeNode* root = s.sortedArrayToBST(nums);
-  queue<TreeNode*> q;
-  q.push(root);
-  while (!q.empty()) {
-    int size = q.size();
-    for (int i = 0; i < size; i++) {
-      TreeNode *t = q.front();
-      if (t->left)
-	q.push(t->left);
-      if (t->right)
-	q.push(t->right);
-      cout << t->val << " ";
-      q.pop();
-      delete t;
+    Solution s;
+    int tree[7] = {1,2,3,4,5,6,7};
+    vector<int> nums(tree, tree+7);
+    TreeNode* root = s.sortedArrayToBST(nums);
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            TreeNode *t = q.front();
+            if (t->left)
+                q.push(t->left);
+            if (t->right)
+                q.push(t->right);
+            cout << t->val << " ";
+            q.pop();
+            delete t;
+        }
+        cout << endl;
     }
     cout << endl;
-  }
-  cout << endl;
-  return 0;
+    return 0;
 }
---
+++ convertSortedArrayToBinarySearchTree/convertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return null;
+        return helper(nums, 0, nums.length - 1);
+    }
+    TreeNode helper(int[] nums, int left, int right) {
+        if (left > right)
+            return null;
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, left, mid - 1);
+        node.right = helper(nums, mid + 1, right);
+        return node;
+    }
+}
