bef73f4619c23cd6e5865d837be376153163c508
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2019-06-20 22:53:08 -0700

    leetcode 246

--- strobogrammaticNumber/strobogrammaticNumber.cpp
+++ strobogrammaticNumber/strobogrammaticNumber.cpp
@@ -6,22 +6,19 @@ using namespace std;
 class Solution {
 public:
     bool isStrobogrammatic(string num) {
-        if (num.size() == 0)
-            return false;
         unordered_map<char, char> m;
-        m.insert(make_pair('6', '9'));
-        m.insert(make_pair('9', '6'));
-        m.insert(make_pair('8', '8'));
-        m.insert(make_pair('1', '1'));
-        m.insert(make_pair('0', '0'));
+        m.insert({'6', '9'});
+        m.insert({'9', '6'});
+        m.insert({'8', '8'});
+        m.insert({'1', '1'});
+        m.insert({'0', '0'});
         int left = 0, right = num.size() - 1;
         while (left <= right) {
-            if (!m.count(num[left]) || !m.count(num[right]))
+            if (m.count(num[left]) && m[num[left]] == num[right]) {
+                left++;right--;
+            } else {
                 return false;
-            else if (m[num[left]] != num[right])
-                return false;
-            left++;
-            right--;
+            }
         }
         return true;
     }
@@ -29,14 +26,14 @@ public:
 
 int main()
 {
-  Solution s;
-  string num = "101";
-  string num2 = "69";
-  string num3 = "818";
-  string num4 = "919";
-  cout << s.isStrobogrammatic(num) << endl;
-  cout << s.isStrobogrammatic(num2) << endl;
-  cout << s.isStrobogrammatic(num3) << endl;
-  cout << s.isStrobogrammatic(num4) << endl;
-  return 0;
+    Solution s;
+    string num = "101";
+    string num2 = "69";
+    string num3 = "818";
+    string num4 = "919";
+    cout << s.isStrobogrammatic(num) << endl;
+    cout << s.isStrobogrammatic(num2) << endl;
+    cout << s.isStrobogrammatic(num3) << endl;
+    cout << s.isStrobogrammatic(num4) << endl;
+    return 0;
 }
