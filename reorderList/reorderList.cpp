0c9c1587b44dc196657ab7afbf609b367766377d
jingminglake <yaxionh@yaxionhdeMacBook-Air.local>
2019-07-19 11:04:01 -0700

    leetcode 143

--- reorderList/reorderList.cpp
+++ reorderList/reorderList.cpp
@@ -1,69 +1,62 @@
 #include <iostream>
-#include <cstddef>
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-  void reorderList(ListNode* head) {
-    if (!head)
-      return;
-    // find middle node 
-    ListNode *low = head;
-    ListNode *fast = head;
-    while (fast && fast->next) {
-      low = low->next;
-      fast = fast->next->next;
+    void reorderList(ListNode* head) {
+        if (!head)
+            return;
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
+        ListNode* l1 = head, *l2 = reverseList(mid);
+        while (l2) {
+            ListNode* tmp = l2->next;
+            l2->next = l1->next;
+            l1->next = l2;
+            l2 = tmp;
+            l1 = l1->next->next;
+        }
     }
-    ListNode *middleNode = low->next;
-    cout << middleNode->val << endl;
-    // reverse second half
-    ListNode *prev = NULL;
-    ListNode *cur = middleNode;
-    while (cur) {
-      ListNode *temp = cur->next;
-      cur->next = prev;
-      prev = cur;
-      cur = temp;
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode* new_head = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return new_head;
     }
-    low->next = NULL; 
-    // merge
-    ListNode *h1 = head;
-    ListNode *h2 = prev;
-    while (h2) {
-      ListNode *temp = h2->next;
-      h2->next = h1->next;
-      h1->next = h2;
-      h1 = h2->next;
-      h2 = temp;
-    }
-  }
 };
 
 int main()
 {
-  int a[5] = {1, 2, 3, 4, 5};
-  ListNode *l = new ListNode(a[0]);
-  ListNode *tail = l;
-  for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
-    ListNode *temp = new ListNode(a[i]);
-    tail->next = temp;
-    tail = temp;
-  }
-  Solution s;
-  s.reorderList(l);
-  ListNode *h = l;
-  while (h) {
-    ListNode *p = h;
-    h = h->next;
-    cout << "delete " << p->val << " ";
-    delete p;
-  }
-  cout << endl;
-  return 0;
+    int a[5] = {1, 2, 3, 4, 5};
+    ListNode *l = new ListNode(a[0]);
+    ListNode *tail = l;
+    for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+        ListNode *temp = new ListNode(a[i]);
+        tail->next = temp;
+        tail = temp;
+    }
+    Solution s;
+    s.reorderList(l);
+    ListNode *h = l;
+    while (h) {
+        ListNode *p = h;
+        h = h->next;
+        cout << "delete " << p->val << " ";
+        delete p;
+    }
+    cout << endl;
+    return 0;
 }
