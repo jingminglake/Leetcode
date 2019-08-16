f643ba8e1290faaa6c1cf52001aca345128ab288
Yaxiong Hu <1006738657@qq.com>
2019-08-15 19:13:38 -0700

    leetcode 22

--- generateParentheses/generateParentheses_2.cpp
+++ generateParentheses/generateParentheses_2.cpp
@@ -4,29 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> generateParentheses(int n) {
-        vector<string> ans;
+    vector<string> generateParenthesis(int n) {
+        int leftP = n, rightP = n;
+        vector<string> res;
         string path;
-        if (n < 1)
-            return ans;
-        DFS(n, n, path, ans);
-        return ans;
-    
+        dfs(n, n, path, res);
+        return res;
     }
-    void DFS(int left, int right, string& path, vector<string>& ans) {
-        if (left == 0 && right == 0) {
-            ans.push_back(path);
+    void dfs(int leftP, int rightP, string& path, vector<string>& res) {
+        if (leftP == 0 && rightP == 0) {
+            res.push_back(path);
             return;
         }
-        if (left > right)
+        if (leftP < 0 || rightP < 0 || leftP > rightP)
             return;
-        if (left > 0) {
-            path.push_back('(');
-            DFS(left-1, right, path, ans);
-            path.pop_back();
-        }
-        path.push_back(')');
-        DFS(left, right-1, path, ans);
+        path += "(";
+        dfs (leftP - 1, rightP, path, res);
+        path.pop_back();
+        path += ")";
+        dfs (leftP, rightP - 1, path, res);
         path.pop_back();
     }
 };
@@ -34,7 +30,7 @@ public:
 int main()
 {
     Solution s;
-    vector<string> vec = s.generateParentheses(3);
+    vector<string> vec = s.generateParenthesis(3);
     vector<string>::iterator it = vec.begin();
     while (it != vec.end()) {
         cout << *it << endl;
