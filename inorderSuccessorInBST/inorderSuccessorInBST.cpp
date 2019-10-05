628100c9b20e7284aefdfe11293dcb62e1bc28b1
Yaxiong Hu <1006738657@qq.com>
2019-10-05 14:54:58 -0700

    leetcode 285

--- inorderSuccessorInBST/inorderSuccessorInBST.cpp
+++ inorderSuccessorInBST/inorderSuccessorInBST.cpp
@@ -3,63 +3,65 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+class Solution {
 public:
-  TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-    if(p->right){ // exists right child
-      TreeNode* succ = p->right;
-      while(succ->left)   // find right child's leftmost
-	succ = succ->left;
-      return succ;
-    } else { // find from root
-      TreeNode* succ =  NULL;
-      TreeNode* current = root;
-      while(current) {
-	if (p->val < current->val) {
-	  succ = current;
-	  current = current->left;
-	} else {
-	  current = current->right;
-	}
-      }
-      return succ;
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if (!root || !p) return nullptr;
+        TreeNode *res = nullptr;
+        if (p->right) {
+            res = p->right;
+            while (res->left) {
+                res = res->left;
+            }
+            return res;
+        } else {
+            TreeNode* cur = root;
+            while (cur && cur != p) {
+                if (p->val > cur->val) {
+                    cur = cur->right;
+                } else {
+                    res = cur;
+                    cur = cur->left;
+                }
+            }
+            return res;
+        }
     }
-  }
 };
 
 int main()
 {
-  Solution s;
-  int tree[12] = {3,2,6,1,9999,5,7,9999,9999,9999,9999,4};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[12] = {3,2,6,1,9999,5,7,9999,9999,9999,9999,4};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.inorderSuccessor(vec[0], vec[2])->val;
-  cout << endl;
-  for (TreeNode *t : vec) {
-    //cout << t->val << " ";
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.inorderSuccessor(vec[0], vec[2])->val;
+    cout << endl;
+    for (TreeNode *t : vec) {
+        //cout << t->val << " ";
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
