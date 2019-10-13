c32c9d585e35ef743d257842fcf861d85fe9d527
yaxhu <yaxhu@paypal.com>
2019-10-13 14:57:07 -0700

    leetcode 47

--- permutationsII/permutationsII_visited.cpp
+++ permutationsII/permutationsII_visited.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > permuteUnique(vector<int>& nums) {
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int> > res;
         sort(nums.begin(), nums.end());
         vector<int> path;
-        vector<bool> visited(nums.size(), false);
-        dfs(nums, res, 0, visited, path);
+        vector<int> visited(nums.size(), false);
+        dfs(nums, 0, path, res, visited);
         return res;
     }
-    void dfs(vector<int>& nums, vector<vector<int> >& res, int start, vector<bool>& visited, vector<int>& path) {
-        if (start == nums.size()) {
+    void dfs(vector<int>& nums, int level, vector<int>& path, vector<vector<int> >& res, vector<int>& visited) {
+        if (level == nums.size()) {
             res.push_back(path);
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
-            if (visited[i])
-                continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1] )
-                continue;
-            path.push_back(nums[i]);
-            visited[i] = true;
-            dfs (nums, res, start + 1, visited, path);
-            path.pop_back();
-            visited[i] = false;
+            if (!visited[i]) {
+                if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+                path.push_back(nums[i]);
+                visited[i] = true;
+                dfs(nums, level + 1, path, res, visited);
+                path.pop_back();
+                visited[i] = false;
+            }
         }
     }
 };
 
 int main()
 {
-  Solution s;
-  int a[4] = {1,2,1,2};
-  vector<int> vec(a, a+4);
-  vector<vector<int> > res = s.permuteUnique(vec);
-  vector<vector<int> >::iterator it = res.begin();
-  while (it != res.end()) {
-    for (int i : *it)
-      cout << i << " ";
-    cout << endl;
-    ++it;
-  }
-  return 0;
+    Solution s;
+    int a[4] = {1,2,1,2};
+    vector<int> vec(a, a+4);
+    vector<vector<int> > res = s.permuteUnique(vec);
+    vector<vector<int> >::iterator it = res.begin();
+    while (it != res.end()) {
+        for (int i : *it)
+            cout << i << " ";
+        cout << endl;
+        ++it;
+    }
+    return 0;
 }
