57315483f30c04a9d5d1b81acf7cd8bdc177a2d2
yaxhu <yaxhu@paypal.com>
2019-10-13 17:33:31 -0700

    leetcode 90

--- subsetsII/subsetsII_2.cpp
+++ subsetsII/subsetsII_2.cpp
@@ -5,17 +5,16 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int> > res;
         vector<int> path;
+        sort(nums.begin(), nums.end());
         dfs(nums, 0, path, res);
         return res;
     }
     void dfs(vector<int>& nums, int start, vector<int>& path, vector<vector<int> >& res) {
         res.push_back(path);
         for (int i = start; i < nums.size(); i++) {
-            if (i != start && nums[i] == nums[i - 1])
-                continue;
+            if (i > start && nums[i] == nums[i - 1]) continue;
             path.push_back(nums[i]);
             dfs(nums, i + 1, path, res);
             path.pop_back();
