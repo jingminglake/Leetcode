c72c321b63ad2176d8551bd80db469d496f32c60
yaxhu <yaxhu@paypal.com>
2019-10-13 23:01:30 -0700

    leetcode 364

--- nestListWeightSumII/nestListWeightSumII.cpp
+++ nestListWeightSumII/nestListWeightSumII.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-  int depthSumInverse(vector<NestedInteger>& nestedList) {
-    int res = 0;
-    vector<int> path;
-    int depth = 0;
-    depthSumInverseHelper(nestedList, depth, path);
-    for (int i = 0; i < path.size(); i++) {
-      res += path[i] * (path.size() - i);
+    int depthSumInverse(vector<NestedInteger>& nestedList) {
+        vector<int> level;
+        level.push_back(0);
+        int res = 0;
+        dfs(nestedList, level, 1);
+        for (int i = 1; i < level.size(); i++) {
+            res += level[i] * (level.size() - i);
+        }
+        return res;
     }
-    return res;
-  }
-  void depthSumInverseHelper(vector<NestedInteger>& nestedList, int depth, vector<int>& path) {
-    if (depth >= path.size())
-      path.resize(depth + 1);
-    for (NestedInteger& ni : nestedList) {
-      if (ni.isInteger()) {
-	path[depth] += ni.getInteger();
-	continue;
-      }
-      depthSumInverseHelper(ni.getList(), depth + 1, path);
+    void dfs(vector<NestedInteger>& nestedList, vector<int>& level, int depth) {
+        for (NestedInteger& ni : nestedList) {
+            if (depth == level.size()) level.push_back(0);
+            if (ni.isInteger()) level[depth] += ni.getInteger();
+            else dfs(ni.getList(), level, depth + 1);
+        }
     }
-  }
 };
