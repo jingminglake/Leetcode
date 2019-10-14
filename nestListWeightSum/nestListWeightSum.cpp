b5efb1b4ad607bf43e1bcdcf604568e2c63ccfd3
yaxhu <yaxhu@paypal.com>
2019-10-13 20:41:19 -0700

    leetcode 339

--- nestListWeightSum/nestListWeightSum.cpp
+++ nestListWeightSum/nestListWeightSum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-  int depthSum(vector<NestedInteger>& nestedList) {
-    int res = 0, depth = 1;
-    depthSumHelper(nestedList, res, depth);
-    return res;
-  }
-  void depthSumHelper(vector<NestedInteger>& nestedList, int& res, int depth) {
-    for (NestedInteger& ni : nestedList) {
-      if (ni.isInteger()) {
-	res += ni.getInteger() * depth;
-	continue;
-      }
-      depthSumHelper(ni.getList(), res, depth + 1);
+    int depthSum(vector<NestedInteger>& nestedList) {
+        int res = 0;
+        return dfs(nestedList, 1);
     }
-  }
+    int dfs(vector<NestedInteger>& nestedList, int depth) {
+        int res = 0;
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) res += depth * ni.getInteger();
+            else res += dfs(ni.getList(), depth + 1);
+        }
+        return res;
+    }
 };
