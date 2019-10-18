c45260930f59b799a002cf5254ee9d2bd9072418
Yaxiong Hu <1006738657@qq.com>
2019-10-18 00:32:01 -0700

    leetcode 216

--- combinationSumIII/combinationSumIII.cpp
+++ combinationSumIII/combinationSumIII.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<vector<int> > combinationSum3(int k, int n) {
-    vector<vector<int> > ans;
-    vector<int> path;
-    combinationSum3Helper(ans, k, n, path, 1);
-    return ans;
-  }
-  void combinationSum3Helper(vector<vector<int> >& ans, int k, int target, vector<int>& path, int start) {
-    if (!target && k == 0) {
-      ans.push_back(path);
-      return;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int> > res;
+        vector<int> path;
+        dfs(k, n, res, path, 1);
+        return res;
     }
-    if (!k)
-      return;
-    for (int i = start; i < 10 && target >= i*k + k*(k-1)/2; i++) {
-      path.push_back(i);
-      combinationSum3Helper(ans, k - 1, target - i, path, i + 1);
-      path.pop_back();
+    void dfs(int k, int n, vector<vector<int> >& res, vector<int>& path, int start) {
+        if (k == 0 && n == 0) {
+            res.push_back(path);
+        }
+        for (int i = start; i <= 9; i++) {
+            if (n >= i) {
+                path.push_back(i);
+                dfs(k - 1, n - i, res, path, i + 1);
+                path.pop_back();
+            }
+        }
     }
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<vector<int> > res = s.combinationSum3(3, 7);
-  for (vector<int> vec : res) {
-    for (int i : vec)
-      cout << i << " ";
+    Solution s;
+    vector<vector<int> > res = s.combinationSum3(3, 7);
+    for (vector<int> vec : res) {
+        for (int i : vec)
+            cout << i << " ";
+        cout << endl;
+    }
     cout << endl;
-  }
-  cout << endl;
-  return 0;
+    return 0;
 }
