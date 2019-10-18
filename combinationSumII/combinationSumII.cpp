03f1e217605b921f5c168152c3556ce1999b2bcc
Yaxiong Hu <1006738657@qq.com>
2019-10-17 23:44:35 -0700

    leetcode 40

--- combinationSumII/combinationSumII.cpp
+++ combinationSumII/combinationSumII.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<vector<int> > combinationSum2(vector<int>& candidates, int target) {
-    int size = candidates.size();
-    sort(candidates.begin(), candidates.end());
-    vector<vector<int> > ans;
-    vector<int> path;
-    combinationSum2Helper(ans, candidates, target, path, 0);
-    return ans;
-  }
-  void combinationSum2Helper(vector<vector<int> >& ans, vector<int>& candidates, int target, vector<int>& path, int start) {
-    if (!target) {
-      ans.push_back(path);
-      return;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int> > res;
+        vector<int> path;
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, res, path, 0);
+        return res;
     }
-    for (int i = start; i != candidates.size() && target >= candidates[i]; i++) {
-      if (i == start || candidates[i] != candidates[i - 1]) {
-	path.push_back(candidates[i]);
-	combinationSum2Helper(ans, candidates, target - candidates[i], path, i + 1);
-	path.pop_back();
-      }
+    void dfs(vector<int>& candidates, int target, vector<vector<int> >& res, vector<int>& path, int start) {
+        if (target == 0) {
+            res.push_back(path);
+        }
+        for (int i = start; i < candidates.size(); i++) {
+            if (target >= candidates[i] && (i == start || candidates[i] != candidates[i - 1])) {
+                path.push_back(candidates[i]);
+                dfs(candidates, target - candidates[i], res, path, i + 1);
+                path.pop_back();
+            }
+        }
     }
-  }
 };
 
 int main()
 {
-  Solution s;
-  int a[7] = {10,1,2,7,6,1,5};
-  vector<int> vec(a, a+7);
-  vector<vector<int> > res = s.combinationSum2(vec, 8);
-  for (vector<int> vec : res) {
-    for (int i : vec)
-      cout << i << " ";
+    Solution s;
+    int a[7] = {10,1,2,7,6,1,5};
+    vector<int> vec(a, a+7);
+    vector<vector<int> > res = s.combinationSum2(vec, 8);
+    for (vector<int> vec : res) {
+        for (int i : vec)
+            cout << i << " ";
+        cout << endl;
+    }
     cout << endl;
-  }
-  cout << endl;
-  return 0;
+    return 0;
 }
