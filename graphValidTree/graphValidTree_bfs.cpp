57f30cb7dcf756e1d52305a77b47a939cf4b3234
yaxhu <yaxhu@paypal.com>
2019-11-06 00:49:24 -0800

    leetcode 261

--- graphValidTree/graphValidTree_bfs.cpp
+++ graphValidTree/graphValidTree_bfs.cpp
@@ -1,53 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
-  bool validTree(int n, vector<pair<int, int>>& edges) {
-    vector<vector<int> > m(n, vector<int>());
-    vector<int> visited(n, 0); // 0 -> unvisited, 1 -> visiting, 2 -> visited
-    for (auto edge : edges) {
-      m[edge.first].push_back(edge.second);
-      m[edge.second].push_back(edge.first);
+    bool validTree(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int> > m;
+        for (vector<int>& edge : edges) {
+            m[edge[0]].push_back(edge[1]);
+            m[edge[1]].push_back(edge[0]);
+        }
+        queue<pair<int, int> > q;
+        q.push({-1, 0});
+        unordered_set<int> visited;
+        visited.insert(0);
+        while (!q.empty()) {
+            pair<int, int> p = q.front();
+            q.pop();
+            for (int neighbor : m[p.second]) {
+                if (neighbor != p.first) {
+                    if (visited.count(neighbor)) {
+                        return false;
+                    } else {
+                        q.push({p.second, neighbor});
+                        visited.insert(neighbor);
+                    }
+                }
+            }
+        }
+        return n == visited.size();
     }
-    if (!bfs(0, m, visited))
-      return false;
-    for (auto v : visited) {
-      if (v == 0)
-	return false;
-    }
-    return true;
-  }
-  bool bfs(int node, vector<vector<int> >& m, vector<int>& visited) {
-    queue<int> q;
-    q.push(node);
-    visited[node] = 1;
-    while (!q.empty()) {
-      int cur = q.front();
-      q.pop();
-      for (int neighbor : m[cur]) {
-	if (visited[neighbor] == 1)
-	  return false;
-	if (visited[neighbor] == 2)
-	  continue;
-	q.push(neighbor);
-	visited[neighbor] = 1;
-      }
-      visited[cur] = 2;
-    }
-    return true;
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<pair<int, int> > edges = {{0,1},
-				   {0,2},
-				   {0,3},
-				   {1,4}};
-  cout <<  s.validTree(5, edges) << endl;
-  return 0;
+    Solution s;
+    vector<vector<int> > edges = {{0,1},
+                                     {0,2},
+                                     {0,3},
+                                     {1,4}};
+    cout <<  s.validTree(5, edges) << endl;
+    return 0;
 }
