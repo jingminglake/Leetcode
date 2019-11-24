1d008df45fdaa27c029ffa4946de790a8b3ac46b
Yaxiong Hu <1006738657@qq.com>
2019-11-23 18:35:31 -0800

    leetcode 563

--- binaryTreeTilt/binaryTreeTilt.cpp
+++ binaryTreeTilt/binaryTreeTilt.cpp
@@ -1,59 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+
+class Solution {
 public:
-  int findTilt(TreeNode *root) {
-    if (!root)
-      return 0;
-    int res = 0;
-    totalValue(root, res);
-    return res;
-  }
-  int totalValue(TreeNode *root, int& res) {
-    if (!root)
-      return 0;
-    int leftValue = totalValue(root->left, res);
-    int rightValue = totalValue(root->right, res);
-    res +=  abs(leftValue - rightValue);
-    return root->val + leftValue + rightValue;
-  }
+    int findTilt(TreeNode* root) {
+        int res = 0;
+        findTiltHelper(root, res);
+        return res;
+    }
+    int findTiltHelper(TreeNode* root, int& res) {
+        if (!root) return 0;
+        int sum_left = findTiltHelper(root->left, res);
+        int sum_right = findTiltHelper(root->right, res);
+        res += abs (sum_left - sum_right);
+        return sum_left + sum_right + root->val;
+    }
 };
 
 int main()
 {
-  Solution s;
-  string str = "123#4";
-  int size = str.length();
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (str[i] != '#') {
-      vec.push_back(new TreeNode(str[i] - '0'));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    string str = "123#4";
+    int size = str.length();
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (str[i] != '#') {
+            vec.push_back(new TreeNode(str[i] - '0'));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.findTilt(vec[0]) << endl;
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.findTilt(vec[0]) << endl;
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
