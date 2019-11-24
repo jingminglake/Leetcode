cd31959ccc37391bda52ec0b291d3ee9a6f1e524
Yaxiong Hu <1006738657@qq.com>
2019-11-23 18:27:17 -0800

    leetcode 104

--- maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.cpp
+++ maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution{
 public:
-  int maxDepth(TreeNode *root) {
-    if (!root)
-      return 0;
-    return max(maxDepth(root->left), maxDepth(root->right)) + 1;
-  }
+    int maxDepth(TreeNode *root) {
+        if (!root)return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
 };
 
 int main()
 {
-  Solution s;
-  string str = "123#4";
-  int size = str.length();
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (str[i] != '#') {
-      vec.push_back(new TreeNode(str[i] - '0'));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    string str = "123#4";
+    int size = str.length();
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (str[i] != '#') {
+            vec.push_back(new TreeNode(str[i] - '0'));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  cout << s.maxDepth(vec[0]) << endl;
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    cout << s.maxDepth(vec[0]) << endl;
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
