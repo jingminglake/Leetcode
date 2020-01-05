50a8e41252925bc073d85ec8693c38aaac023fb4
yaxhu <yaxhu@paypal.com>
2020-01-05 15:56:18 -0800

    leetcode 459

--- repeatedSubstringPattern/repeatedSubstringPattern.cpp
+++ repeatedSubstringPattern/repeatedSubstringPattern.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool repeatedSubstringPattern(string s) {
-    int size = s.length();
-    if (size < 2)
-      return false;
-    for (int k = 2; k <= size; k++) {
-      if (size % k != 0)
-        continue;
-      int len = size/k;
-      string pattern = s.substr(0, len);
-      bool flag = true;
-      for (int i = 1; i < k; i++) {
-	if (pattern != s.substr(len * i, len)) {
-	  flag = false;
-	  break;
-	}
-      }
-      if (flag)
-	return true;
+    bool repeatedSubstringPattern(string s) {
+        int len = s.length();
+        if (len < 2) return false;
+        for (int i = len / 2; i >= 1; i--) {
+            if (len % i == 0) {
+                string pattern = s.substr(0, i);
+                if (canConstruct(s, pattern)) return true;
+            }
+        }
+        return false;
     }
-    return false;
-  }
+    bool canConstruct(string& s, string& pattern) {
+        int len = pattern.length();
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != pattern[i % len]) return false;
+        }
+        return true;
+    }
 };
 
 int main()
 {
-  Solution s;
-  string str = "bab";
-  cout << s.repeatedSubstringPattern(str) << endl;
-  return 0;
+    Solution s;
+    string str = "bab";
+    cout << s.repeatedSubstringPattern(str) << endl;
+    return 0;
 }
