5ee793bf1f52dfde74b2d507b283df0ea2e9f9c1
yaxhu <yaxhu@paypal.com>
2020-01-06 22:29:18 -0800

    leetcode 243

--- shortestWordDistance/shortestWordDistance.cpp
+++ shortestWordDistance/shortestWordDistance.cpp
@@ -2,35 +2,34 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int shortestDistance(vector<string>& words, string word1, string word2) {
-    int res = INT_MAX;
-    int m = -1, n = -1;
-    for (int i = 0; i < words.size(); i++) {
-      if (word1 == words[i]) {
-	m = i;
-	if (n != -1) {
-	  res = min (res, abs(m - n));
-	}
-      } 
-      if (word2 == words[i]) {
-	n = i;
-	if (m != -1) {
-	  res = min (res, abs(m - n));
-	}
-      }
+    int shortestDistance(vector<string>& words, string word1, string word2) {
+        int res = INT_MAX;
+        pair<int, int> prev = {0, 0}; // first -> wordx ; second ->index
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i] == word1) {
+                if (prev.first == 2) {
+                    res = min (res, i - prev.second);
+                }
+                prev = {1, i};
+            } else if (words[i] == word2) {
+                if (prev.first == 1) {
+                    res = min (res, i - prev.second);
+                }
+                prev = {2, i};
+            }
+        }
+        return res;
     }
-    return res;
-  }
 };
 
 int main()
 {
-  Solution s;
-  vector<string> words = {"practice", "makes", "perfect", "coding", "makes"};
-  string word1 = "coding";
-  string word2 = "practice";
-  cout << s.shortestDistance(words, word1, word2) << endl;
-  return 0;
+    Solution s;
+    vector<string> words = {"practice", "makes", "perfect", "coding", "makes"};
+    string word1 = "coding";
+    string word2 = "practice";
+    cout << s.shortestDistance(words, word1, word2) << endl;
+    return 0;
 }
