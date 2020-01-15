427f79c9b94f9a7d91152e003f02905bf5edc46d
yaxhu <yaxhu@paypal.com>
2020-01-14 20:34:28 -0800

    leetcode 344

--- reverseString/reverseString.cpp
+++ reverseString/reverseString.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution{
 public:
-  string reverseString(string s) {
-    int i = 0;
-    int j = s.length() - 1;
-    while (i < j) {
-      swap(s[i++], s[j--]);
+    string reverseString(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            swap(s[i++], s[j--]);
+        }
+        return s;
     }
-    return s;
-  }
-  void swap(char& a, char& b) {
-    char temp = a;
-    a = b;
-    b = temp;
-  }
+    void swap(char& a, char& b) {
+        char temp = a;
+        a = b;
+        b = temp;
+    }
 };
 
 int main()
 {
-  Solution s;
-  string str = "hello";
-  cout << s.reverseString(str) << endl;
-  return 0;
+    Solution s;
+    string str = "hello";
+    cout << s.reverseString(str) << endl;
+    return 0;
 }
