0843181df9f136f8970205944a139c00a976c4a6
yaxhu <yaxhu@paypal.com>
2020-01-30 23:22:22 -0800

    leetcode 20

--- validParentheses/validParentheses.cpp
+++ validParentheses/validParentheses.cpp
@@ -6,18 +6,15 @@ using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stk;
         unordered_map<char, char> m;
-        m.insert(make_pair(')', '('));
-        m.insert(make_pair('}', '{'));
-        m.insert(make_pair(']', '['));
+        m.insert({')', '('});
+        m.insert({'}', '{'});
+        m.insert({']', '['});
+        stack<char> stk;
         for (char c : s) {
             if (c == ')' || c == '}' || c == ']') {
-                if (stk.empty() || stk.top() != m[c]) {
-                    return false;
-                } else {
-                    stk.pop();
-                }
+                if (stk.empty() || stk.top() != m[c]) return false;
+                else stk.pop();
             } else {
                 stk.push(c);
             }
@@ -28,7 +25,7 @@ public:
 
 int main()
 {
-  Solution s;
-  cout << s.isValid(string("{}{}{}{}[][]")) << endl;
-  return 0;
+    Solution s;
+    cout << s.isValid(string("{}{}{}{}[][]")) << endl;
+    return 0;
 }
