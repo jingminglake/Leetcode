4a7b66b0fb95c2d2b4b43172b873ff70489c8ce9
yaxhu <yaxhu@paypal.com>
2020-01-30 23:44:50 -0800

    leetcode 167

--- twoSumII/twoSumII.cpp
+++ twoSumII/twoSumII.cpp
@@ -2,40 +2,37 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  vector<int> twoSum(vector<int>& numbers, int target) {
-    int size = numbers.size();
-    vector<int> ans;
-    int left = 0, right = size - 1;
-    while (left < right) {
-      if (numbers[left] + numbers[right] < target) {
-	left++;
-      }
-      else if (numbers[left] + numbers[right] > target) {
-	right--;
-      }
-      else {
-	ans.push_back(left + 1);
-	ans.push_back(right + 1);
-	break;
-      }	
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        int left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum > target) { 
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                res = {left + 1, right + 1};
+                break;
+            }
+        }
+        return res;
     }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int a[4] = {2, 7, 11, 15};
-  vector<int> vec(a,a+4);
-  vector<int> ans = s.twoSum(vec, 9);
-  vector<int>::iterator it = ans.begin();
-  while (it != ans.end()) {
-    cout << *it << " ";
-    ++it;
-  }
-  cout << endl;
-  return 0;
+    Solution s;
+    int a[4] = {2, 7, 11, 15};
+    vector<int> vec(a,a+4);
+    vector<int> ans = s.twoSum(vec, 9);
+    vector<int>::iterator it = ans.begin();
+    while (it != ans.end()) {
+        cout << *it << " ";
+        ++it;
+    }
+    cout << endl;
+    return 0;
 }
