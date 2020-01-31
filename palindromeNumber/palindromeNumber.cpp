e288884d5cdf63c4b03121ac0a91450f75ff8d3e
yaxhu <yaxhu@paypal.com>
2020-01-31 01:24:35 -0800

    leetcode 9

--- palindromeNumber/palindromeNumber.cpp
+++ palindromeNumber/palindromeNumber.cpp
@@ -1,34 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  bool isPalindrome(int x) {
-    if(x < 0)
-      return false;
-    bool ans = true;
-    int high = 1, low = 1;
-    int xx = x;
-    while(xx >= 10) {
-      high *= 10;
-      xx = xx / 10;
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int64_t res = 0;
+        int64_t num = x;
+        while (num) {
+            res = res * 10 + num % 10;
+            num = num / 10;
+        }
+        return x == res;
     }
-    while(high > low) {
-      if( ((x / high) % 10) != ((x / low) % 10) ){	
-	ans = false;
-	break;
-      }
-      high = high / 10;
-      low = low * 10;
-    }
-    //cout << high << endl;
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  cout << s.isPalindrome(10) << endl;
-  return 0;
+    Solution s;
+    cout << s.isPalindrome(10) << endl;
+    return 0;
 }
