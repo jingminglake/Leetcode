b2c954dbfbffa680d99950e202b1e34e466a484b
yaxhu <yaxhu@paypal.com>
2020-01-31 01:06:52 -0800

    leetcode 26

--- removeDupFromSortedArray/removeDupFromSortedArray.cpp
+++ removeDupFromSortedArray/removeDupFromSortedArray.cpp
@@ -5,12 +5,11 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() < 2)
-            return nums.size();
-        int res = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1])
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
                 nums[res++] = nums[i];
+            }
         }
         return res;
     }
@@ -18,12 +17,12 @@ public:
 
 int main()
 {
-  Solution s;
-  int a[5] = {1,1,2,4,5};
-  vector<int> vec(a, a+5);
-  int size = s.removeDuplicates(vec);
-  for (int i = 0; i < size; i++)
-      cout << vec[i] << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    int a[5] = {1,1,2,4,5};
+    vector<int> vec(a, a+5);
+    int size = s.removeDuplicates(vec);
+    for (int i = 0; i < size; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+    return 0;
 }
