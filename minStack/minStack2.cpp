c89304c62b4d7002a8fb1412a2843c573a3a7070
yaxhu <yaxhu@paypal.com>
2020-01-31 22:00:27 -0800

    leetcode 155

--- minStack/minStack2.cpp
+++ minStack/minStack2.cpp
@@ -2,50 +2,51 @@
 #include <stack>
 using namespace std;
 
-class MinStack{
+class MinStack {
 public:
-  MinStack() {
-    minV = INT_MAX;
-  }
-  void push(int x) {
-    if (x <= minV) {
-      s.push(minV);
-      minV = x;
+    /** initialize your data structure here. */
+    MinStack() {
+        minV = INT_MAX;
     }
-    s.push(x);
-  }
-  void pop() {
-    if (s.empty())
-      return;
-    if(s.top() == minV) {
-      s.pop();
-      minV = s.top();
+    
+    void push(int x) {
+        if (x <= minV) {
+            s.push(minV);
+            minV = x;
+        }
+        s.push(x);
     }
-    s.pop();
-  }
-  int top() {
-    if (s.empty())
-      return -1; 
-    return s.top();
-  }
-  int getMin() {
-    if (s.empty())
-      return 0;
-    return minV;
-  }
-  stack<int> s;
-  int minV;
+    
+    void pop() {
+        if (!s.empty()) {
+            if (s.top() == minV) {
+                s.pop();
+                minV = s.top();
+            }
+            s.pop();
+        }
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minV;
+    }
+    int minV;
+    stack<int> s;
 };
 
 int main()
 {
-   MinStack obj;
-   int i = 1;
-   while (i < 10)
-     obj.push(i++);
-   obj.pop();
-   int param_3 = obj.top();
-   bool param_4 = obj.getMin();
-   cout << param_3 << " " << param_4 << endl;
-   return 0;
+    MinStack obj;
+    int i = 1;
+    while (i < 10)
+        obj.push(i++);
+    obj.pop();
+    int param_3 = obj.top();
+    bool param_4 = obj.getMin();
+    cout << param_3 << " " << param_4 << endl;
+    return 0;
 }
