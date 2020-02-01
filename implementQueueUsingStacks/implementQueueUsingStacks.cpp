257525a203bc055fdf3941e6a49a203d851bc107
yaxhu <yaxhu@paypal.com>
2020-02-01 01:11:27 -0800

    leetcode 232

--- implementQueueUsingStacks/implementQueueUsingStacks.cpp
+++ implementQueueUsingStacks/implementQueueUsingStacks.cpp
@@ -4,43 +4,43 @@ using namespace std;
 
 class MyQueue{
 public:
-  MyQueue() {
+    MyQueue() {
     
-  }
-  void push(int x) {
-    input.push(x);
-  }
-  int pop() {
-    int res = peek();
-    output.pop();
-    return res;
-  }
-  int peek() {
-    if (empty())
-      return -1;
-    if (output.empty()) {
-      while (input.size()) {
-	output.push(input.top());
-	input.pop();
-      }
     }
-    return output.top();
-  }
-  bool empty() {
-    return input.empty() && output.empty();
-  }
-  stack<int> input, output;
+    void push(int x) {
+        input.push(x);
+    }
+    int pop() {
+        int res = peek();
+        output.pop();
+        return res;
+    }
+    int peek() {
+        if (empty())
+            return -1;
+        if (output.empty()) {
+            while (input.size()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+    stack<int> input, output;
 };
 
 int main()
 {
-   MyQueue obj;
-   int i = 1;
-   while (i < 10)
-     obj.push(i++);
-   int param_2 = obj.pop();
-   int param_3 = obj.peek();
-   bool param_4 = obj.empty();
-   cout << param_2 << " " << param_3 << " " << param_4 << endl;
-   return 0;
+    MyQueue obj;
+    int i = 1;
+    while (i < 10)
+        obj.push(i++);
+    int param_2 = obj.pop();
+    int param_3 = obj.peek();
+    bool param_4 = obj.empty();
+    cout << param_2 << " " << param_3 << " " << param_4 << endl;
+    return 0;
 }
