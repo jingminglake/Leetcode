a0ae0b06f63c3036b5c5e76fb1dc82e609762d1f
yaxhu <yaxhu@paypal.com>
2020-02-01 12:23:21 -0800

    leetcode 102

--- binaryTreeLevelOrderTraversal/binaryTreeLevelOrderTraversal.cpp
+++ binaryTreeLevelOrderTraversal/binaryTreeLevelOrderTraversal.cpp
@@ -4,70 +4,67 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution{
+
+class Solution {
 public:
-  vector<vector<int> > levelOrder(TreeNode *root) {
-    vector<vector<int> > ans;
-    if (!root)
-      return ans;
-    queue<TreeNode* > q;
-    q.push(root);
-    while (!q.empty()) {
-      vector<int> tempV;
-      int size = q.size();
-      for (int i = 0; i < size; i++) {
-	TreeNode *temp = q.front();
-	tempV.push_back(temp->val);
-	q.pop();
-	if (temp->left)
-	  q.push(temp->left);
-	if (temp->right)
-	  q.push(temp->right);
-      }
-      ans.push_back(tempV);
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int q_size = q.size();
+            vector<int> level;
+            for (int i = 0; i < q_size; i++) {
+                TreeNode* n = q.front(); q.pop();
+                level.push_back(n->val);
+                if (n->left) q.push(n->left);
+                if (n->right) q.push(n->right);
+            }
+            res.push_back(level);
+        }
+        return res;
     }
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {3,9,20,9999,9999,15,7};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[7] = {3,9,20,9999,9999,15,7};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  vector<vector<int> > res = s.levelOrder(vec[0]);
-  for (vector<int> &v : res) {
-    for (int i : v) {
-      cout << i << " ";
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
     }
-    cout << endl;
-  }
-  for (TreeNode *t : vec) {
-    //cout << t->val << " ";
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    vector<vector<int> > res = s.levelOrder(vec[0]);
+    for (vector<int> &v : res) {
+        for (int i : v) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    for (TreeNode *t : vec) {
+        //cout << t->val << " ";
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
