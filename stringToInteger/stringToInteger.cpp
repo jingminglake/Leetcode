6d127d8c7099bba7198c26cd580c21d5c5b6942b
yaxhu <yaxhu@paypal.com>
2020-02-01 18:43:49 -0800

    leetcode 8

--- stringToInteger/stringToInteger.cpp
+++ stringToInteger/stringToInteger.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int myAtoi(string str) {
-    long long ans = 0;
-    int i = 0;
-    int sign = 1;
-    while(str[i] == ' ')
-      i++;
-    if(str[i] == '+' || str[i] == '-') {
-      if(str[i] == '-')
-	sign = -1;
-      i++;
+    int myAtoi(string str) {
+        long long res = 0;
+        int i = 0;
+        int sign = 1;
+        while(str[i] == ' ') i++;
+        if(str[i] == '+' || str[i] == '-') {
+            if(str[i] == '-') sign = -1;
+            i++;
+        }
+        while(str[i] >= '0' && str[i] <= '9') {
+            res *= 10;
+            res += (int) (str[i] - '0');
+            i++;
+            if (res * sign > INT_MAX)
+                return INT_MAX;
+            else if(res * sign < INT_MIN)
+                return INT_MIN; 
+        }
+        return res * sign;  
     }
-    while(isDigit(str[i])) {
-      ans *= 10;
-      ans += (int) (str[i] - '0');
-      i++;
-      if (ans * sign > INT_MAX)
-	return INT_MAX;
-      else if(ans * sign < INT_MIN)
-	return INT_MIN; 
-    }
-    return ans * sign;  
-  }
-  bool isDigit(char& c) {
-    if(c < '0' || c > '9')
-      return false;
-    else
-      return true;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string ss = "-1123111111111111111";
-  //int a = atoi(ss.c_str());
+    Solution s;
+    string ss = "-1123111111111111111";
+    //int a = atoi(ss.c_str());
   
-  cout << s.myAtoi(ss) << endl;
-  return 0;
+    cout << s.myAtoi(ss) << endl;
+    return 0;
 }
