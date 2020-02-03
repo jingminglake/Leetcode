390479b2e8adb6c16a16eac9073d06b9594fae94
yaxhu <yaxhu@paypal.com>
2020-02-02 20:01:49 -0800

    leetcode 207

--- courseSchedule/courseSchedule_adjacentList.cpp
+++ courseSchedule/courseSchedule_adjacentList.cpp
@@ -5,17 +5,16 @@ using namespace std;
 
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int> > neighbors(numCourses);
         vector<int> indegree(numCourses);
         for (auto& p : prerequisites) {
-            neighbors[p.second].push_back(p.first);
-            indegree[p.first]++;
+            neighbors[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
         }
         queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
+            if (indegree[i] == 0) q.push(i);
         }
         int count = 0;
         while (!q.empty()) {
@@ -34,12 +33,12 @@ public:
 int main()
 {
     Solution s;
-    vector<pair<int, int> > prerequisites;
-    prerequisites.push_back(make_pair(0,3));
-    prerequisites.push_back(make_pair(1,3));
-    prerequisites.push_back(make_pair(2,3));
-    prerequisites.push_back(make_pair(4,3));
-    prerequisites.push_back(make_pair(5,4));
+    vector<vector<int>> prerequisites;
+    prerequisites.push_back({0,3});
+    prerequisites.push_back({1,3});
+    prerequisites.push_back({2,3});
+    prerequisites.push_back({4,3});
+    prerequisites.push_back({5,4});
     cout << s.canFinish(6, prerequisites);
     cout << endl;
     return 0;
