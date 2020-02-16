217934c728a39b9946194edec6900be328505309
yaxhu <yaxhu@paypal.com>
2020-02-16 15:30:22 -0800

    leetcode 297

--- serializeAndDeserializeBinaryTree/serializeAndDeserializeBinaryTree3.cpp
+++ serializeAndDeserializeBinaryTree/serializeAndDeserializeBinaryTree3.cpp
@@ -11,36 +11,69 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// Your Codec object will be instantiated and called as such:
-// Codec codec;
-// codec.deserialize(codec.serialize(root));
 class Codec {
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if (!root)
-            return "# ";
-        string res = to_string(root->val) + " " + serialize(root->left) + serialize(root->right);
+        string res;
+        if (!root) return res;
+        TreeNode* cur = root;
+        queue<TreeNode*> q;
+        q.push(cur);
+        while (!q.empty()) {
+            TreeNode *t = q.front(); q.pop();
+            if (t) { 
+                res += to_string(t->val) + " ";
+                q.push(t->left);
+                q.push(t->right);
+            } else {
+                res += "# ";
+            }
+        }
         return res;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        istringstream iss(data);
-        return deserializeHelper(iss);
-    }
-    
-    TreeNode* deserializeHelper(istringstream& iss) {
-        string node = "";
-        iss >> node;
-        if (node == "#")
-            return nullptr;
-        TreeNode* root = new TreeNode(stoi(node));
-        root->left = deserializeHelper(iss);
-        root->right = deserializeHelper(iss);
+        vector<string> vec = splitBySpace(data);
+        queue<string> q;
+        for (string& s : vec) q.push(s);
+        if (q.empty()) return nullptr;
+        string val = q.front(); q.pop();
+        queue<TreeNode*> q2;
+        TreeNode* root = new TreeNode(stoi(val));
+        q2.push(root);
+        while (!q2.empty()) {
+            TreeNode* t = q2.front(); q2.pop();
+            if (q.empty()) break;
+            string val = q.front(); q.pop();
+            if (val != "#") {
+                TreeNode* cur = new TreeNode(stoi(val));
+                t->left = cur;
+                q2.push(cur);
+            }
+            if (q.empty()) break;
+            val = q.front(); q.pop();
+            if (val != "#") {
+                TreeNode* cur = new TreeNode(stoi(val));
+                t->right = cur;
+                q2.push(cur);
+            }
+        }
         return root;
     }
+    
+    vector<string> splitBySpace(string data) {
+        vector<string> res;
+        istringstream iss(data);
+        string str;
+        while (getline(iss, str, ' ')) {
+            res.push_back(str);
+        }
+        return res;
+    }
+    
     void preorder(TreeNode* root) {
         if (!root)
             return;
