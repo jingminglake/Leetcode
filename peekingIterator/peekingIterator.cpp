be46a47b2657b7d2b3a5c6b1ab6a48a310331251
yaxhu <yaxhu@paypal.com>
2020-02-23 15:06:10 -0800

    leetcode 284

--- peekingIterator/peekingIterator.cpp
+++ peekingIterator/peekingIterator.cpp
@@ -1,48 +1,47 @@
 // Below is the interface for Iterator, which is already defined for you.
 // **DO NOT** modify the interface for Iterator.
+
 class Iterator {
     struct Data;
-	Data* data;
+    Data* data;
 public:
-	Iterator(const vector<int>& nums);
-	Iterator(const Iterator& iter);
-	virtual ~Iterator();
-	// Returns the next element in the iteration.
-	int next();
-	// Returns true if the iteration has more elements.
-	bool hasNext() const;
+    Iterator(const vector<int>& nums);
+    Iterator(const Iterator& iter);
+    virtual ~Iterator();
+    // Returns the next element in the iteration.
+    int next();
+    // Returns true if the iteration has more elements.
+    bool hasNext() const;
 };
 
 
 class PeekingIterator : public Iterator {
 public:
-	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-	    // Initialize any member here.
-	    // **DO NOT** save a copy of nums and manipulate it directly.
-	    // You should only use the Iterator interface methods.
-	    curHasNext = Iterator::hasNext();
-        if (curHasNext)
-            cur = Iterator::next();
-	}
+    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+        // Initialize any member here.
+        // **DO NOT** save a copy of nums and manipulate it directly.
+        // You should only use the Iterator interface methods.
+        curHasNext = Iterator::hasNext();
+        if (curHasNext) cur = Iterator::next();
+    }
 
     // Returns the next element in the iteration without advancing the iterator.
-	int peek() {
+    int peek() {
         return cur;
-	}
+    }
 
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	int next() {
-	    int res = cur;
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next() {
+        int res = cur;
         curHasNext = Iterator::hasNext();
-        if (curHasNext)
-            cur = Iterator::next();
+        if (curHasNext) cur = Iterator::next();
         return res;
-	}
+    }
 
-	bool hasNext() const {
-	    return curHasNext;
-	}
+    bool hasNext() const {
+        return curHasNext;
+    }
     int cur;
     bool curHasNext;
 };
