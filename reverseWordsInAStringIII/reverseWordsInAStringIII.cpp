b16197a488b2088f245207f04261757f860b0e85
yaxhu <yaxhu@paypal.com>
2020-04-05 12:27:42 -0700

    leetcode 557

--- reverseWordsInAStringIII/reverseWordsInAStringIII.cpp
+++ reverseWordsInAStringIII/reverseWordsInAStringIII.cpp
@@ -6,15 +6,16 @@ public:
     string reverseWords(string s) {
         int start = 0;
         for (int i = 0; i < s.length(); i++) {
-            if ( (i == 0 || s[i - 1] == ' ') && s[i] != ' ') {
+            if ((i - 1 < 0 || s[i - 1] == ' ') && s[i] != ' ') {
                 start = i;
-            } else if (s[i] != ' ' && (i + 1 == s.length() || s[i + 1] == ' ') && i > start) {
-                reverseHelper(s, start, i);
+            } else if (s[i] != ' ' && (i + 1 == s.length() || s[i + 1] == ' ')) {
+                reverse(s, start, i);
             }
         }
         return s;
     }
-    void reverseHelper(string& s, int start, int end) {
+    
+    void reverse(string& s, int start, int end) {
         while (start < end) {
             char c = s[start];
             s[start] = s[end];
