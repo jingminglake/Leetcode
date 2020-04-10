2244bf454fc2f445145b8c335c65a9f3055d2c07
yaxhu <yaxhu@paypal.com>
2020-04-10 15:30:20 -0700

    leetcode 443

--- stringcompression/stringcompression_2.cpp
+++ stringcompression/stringcompression_2.cpp
@@ -5,43 +5,31 @@ using namespace std;
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int res = 0;
-        if (chars.size() == 0)
-            return res;
-        chars[res++] = chars[0];
-        int cur = chars[0];
-        int count = 1;
-        for (int i = 1; i < chars.size(); i++) {
-            if (chars[i] == cur)
-                count++;
-            else {
-                if (count >= 2) {
-                    string num = to_string(count);
-                    for (char digit : num)
-                        chars[res++] = digit;
-                }
-                cur = chars[i];
-                chars[res++] = cur;
-                count = 1;
+        int index = 0;
+        int i = 0;
+        while (i < chars.size()) {
+            char cur = chars[i];
+            int cnt = 0;
+            while (i < chars.size() && chars[i] == cur) {
+                i++; cnt++;
+            }
+            chars[index++] = cur;
+            if (cnt >= 2) {
+                for (char c : to_string(cnt)) chars[index++] = c;
             }
         }
-        if (count > 1) {
-            string num = to_string(count);
-            for (char digit : num)
-                chars[res++] = digit;
-        }
-        return res;
+        return index;
     }
 };
 
 int main()
 {
-  Solution s;
-  vector<char> chars1 = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
-  vector<char> chars2 = {'a'};
-  vector<char> chars3 = {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
-  cout << s.compress(chars1) << endl;
-  cout << s.compress(chars2) << endl;
-  cout << s.compress(chars3) << endl;
-  return 0;
+    Solution s;
+    vector<char> chars1 = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    vector<char> chars2 = {'a'};
+    vector<char> chars3 = {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    cout << s.compress(chars1) << endl;
+    cout << s.compress(chars2) << endl;
+    cout << s.compress(chars3) << endl;
+    return 0;
 }
