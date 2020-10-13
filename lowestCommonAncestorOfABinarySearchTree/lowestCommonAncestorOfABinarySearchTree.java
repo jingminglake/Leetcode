cafc72244d7ea16b7d43061f570c2442be3c0be7
yaxhu <yaxhu@paypal.com>
2020-10-12 21:50:24 -0700

    leetcode 236

---
+++ lowestCommonAncestorOfABinarySearchTree/lowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode leftRes = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightRes = lowestCommonAncestor(root.right, p, q);
+        if (leftRes == null) return rightRes;
+        else if (rightRes == null) return leftRes;
+        else return root;
+    }
+}
