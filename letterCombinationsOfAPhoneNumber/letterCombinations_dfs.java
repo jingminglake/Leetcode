3156f1f109296a77833c60622cc98dee79f46436
yaxhu <yaxhu@paypal.com>
2021-01-10 19:06:06 -0800

    leetcode 17

--- letterCombinationsOfAPhoneNumber/letterCombinations_dfs.java
+++ letterCombinationsOfAPhoneNumber/letterCombinations_dfs.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> res = new ArrayList<String>();
+         List<String> res = new ArrayList<>();
         if (digits == null || digits.length() == 0) {
             return res;
         }
-        String[] digit2letters = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        String[] digit2letter = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         StringBuilder path = new StringBuilder();
-        dfs(digits, path, 0, res, digit2letters);
+        dfs(digits, digit2letter, res, path, 0);
         return res;
     }
-    void dfs(String digits, StringBuilder path, int level, List<String> res, String[] digit2letters) {
+    
+    private void dfs(String digits, String[] digit2letter, List<String> res, StringBuilder path, int level) {
         if (level == digits.length()) {
             res.add(path.toString());
             return;
         }
-        String letters = digit2letters[digits.charAt(level) - '0'];
-        if (letters.length() == 0)
-            dfs(digits, path, level + 1, res, digit2letters);
-        for (int i = 0; i < letters.length(); i++) {
-            char c = letters.charAt(i);
+        String letter = digit2letter[digits.charAt(level) - '0'];
+        for (char c : letter.toCharArray()) {
             path.append(c);
-            dfs(digits, path, level + 1, res, digit2letters);
-            path.deleteCharAt(path.length() - 1);
+            dfs(digits, digit2letter, res, path, level + 1);
+            path.deleteCharAt(path.length() - 1);;
         }
     }
 }
