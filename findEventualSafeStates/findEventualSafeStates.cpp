1a517be877c8ac6cd60241abbd747674afa50064
Yaxiong Hu <1006738657@qq.com>
2021-02-15 14:56:37 -0800

    leetcode 802

--- findEventualSafeStates/findEventualSafeStates.cpp
+++ findEventualSafeStates/findEventualSafeStates.cpp
@@ -8,31 +8,30 @@ public:
         vector<int> res;
         if (graph.size() == 0)
             return res;
-        vector<int> visited(graph.size(), 0); // 0 -> unvisited, 1 -> safe, 2->visited
+        vector<int> status(graph.size(), 0); // 0 -> unvisited, 1 -> visited, 2 -> safe, 3 -> unsafe
         for (int i = 0; i < graph.size(); i++) {
-            if (dfs(graph, i, visited))
+            if (dfs(graph, i, status) == 2)
                 res.push_back(i);
         }
         return res;
     }
-    bool dfs(vector<vector<int> >& graph, int startNode, vector<int>& visited) {
-        if (visited[startNode] != 0)
-            return visited[startNode] == 1;
-        visited[startNode] = 2;
+    int dfs(vector<vector<int> >& graph, int startNode, vector<int>& status) {
+        if (status[startNode] == 2 || status[startNode] == 3)
+            return status[startNode];
+        status[startNode] = 1;
         for (int neighbor : graph[startNode]) {
-            if (!dfs(graph, neighbor, visited))
-                return false;
+            if (status[neighbor] == 1 || dfs(graph, neighbor, status) == 3)
+                return status[startNode] = 3;
         }
-        visited[startNode] = 1;
-        return true;
+        return status[startNode] = 2;
     }
 };
 
 int main() {
-  Solution s;
-  vector<vector<int> > graph = {{1,2}, {2,3}, {5}, {0}, {5}, {}, {}};
-  for (int i : s.eventualSafeNodes(graph))
-    cout << i << " ";
-  cout << endl;
-  return 0;
+    Solution s;
+    vector<vector<int> > graph = {{1,2}, {2,3}, {5}, {0}, {5}, {}, {}};
+    for (int i : s.eventualSafeNodes(graph))
+        cout << i << " ";
+    cout << endl;
+    return 0;
 }
