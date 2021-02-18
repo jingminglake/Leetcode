19ad864578eb29656ad893a0443765fa405e65af
Yaxiong Hu <1006738657@qq.com>
2021-02-18 03:01:49 -0800

    leetcode 51

--- N-Queens/nQueens.java
+++ N-Queens/nQueens.java
@@ -1,48 +1,48 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<List<String>>();
+        List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 board[i][j] = '.';
             }
         }
-        dfs(board, n, res, 0);
+        dfs(n, board, res, 0);
         return res;
     }
     
-    private void dfs(char[][] board, int n, List<List<String>> res, int row) {
+    public void dfs(int n, char[][] board, List<List<String>> res, int row) {
         if (row == n) {
             res.add(construct(board));
             return;
         }
         for (int col = 0; col < n; col++) {
-            if (isValid(board, n, row, col)) {
+            if (isValid(board, row, col, n)) {
                 board[row][col] = 'Q';
-                dfs(board, n, res, row + 1);
+                dfs(n, board, res, row + 1);
                 board[row][col] = '.';
             }
         }
     }
-
-    private boolean isValid(char[][] board, int n, int row, int col) {
+    
+    private boolean isValid(char[][] board, int row, int col, int n) {
         //check if the column had a queen before.
-        for (int i = 0; i < row; i++)
-            if (board[i][col] == 'Q')
-                return false;
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
         //check if the 45° diagonal had a queen before.
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
-            if (board[i][j] == 'Q')
-                return false;
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
         //check if the 135° diagonal had a queen before.
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
-            if (board[i][j] == 'Q')
-                return false;
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
         return true;
     }
     
     private List<String> construct(char[][] board) {
-        List<String> res = new LinkedList<String>();
+        List<String> res = new ArrayList<String>();
         for(int i = 0; i < board.length; i++) {
             String s = new String(board[i]);
             res.add(s);
