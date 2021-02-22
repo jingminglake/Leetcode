44134ee607917599c33a0ec8bbf1be60af7dd24b
Yaxiong Hu <1006738657@qq.com>
2021-02-21 23:51:07 -0800

    leetcode 107

--- binaryTreeLevelOrderTraversalII/binaryTreeLevelOrderTraversalII.cpp
+++ binaryTreeLevelOrderTraversalII/binaryTreeLevelOrderTraversalII.cpp
@@ -4,71 +4,71 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution{
 public:
-  vector<vector<int> > levelOrderBottom(TreeNode *root) {
-    vector<vector<int> > ans;
-    if (!root)
-      return ans;
-    queue<TreeNode *> q;
-    q.push(root);
-    vector<int> vec;
-    while (!q.empty()) {
-      int levelNum = q.size();
-      TreeNode *temp;
-      for (int i = 0; i < levelNum; i++) {
-	temp = q.front();
-	if (temp->left)
-	  q.push(temp->left);
-	if (temp->right)
-	  q.push(temp->right);
-      	vec.push_back(temp->val);
-	q.pop();
-      }
-      ans.push_back(vec);
-      vec.clear();
+    vector<vector<int> > levelOrderBottom(TreeNode *root) {
+        vector<vector<int> > ans;
+        if (!root)
+            return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        vector<int> vec;
+        while (!q.empty()) {
+            int levelNum = q.size();
+            TreeNode *temp;
+            for (int i = 0; i < levelNum; i++) {
+                temp = q.front();
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+                vec.push_back(temp->val);
+                q.pop();
+            }
+            ans.push_back(vec);
+            vec.clear();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
-    reverse(ans.begin(), ans.end());
-    return ans;
-  }
 };
 
 int main()
 {
-  Solution s;
-  int tree[7] = {1,2,3,4,9999,9999,5};
-  int size = sizeof(tree)/sizeof(tree[0]);
-  vector<TreeNode *> vec;
-  for (int i = 0; i < size; i++) {
-    if (tree[i] != 9999) {
-      vec.push_back(new TreeNode(tree[i]));
-    } else {
-      vec.push_back(NULL);
+    Solution s;
+    int tree[7] = {1,2,3,4,9999,9999,5};
+    int size = sizeof(tree)/sizeof(tree[0]);
+    vector<TreeNode *> vec;
+    for (int i = 0; i < size; i++) {
+        if (tree[i] != 9999) {
+            vec.push_back(new TreeNode(tree[i]));
+        } else {
+            vec.push_back(NULL);
+        }
     }
-  }
-  for (int i = 0; i < size/2; i++) {
-    if(!vec[i])
-      continue;
-    if (i*2 + 1 < size)
-      vec[i]->left = vec[i*2 + 1];
-    if (i*2 + 2 < size)
-      vec[i]->right = vec[i*2 + 2];
-  }
-  vector<vector<int> > levels = s.levelOrderBottom(vec[0]);
-  for (vector<int>& level : levels) {
-    for (int i : level)
-      cout << i << " ";
-    cout << endl;
-  }
-  for (TreeNode *t : vec) {
-    delete t;
-  }
-  vec.clear();
-  return 0;
+    for (int i = 0; i < size/2; i++) {
+        if(!vec[i])
+            continue;
+        if (i*2 + 1 < size)
+            vec[i]->left = vec[i*2 + 1];
+        if (i*2 + 2 < size)
+            vec[i]->right = vec[i*2 + 2];
+    }
+    vector<vector<int> > levels = s.levelOrderBottom(vec[0]);
+    for (vector<int>& level : levels) {
+        for (int i : level)
+            cout << i << " ";
+        cout << endl;
+    }
+    for (TreeNode *t : vec) {
+        delete t;
+    }
+    vec.clear();
+    return 0;
 }
---
+++ binaryTreeLevelOrderTraversalII/binaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int q_size = q.size();
+            List<Integer> levelRes = new LinkedList<>();
+            for (int i = 0; i < q_size; i++) {
+                TreeNode cur = q.poll();
+                levelRes.add(cur.val);
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+            res.add(0, levelRes);
+        }
+        return res;
+    }
+}
