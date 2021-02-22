ff8def2886277405a0a4751296aa57b8fc865e62
Yaxiong Hu <1006738657@qq.com>
2021-02-21 22:54:02 -0800

    leetcode 399

--- evaluateDivision/evaluateDivision.java
+++ evaluateDivision/evaluateDivision.java
@@ -1,43 +1,41 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         Map<String, Map<String, Double>> graph = new HashMap<>();
-        for (int i = 0; i < values.length; i++) {
-            if (!graph.containsKey(equations.get(i).get(0))) {
-                graph.put(equations.get(i).get(0), new HashMap<>());
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            if (!graph.containsKey(equation.get(0))) {
+                graph.put(equation.get(0), new HashMap<>());
             }
-            if (!graph.containsKey(equations.get(i).get(1))) {
-                graph.put(equations.get(i).get(1), new HashMap<>());
+            graph.get(equation.get(0)).put(equation.get(1), values[i]);
+            if (!graph.containsKey(equation.get(1))) {
+                graph.put(equation.get(1), new HashMap<>());
             }
-            graph.get(equations.get(i).get(0)).put(equations.get(i).get(1), values[i]);
-            graph.get(equations.get(i).get(1)).put(equations.get(i).get(0), 1 / values[i]);
+            graph.get(equation.get(1)).put(equation.get(0), 1 / values[i]);
         }
         double[] res = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
-            String start = queries.get(i).get(0);
-            String end = queries.get(i).get(1);
-            if (!graph.containsKey(start) || !graph.containsKey(end)) {
+            if (!graph.containsKey(queries.get(i).get(0)) || !graph.containsKey(queries.get(i).get(1))) {
                 res[i] = -1.0;
             } else {
-                res[i] = dfs(start, end, graph, new HashSet<>());
+                res[i] = dfs(graph, queries.get(i).get(0), queries.get(i).get(1), new HashSet<>());
             }
         }
         return res;
     }
     
-    private double dfs(String start, String end, Map<String, Map<String, Double>> graph, Set<String> visited) {
-        if (visited.contains(start)) return -1.0;
-        if (start.equals(end)) return 1.0;
-        visited.add(start);
-        Map<String, Double> pairs = graph.get(start);
-        double res = -1.0;
-        for (String neighbor : pairs.keySet()) {
+    public double dfs(Map<String, Map<String, Double>> graph, String start, String end, Set<String> visited) {
+        if (start.equals(end)) return 1;
+        
+        for (String neighbor : graph.get(start).keySet()) {
             if (!visited.contains(neighbor)) {
-                res = dfs(neighbor, end, graph, visited);
-                if (res != -1.0) {
-                    return res * pairs.get(neighbor);
+                visited.add(neighbor);
+                double neighorRes = dfs(graph, neighbor, end, visited);
+                if (neighorRes != -1.0) {
+                    return graph.get(start).get(neighbor) * neighorRes;
                 }
+                visited.remove(neighbor);
             }
         }
-        return res;
+        return -1;
     }
 }
