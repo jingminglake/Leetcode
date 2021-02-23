bda01607cb479d6f1f5fcd930eb24821f6f7c7dc
Yaxiong Hu <1006738657@qq.com>
2021-02-22 19:52:37 -0800

    leetcode 207

--- courseSchedule/courseSchedule_adjacentList.java
+++ courseSchedule/courseSchedule_adjacentList.java
@@ -1,38 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(numCourses <= 0)
-            return false;
-        if(prerequisites == null || prerequisites.length == 0)
-            return true;
-        Map<Integer, Set<Integer> > graph = new HashMap<>();
-        for (int i = 0; i < prerequisites.length; i++) {
-            //System.out.println("++++++" + prerequisites[i][1] + "++++" + prerequisites[i][0]);
-            graph.putIfAbsent(prerequisites[i][1], new HashSet<Integer>());
-            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int[] pre : prerequisites) {
+            if (!graph.containsKey(pre[1])) {
+                graph.put(pre[1], new HashSet<>());
+            }
+            graph.get(pre[1]).add(pre[0]);
         }
-        //System.out.println("+++" + graph.size());
         int[] indegree = new int[numCourses];
         for (Set<Integer> s : graph.values()) {
-            //System.out.println("====");
-            for (Integer i : s) {
-                indegree[i]++;
-            }
+            for (Integer i : s) indegree[i]++;
         }
         Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < indegree.length; i++) {
-            //System.out.print(indegree[i]);
-            if (indegree[i] == 0)
-                q.offer(i);
+            if (indegree[i] == 0) q.offer(i);
         }
-        int num = 0;
+        int visitedNum = 0;
         while (!q.isEmpty()) {
-            int node = q.poll();
-            num++;
-            for (Integer n : graph.getOrDefault(node, new HashSet<Integer>()) ) {
-                if (--indegree[n] == 0)
-                    q.offer(n);
+            Integer node = q.poll();
+            visitedNum++;
+            Set<Integer> neighbors = graph.containsKey(node) ? graph.get(node) : new HashSet<>();
+            for (Integer neighbor : neighbors) {
+                if (--indegree[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
             }
         }
-        return numCourses == num;
+        return visitedNum == numCourses;
     }
 }
