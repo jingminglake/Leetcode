bd98593e0047a9a0a0a71c825d6aeb47ecb471d1
Yaxiong Hu <1006738657@qq.com>
2021-03-15 02:45:12 -0700

    leetcode 278

--- firstBadVersion/firstBadVersion.java
+++ firstBadVersion/firstBadVersion.java
@@ -1,18 +1,8 @@
-#include <iostream>
-using namespace std;
-
-bool isBadVersion(int num) {
-    if (num >= 8)
-        return true;
-    else
-        return false;
-}
-
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
-class Solution{
-public:
-    int firstBadVersion(int n) {
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
         if (n == 1 && isBadVersion(1)) return 1;
         int left = 1, right = n;
         while (left + 1 < right) {
@@ -27,11 +17,4 @@ public:
         if (isBadVersion(right)) return right;
         return -1;
     }
-};
-
-int main()
-{
-    Solution s;
-    cout << s.firstBadVersion(10) << endl;
-    return 0;
 }
