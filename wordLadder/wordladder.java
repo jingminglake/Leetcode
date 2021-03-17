647d843d9e9c583e34b32e4740fc3ad4389770ac
Yaxiong Hu <1006738657@qq.com>
2021-03-17 02:02:11 -0700

    leetcode 127

--- wordLadder/wordladder.java
+++ wordLadder/wordladder.java
@@ -2,25 +2,31 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>();
         for (String word : wordList) wordSet.add(word);
-        int res = 0;
+        
+        if (!wordSet.contains(endWord)) return 0;
+        
         Queue<String> q = new LinkedList<>();
         q.offer(beginWord);
+        wordSet.remove(beginWord);
+        int level = 1;
         while (!q.isEmpty()) {
+            level++;
             int q_size = q.size();
-            res++;
             for (int i = 0; i < q_size; i++) {
-                String curWord = q.poll();
-                for (int index = 0; index < curWord.length(); index++) {
-                    char[] curWordChars = curWord.toCharArray();
+                String cur = q.poll();
+                char[] curChars = cur.toCharArray();
+                for (int k = 0; k < cur.length(); k++) {
+                    char origin = curChars[k]; 
                     for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == curWordChars[index]) continue;
-                        curWordChars[index] = c;
-                        String nextWord = new String(curWordChars);
-                        if (!wordSet.contains(nextWord)) continue;
-                        if (nextWord.equals(endWord)) return res + 1;
-                        wordSet.remove(nextWord);
-                        q.offer(nextWord);
+                        if (c == curChars[k]) continue;
+                        curChars[k] = c;
+                        String new_word = new String(curChars);
+                        if (!wordSet.contains(new_word)) continue;
+                        if (endWord.equals(new_word)) return level;
+                        q.offer(new_word);
+                        wordSet.remove(new_word);
                     }
+                    curChars[k] = origin; 
                 }
             }
         }
