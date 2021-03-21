8b3b32c4c153b811330f195be3a23b6839cbee74
Yaxiong Hu <1006738657@qq.com>
2021-03-20 20:59:09 -0700

    leetcode 240

---
+++ searchA2DMatrixII/searchA2DMatrixII.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (target > matrix[i][j]) {
+                i++;
+            } else if (target < matrix[i][j]) {
+                j--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
