bb2f6d4c6a1988a48253779d6b7547de251401f9
Yaxiong Hu <1006738657@qq.com>
2021-03-28 18:59:46 -0700

    leetcode 162

--- findPeakElement/findPeakElement.java
+++ findPeakElement/findPeakElement.java
@@ -1,15 +1,10 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-        int len = nums.size();
+    public int findPeakElement(int[] nums) {
+        int len = nums.length;
         if (len == 1) return 0;
         if (nums[0] > nums[1]) return 0;
         if (nums[len - 1] > nums[len - 2]) return len - 1;
-        int left = 1, right = nums.size() - 2;
+        int left = 1, right = nums.length - 2;
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
@@ -23,14 +18,4 @@ public:
         if (nums[left] < nums[right]) return right;
         else return left;
     }
-};
-
-int main()
-{
-    Solution s;
-    int a[4] = {1,2,3,1};
-    vector<int> vec(a, a+2);
-    cout <<  s.findPeakElement(vec);
-    cout << endl;
-    return 0;
 }
