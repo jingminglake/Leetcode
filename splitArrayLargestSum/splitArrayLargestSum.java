3d3a90451fd682222110bbad9b65a25da348ca12
Yaxiong Hu <1006738657@qq.com>
2021-04-02 19:04:42 -0700

    leetcode 410

--- splitArrayLargestSum/splitArrayLargestSum.java
+++ splitArrayLargestSum/splitArrayLargestSum.java
@@ -1,11 +1,6 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
-public:
-    int splitArray(vector<int>& nums, int m) {
-        if (nums.size() == 1) return nums[0];
+    public int splitArray(int[] nums, int m) {
+        if (nums.length == 1) return nums[0];
         long left = nums[0], right = 0;
         for (int n : nums) {
             if (n > left) left = n;
@@ -24,25 +19,16 @@ public:
         else return -1;
     }
     
-    bool lessEqualM(vector<int>& nums, int m, long mid) {
+    public boolean lessEqualM(int[] nums, int m, long mid) {
         int n = 0;
         int cur = 0;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.length; i++) {
             cur += nums[i];
-            if (i == nums.size() - 1 || cur + nums[i + 1] > mid) {
+            if (i == nums.length - 1 || cur + nums[i + 1] > mid) {
                 cur = 0;
                 if (++n > m) return false; 
             }
         }
         return true;
     }
-};
-
-int main()
-{
-    Solution s;
-    vector<int> nums = {7,2,5,10,8};
-    cout << s.splitArray(nums, 2);
-    cout << endl;
-    return 0;
 }
