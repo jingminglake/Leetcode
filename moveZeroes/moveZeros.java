f2277f840da9c2b022d163708239fa174203235b
Yaxiong Hu <1006738657@qq.com>
2021-04-14 20:10:02 -0700

    leetcode 283

--- moveZeroes/moveZeros.java
+++ moveZeroes/moveZeros.java
@@ -1,14 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return;
-        int left = 0, right = 0;
-        while (right < nums.length) {
-            if (nums[right] != 0)
-                nums[left++] = nums[right];
-            right++;
+        int last = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[last];
+                nums[last++] = nums[i];
+                nums[i] = temp;
+            }
         }
-        while (left < nums.length)
-            nums[left++] = 0;
     }
 }
