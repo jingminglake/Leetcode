5a7ebf919a688fa7be08b186e79ed05717691f00
Yaxiong Hu <1006738657@qq.com>
2021-04-21 02:33:44 -0700

    leetcode 640

--- solveTheEquation/solveTheEquation.java
+++ solveTheEquation/solveTheEquation.java
@@ -1,23 +1,29 @@
 class Solution {
     public String solveEquation(String equation) {
-        int[] left = evalExp(equation.split("=")[0]);
-        int[] right = evalExp(equation.split("=")[1]);
+        if (equation == null || !equation.contains("=")) return "No solution";
+        String[] part = equation.split("=");
+        int[] leftRes = evalExp(part[0]);
+        int[] rightRes = evalExp(part[1]);
         int[] res = new int[2];
-        res[0] = left[0] - right[0];
-        res[1] = right[1] - left[1];
+        res[0] = leftRes[0] - rightRes[0];
+        res[1] = rightRes[1] - leftRes[1];
         if (res[0] == 0 && res[1] == 0) return "Infinite solutions";
         if (res[0] == 0) return "No solution";
         return "x=" + res[1] / res[0];
     }
-    public int[] evalExp(String exp) {
+    
+    private int[] evalExp(String exp) {
+        int res[] = new int[2];
+        if (exp == null) return res;
         String[] tokens = exp.split("(?=[-+])");
-        int[] res = new int[2];
         for (String token : tokens) {
-            if (token.equals("+x") || token.equals("x")) res[0] += 1;
-            else if (token.equals("-x")) res[0] -= 1;
+            //System.out.print(token + " ");
+            if (token.equals("+x") || token.equals("x")) res[0]++;
+            else if (token.equals("-x")) res[0]--;
             else if (token.contains("x")) res[0] += Integer.parseInt(token.substring(0, token.indexOf("x")));
             else res[1] += Integer.parseInt(token);
         }
+        //System.out.println();
         return res;
     }
 }
