87b3d434c40905c031211f847cd5cf4521326819
Yaxiong Hu <1006738657@qq.com>
2021-04-24 17:48:03 -0700

    leetcode 680

--- validPalindromeII/validPalindromeII.cpp
+++ validPalindromeII/validPalindromeII.cpp
@@ -2,41 +2,33 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-  bool validPalindrome(string s) {
-    int left = 0, right = s.length() - 1;
-    while (left <= right) {
-      if (s[left] == s[right]) {
-	left++;
-	right--;
-      } else {
-	return validPalindromeHelper(s, left + 1, right) || validPalindromeHelper(s, left, right - 1);
-      }
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+               return validPalindromeHelper(s, left + 1, right) || validPalindromeHelper(s, left, right - 1); 
+            } else {
+               left++; right--;
+            }
+        }
+        return true;
     }
-    return true;
-  }
-  bool validPalindromeHelper(string s, int i, int j) {
-    if (i > j)
-      return false;
-    while (i <= j) {
-      if (s[i] == s[j]) {
-	i++;
-	j--;
-      } else
-	return false;
+    bool validPalindromeHelper(string s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) return false;
+        }
+        return true;
     }
-    return true;
-  }
 };
 
 int main()
 {
-  Solution s;
-  string ss = "aba";
-  string ss1 = "abca";
-  cout << s.validPalindrome(ss) << endl;
-  cout << s.validPalindrome(ss1) << endl;
-  return 0;
+    Solution s;
+    string ss = "aba";
+    string ss1 = "abca";
+    cout << s.validPalindrome(ss) << endl;
+    cout << s.validPalindrome(ss1) << endl;
+    return 0;
 }
---
+++ validPalindromeII/validPalindromeII.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            } else {
+                left++; right--;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
