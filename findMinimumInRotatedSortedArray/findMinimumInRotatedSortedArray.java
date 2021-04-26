7efa44c70642789664f9e4bf87812ffdb0e20d37
Yaxiong Hu <1006738657@qq.com>
2021-04-26 16:39:34 -0700

    leetcode 153

--- findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.java
+++ findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.java
@@ -1,18 +1,17 @@
+// 153, all nums are unique
+
 class Solution {
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
-        if (right == 0) return nums[0];
-        if (nums[left] <= nums[right]) return nums[left];
-        
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] >= nums[0]) {
+            if (nums[mid] > nums[right]) { // mid must be in left part
                 left = mid;
-            } else {
+            } else { // mid must be in right part
                 right = mid;
             }
         }
-        if (nums[left] <= nums[right]) return nums[left];
+        if (nums[left] < nums[right]) return nums[left];
         else return nums[right];
     }
 }
