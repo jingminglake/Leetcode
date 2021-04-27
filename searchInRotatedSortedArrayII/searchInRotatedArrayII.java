350076da521576a8d5166b9c9a411861ab7bbe6a
Yaxiong Hu <1006738657@qq.com>
2021-04-26 22:33:10 -0700

    leetcode 81

--- searchInRotatedSortedArrayII/searchInRotatedArrayII.java
+++ searchInRotatedSortedArrayII/searchInRotatedArrayII.java
@@ -3,24 +3,24 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
-            if (target == nums[mid])
-                return true;
-            if (nums[mid] > nums[right]) {
-                if (nums[left] <= target && target < nums[mid])
+            if (nums[mid] == target) return true;
+            if (nums[mid] > nums[right]) { // mid in left part
+                if (nums[left] <= target && target < nums[mid]) {
                     right = mid;
-                else
-                    left = mid; 
-            } else if (nums[mid] < nums[right]){
-                if (nums[mid] < target && target <= nums[right])
+                } else {
                     left = mid;
-                else 
+                } 
+            } else if (nums[mid] < nums[right]) { // mid in right part
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid;
+                } else {
                     right = mid;
+                }
             } else {
                 right--;
             }
         }
-        if (nums[left] == target || nums[right] == target)
-            return true;
+        if (nums[left] == target || nums[right] == target) return true;
         else return false;
     }
 }
