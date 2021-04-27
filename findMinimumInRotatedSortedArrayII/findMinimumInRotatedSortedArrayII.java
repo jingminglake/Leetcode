7c5761a2a2ebd5cb5d9addd0e36cae46d8aa7e17
Yaxiong Hu <1006738657@qq.com>
2021-04-26 17:35:37 -0700

    leetcode 154

--- findMinimumInRotatedSortedArrayII/findMinimumInRotatedSortedArrayII.java
+++ findMinimumInRotatedSortedArrayII/findMinimumInRotatedSortedArrayII.java
@@ -1,27 +1,17 @@
 class Solution {
     public int findMin(int[] nums) {
-        int n = nums.length;
-        if (n == 1 || nums[0] < nums[n - 1]) return nums[0];
-        int left = 0, right = n - 1;
+        int left = 0, right = nums.length - 1;
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
-            if (nums[left] == nums[right]) {
-                if (nums[mid] > nums[left]) {
-                    left = mid;
-                } else if (nums[mid] < nums[left]) {
-                    right = mid;
-                } else {
-                   right--;
-                }
+            if (nums[mid] > nums[right]) { // mid must be in left part
+                left = mid;
+            } else if (nums[mid] < nums[right]) { // mid must be in right part
+                right = mid;
             } else {
-                if (nums[mid] >= nums[left]) {
-                    left = mid;
-                } else {
-                    right = mid;
-                }
+                right--; // not matter mid in left or right(we don't know), min should be in the left side of mid
             }
         }
-        if (nums[left] <= nums[right]) return nums[left];
+        if (nums[left] < nums[right]) return nums[left];
         else return nums[right];
     }
 }
