fc40b5714e43209f0d6730e3ea7a87d799a90a09
Yaxiong Hu <1006738657@qq.com>
2021-04-26 22:50:41 -0700

    leetcode 50

--- powXN/powXN_2.java
+++ powXN/powXN_2.java
@@ -1,16 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
+        if (n == 0) return 1;
         long N = n;
-        if (N == 0) return 1;
         if (N < 0) {
             N = -N;
             x = 1 / x;
         }
-        double res = myPow(x * x, (int)(N / 2)); 
+        double sub = myPow(x, (int) (N / 2));
         if (N % 2 == 0) {
-            return res;
+            return sub * sub;
         } else {
-            return res * x;
+            return sub * sub * x;
         }
     }
 }
