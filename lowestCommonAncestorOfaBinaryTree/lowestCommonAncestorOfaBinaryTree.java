e8ab6b275c6cd104a0725b88f2d9180c1f1b997f
Yaxiong Hu <1006738657@qq.com>
2021-11-29 21:20:36 -0600

    leetcode 236

--- lowestCommonAncestorOfaBinaryTree/lowestCommonAncestorOfaBinaryTree.java
+++ lowestCommonAncestorOfaBinaryTree/lowestCommonAncestorOfaBinaryTree.java
@@ -9,18 +9,11 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q)
-            return root;
+        if (root == null || root.val == p.val || root.val == q.val) return root;
         TreeNode leftRes = lowestCommonAncestor(root.left, p, q);
         TreeNode rightRes = lowestCommonAncestor(root.right, p, q);
-        if (leftRes != null && rightRes != null) {
-            return root;
-        } else if (leftRes != null) {
-            return leftRes;
-        } else if (rightRes != null) {
-            return rightRes;
-        } else {
-            return null;
-        }
+        if (leftRes == null) return rightRes;
+        else if (rightRes == null) return leftRes;
+        else return root;
     }
 }
