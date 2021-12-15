87c5389404a4170ae60ede3d99e61b5621ccb1eb
Yaxiong Hu <1006738657@qq.com>
2021-12-14 21:25:32 -0600

    leetcode 449

---
+++ serializeAndDeserializeBST/serializeAndDeserializeBST.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+    
+    public void serializeHelper(TreeNode root, StringBuilder sb) {
+        if (root == null) return;
+        sb.append(root.val).append(" ");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty()) return null;
+        Queue<String> q = new LinkedList<>(Arrays.asList(data.split(" ")));
+        return deserializeHelper(q, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    public TreeNode deserializeHelper(Queue<String> q, int lower, int upper) {
+        if (q.isEmpty()) return null;
+        String s = q.peek();
+        int val = Integer.parseInt(s);
+        if (val < lower || val > upper) return null;
+        q.poll();
+        TreeNode root = new TreeNode(val);
+        root.left = deserializeHelper(q, lower, val);
+        root.right = deserializeHelper(q, val, upper);
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
