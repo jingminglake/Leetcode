17b69f90ac6ac83ddac7e70d395527f431fc210f
Yaxiong Hu <1006738657@qq.com>
2021-12-16 19:20:46 -0600

    leetcode 101

---
+++ symmetricTree/symmetricTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetricHelper(root.left, root.right);
+    }
+    
+    public boolean isSymmetricHelper(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        } else if (root1 == null || root2 == null) {
+            return false;
+        } else if (root1.val == root2.val) {
+            return isSymmetricHelper(root1.left, root2.right) &&
+                isSymmetricHelper(root1.right, root2.left);
+        } else {
+            return false;
+        }
+    }
+}
