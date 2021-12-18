25be94fb2fbce3cfd44bc5d404805902b7ec4bd5
Yaxiong Hu <1006738657@qq.com>
2021-12-18 03:32:45 -0600

    leetcode 652

--- findDuplicateSubtrees/findDuplicateSubtrees.java
+++ findDuplicateSubtrees/findDuplicateSubtrees.java
@@ -4,23 +4,34 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        List<TreeNode> res = new LinkedList<>();
-        Map<String, Integer> m = new HashMap<>();
-        traverse(root, m, res);
+        List<TreeNode> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        findDuplicateSubtreesHelper(root, map, res);
         return res;
     }
-    public String traverse(TreeNode root, Map<String, Integer> m, List<TreeNode> res) {
-        if (root == null)
-            return "#";
-        String str = root.val + traverse(root.left, m, res) + traverse(root.right, m, res);
-        if (m.getOrDefault(str, 0) == 1)
-            res.add(root);
-        m.put(str, m.getOrDefault(str, 0) + 1);
-        return str;
+    
+    public String findDuplicateSubtreesHelper(TreeNode root, Map<String, Integer> map, List<TreeNode> res) {
+        if (root == null) return "#";
+        StringBuilder sb = new StringBuilder();
+        String uniqueOrder = String.valueOf(root.val) + "," + findDuplicateSubtreesHelper(root.left, map, res) +  "," + findDuplicateSubtreesHelper(root.right, map, res);
+        if (map.containsKey(uniqueOrder)) {
+            int num = map.get(uniqueOrder);
+            if (num == 1) res.add(root);
+            map.put(uniqueOrder, num + 1);
+        } else {
+            map.put(uniqueOrder, 1);
+        }
+        return uniqueOrder;
     }
 }
