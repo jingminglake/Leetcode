15ab948ed1dae15f14ab4e03f5d964948e49e336
Yaxiong Hu <1006738657@qq.com>
2021-12-19 17:44:59 -0600

    leetcode 516

---
+++ longestPalindromicSubsequence/longestPalindromicSubsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int res = 0;
+        int[][] dp = new int[s.length()][s.length()]; 
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    } else {
+                        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[0][s.length() - 1];  
+    }
+}
