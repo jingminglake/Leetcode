118e47c6747549ea37358d8a3a91464f5882b05e
Yaxiong Hu <1006738657@qq.com>
2021-12-20 00:43:38 -0600

    leetcode 53

---
+++ maximumSubarray/maxSubArray_2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
