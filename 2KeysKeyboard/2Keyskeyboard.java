f2c5b11dd60f355a93c52537d8a7ae76d88c55eb
Yaxiong Hu <1006738657@qq.com>
2021-12-20 18:19:59 -0600

    leetcode 650

--- 2KeysKeyboard/2Keyskeyboard.java
+++ 2KeysKeyboard/2Keyskeyboard.java
@@ -1,32 +1,19 @@
-// clang++ 2Keyskeyboard.cpp -std=c++11
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
-public:
-    int minSteps(int n) {
+    public int minSteps(int n) {
         if (n < 2) return 0;
-        vector<int> dp(n + 1, INT_MAX);
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) dp[i] = Integer.MAX_VALUE;
         dp[0] = 0;
         dp[1] = 0;
         dp[2] = 2;
         for (int i = 3; i <= n; i++) {
             for (int j = i / 2; j >= 1; j--) {
                 if (i % j == 0) {
-                    dp[i] = min(dp[i], dp[j] + i / j);
+                    dp[i] = Math.min (dp[i], dp[j] + i / j);
                     break;
                 }
             }
         }
         return dp[n];
     }
-};
-
-int main()
-{
-    Solution s;
-    cout << s.minSteps(100) << endl;
-    return 0;
 }
