f7372c55dd4e5a7eeeed525aaf534f70a8f415cc
Yaxiong Hu <1006738657@qq.com>
2021-12-24 05:00:44 -0600

    leetcode 322

---
+++ coinChange/coinChange.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        if (dp[amount] == Integer.MAX_VALUE) return -1;
+        else return dp[amount];
+    }
+}
