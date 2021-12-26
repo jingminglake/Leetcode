0b316dfbe766b9f87af72fdd332b5573f7e1f5e8
Yaxiong Hu <1006738657@qq.com>
2021-12-25 18:52:48 -0600

    leetcode 221

---
+++ maximalSquare/maximalSquare.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int res = 0;
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
+            res = Math.max(res, dp[i][0]);
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j] == '1' ? 1 : 0;
+            res = Math.max(res, dp[0][j]);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res * res;
+    }
+}
