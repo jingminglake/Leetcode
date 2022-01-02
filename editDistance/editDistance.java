9807835fd71b4f29c30faf31efa97da5f2e05a7e
Yaxiong Hu <1006738657@qq.com>
2022-01-02 16:12:00 -0600

    leetcode 72

---
+++ editDistance/editDistance.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) dp[i][0] = i;
+        for (int j = 0; j <= word2.length(); j++) dp[0][j] = j;
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
