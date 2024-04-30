80fe60673861011bcdd3ca524d17478240e98a73
Yaxiong Hu <1006738657@qq.com>
2024-04-30 03:14:03 -0500

    leetcode 403

--- frogJump/frogJump.java
+++ frogJump/frogJump.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        Map<Integer, Set<Integer>> hm = new HashMap<>();
-        for (int i = 0; i < stones.length; i++) hm.put(i, new HashSet<>()); // init
-        hm.get(0).add(0);
-        for (int i = 0; i < stones.length; i++) {
-            for (int lastStep : hm.get(i)) {
-                for (int j = i + 1; j < stones.length; j++) {
-                    int gap = stones[j] - stones[i];
-                    if (lastStep - 1 >= 0 && lastStep - 1 == gap) hm.get(j).add(lastStep - 1);
-                    else if (lastStep == gap) hm.get(j).add(lastStep);
-                    else if (lastStep + 1 == gap) hm.get(j).add(lastStep + 1);
-                    else if (lastStep + 1 < gap) break;
-                }
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+        for (int stone : stones) {
+            dp.put(stone, new HashSet<>());
+        }
+        dp.get(0).add(0);
+
+        for (int s : stones) {
+            for (int k : dp.get(s)) {
+                if (k - 1 > 0 && dp.containsKey(s + k - 1))
+                    dp.get(s + k - 1).add(k - 1);
+                if (dp.containsKey(s + k))
+                    dp.get(s + k).add(k);
+                if (dp.containsKey(s + k + 1))
+                    dp.get(s + k + 1).add(k + 1);
             }
         }
-        return hm.get(stones.length - 1).size() >= 1;
+
+        return !dp.get(stones[stones.length - 1]).isEmpty();
     }
 }
