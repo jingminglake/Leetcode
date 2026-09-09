d27f4e8e3e91b3c3ea0bfc6cdc5fcac52e06e24b
Yaxiong Hu <1006738657@qq.com>
2026-09-09 16:49:05 -0500

    update

---
+++ 973.kClosestPointsToOrign/kClosestPointsToOrign.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        List<int[]> res = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            double dis1 = a[0] * a[0] + a[1] * a[1];
+            double dis2 = b[0] * b[0] + b[1] * b[1];
+            return Double.compare(dis1, dis2);
+        });
+
+        for (int[] p : points) {
+            pq.add(p);
+        }
+        for (int i = 0; i < k; i++) {
+            if (pq.isEmpty()) break;
+            int[] p = pq.poll();
+            res.add(p);
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
