853763f4df9c82c346fb4307774a5c2e34c6891d
Yaxiong Hu <1006738657@qq.com>
2026-09-10 18:35:37 -0500

    update

---
+++ 322.coinChange/coinChange_2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int impossible = amount + 1;
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, impossible);
+
+        dp[0] = 0;
+
+        for (int currentAmount = 1; currentAmount <= amount; currentAmount++) {
+
+            for (int coin : coins) {
+                if (coin <= currentAmount) {
+                    dp[currentAmount] = Math.min(dp[currentAmount], dp[currentAmount - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == impossible ? -1 : dp[amount];
+    }
+}
