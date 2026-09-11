7322dd985363f7835e158c4f0c081ec69701bae2
Yaxiong Hu <1006738657@qq.com>
2026-09-10 20:20:01 -0500

    update

---
+++ 98.validateBinarySearchTree/validateBinarySearchTree_2.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private boolean validate(TreeNode root, long lowerB, long upperB) {
+        if (root == null) return true;
+        if (root.val <= lowerB || root.val >= upperB) return false;
+        return validate(root.left, lowerB, root.val) && validate(root.right, root.val, upperB);
+    }
+}
